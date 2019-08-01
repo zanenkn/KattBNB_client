7bc33fe92ca50e13a8eb41970610ba53df96cbca
zanenkn <zane.neikena@gmail.com>
2019-08-01 19:48:13 +0200

    Installs semantic-ui-react.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './semantic/dist/semantic.min.css'
+import {Header} from 'semantic-ui-react'
 
 function App() {
   return (
-    <div id='test'>
-      Meow!
-    </div>
+    <Header>
+      <div id='test'>
+        Meow!
+      </div>
+    </Header>
+    
   )
 }
 
