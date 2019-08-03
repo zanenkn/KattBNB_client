c65e4a11fb59c92ddc5a66f8aa221433a43e2b10
zanenkn <zane.neikena@gmail.com>
2019-08-03 16:44:19 +0200

    Correcting mistakes in the test.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- cypress/integration/VisitorCanViewMenu.spec.js
+++ cypress/integration/VisitorCanViewMenu.spec.js
@@ -5,24 +5,32 @@ describe('Visitor can view menu', () => {
   })
   
   it('and can toggle visibility by clicking on the hamburger', () => {
-    cy.find('#hamburger').click()
-    cy.find('#menu').should('be.visible')
-    cy.find('#hamburger').click()
-    cy.find('#menu').should('not.be.visible')
+    cy.get('#hamburger').within(() => {
+      cy.get('.icon').click()
+    })
+    cy.get('#menu').should('be.visible')
+    cy.get('#hamburger').within(() => {
+      cy.get('.icon').click()
+    })
+    cy.get('#menu').should('not.be.visible')
   })
 
   it('and can see links to different homepage sections', () => {
     let links = ['Login', 'Sign up', 'About us', 'Legal', 'FAQ', 'Contact us', 'Blog']
-    cy.find('#hamburger').click()
+    cy.get('#hamburger').within(() => {
+      cy.get('.icon').click()
+    })
     links.forEach(link => {
       cy.contains(link)
     })
   })
 
   it('and can toggle menu visibility off by clicking outside the menu', () => {
-    cy.find('#hamburger').click()
-    cy.find('#menu').should('be.visible')
-    cy.find('#app-content').click()
-    cy.find('#menu').should('not.be.visible')
+    cy.get('#hamburger').within(() => {
+      cy.get('.icon').click()
+    })
+    cy.get('#menu').should('be.visible')
+    cy.get('.content-wrapper').click()
+    cy.get('#menu').should('not.be.visible')
   })
 })
