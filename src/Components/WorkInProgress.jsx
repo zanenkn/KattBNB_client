c21c6f76ac3a59d6a153e166c893b3784d432c3c
zanenkn <zane.neikena@gmail.com>
2019-08-04 19:15:09 +0200

    Creates WorkInProgress component and imports it in AboutUs component.
    Co-authored-by: george <raptor_f1@hotmail.com>

---
+++ src/Components/WorkInProgress.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header, Segment } from 'semantic-ui-react'
+
+const WIP = () => {
+  return (
+    <Header as='h2'>
+      This section is work in progress!
+    </Header>
+  )
+}
+
+export default WIP
