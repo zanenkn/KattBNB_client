dc713705cb9cb3b4e44230f7cabec7615cf4a6c2
zanenkn <zane.neikena@gmail.com>
2019-08-04 20:29:24 +0200

    Adds on click action to the looking glass icon in order to close the menu if the icon is clicked.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -18,7 +18,7 @@ class Navbar extends Component {
 
           <Grid.Column style={{ 'padding': 0 }} width={12}>
             <Grid id='navlinks'>
-              <Grid.Column className='navlink' width={4} as={Link} to='/'>
+              <Grid.Column className='navlink' width={4} as={Link} to='/' onClick={this.props.menuVisible ? () => { this.props.dispatch({ type: 'CHANGE_VISIBILITY' }) } : () => { }}>
                 <svg className='icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill='#FFFFFF' d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z" /></svg>
               </Grid.Column>
               <Grid.Column className='navlink' width={4}>
