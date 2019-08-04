93066c3e925f579e0c93dbc8825880102fee987c
zanenkn <zane.neikena@gmail.com>
2019-08-04 20:16:47 +0200

    Styling the WorkInProgress component.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/WorkInProgress.jsx
+++ src/Components/WorkInProgress.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
-import { Header, Segment } from 'semantic-ui-react'
+import { Header, Segment, Icon, Container } from 'semantic-ui-react'
 
 const WIP = () => {
   return (
-    <Header as='h2'>
-      This section is work in progress!
-    </Header>
+    <Segment padded compact style={{'margin': 'auto', 'padding': '2rem', 'margin-top': '2rem'}} >
+
+      <Header as='h4'>
+        <Container padded>
+          <Icon loading name='cog' style={{'color': '#80808069', 'font-size': '3rem', 'margin-bottom': '1rem'}} />
+        </Container>
+        <Container text padded>
+          This section is still a work in progress!
+        </Container>
+      </Header>
+
+    </Segment>
   )
 }
 
