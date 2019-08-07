7ecedee4dee9cabe4c6e26c9fc8e3aaa42bb08e5
zanenkn <zane.neikena@gmail.com>
2019-08-07 15:48:36 +0200

    Adds some form fields for Signup component, styling.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,19 +1,54 @@
 import React, { Component } from 'react'
-import { Sidebar, Header, Segment } from 'semantic-ui-react'
-import WIP from './WorkInProgress'
+import { Sidebar, Header, Segment, Form, Button } from 'semantic-ui-react'
 
 class SignUp extends Component {
+  state = {
+    email: '',
+    password: '',
+    password_confirmation: '',
+    username: '',
+    location: ''
+  }
   render() {
     return (
       <Sidebar.Pushable className='content-wrapper' >
+        
         <Header as='h1'>
           Sign up
         </Header>
-        
-        <WIP />
 
-        <Segment>
-          lorem ipsum dolor
+        <Segment className='whitebox'>
+          <Form id="signup-form">
+              <Form.Input
+                id="email"
+                value={this.state.email}
+                onChange={this.onChangeHandler}
+                placeholder="Email"
+              />
+              <Form.Input
+                id="password"
+                type="password"
+                value={this.state.password}
+                onChange={this.onChangeHandler}
+                placeholder="Password"
+              />
+              <Form.Input
+                id="password_confirmation"
+                type="password"
+                value={this.state.password_confirmation}
+                onChange={this.onChangeHandler}
+                placeholder="Repeat password"
+              />
+              <Form.Input
+                id="username"
+                value={this.state.username}
+                onChange={this.onChangeHandler}
+                placeholder="Username"
+              />
+            </Form>
+
+            <Button className='submit-button' id="sign-up-button" onClick={this.createUser}>Sign up</Button>
+
         </Segment>
 
       </Sidebar.Pushable>
