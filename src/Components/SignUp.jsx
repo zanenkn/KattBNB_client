4125e63429a5eab28a682108c992fe08df8fc6d5
zanenkn <zane.neikena@gmail.com>
2019-08-07 16:35:11 +0200

    Creates and imports location options, styling for button.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { Sidebar, Header, Segment, Form, Button } from 'semantic-ui-react'
+import { Sidebar, Header, Segment, Form, Button, Dropdown } from 'semantic-ui-react'
+import { LOCATION_OPTIONS } from '../Modules/locationData'
 
 class SignUp extends Component {
   state = {
@@ -19,36 +20,45 @@ class SignUp extends Component {
 
         <Segment className='whitebox'>
           <Form id="signup-form">
-              <Form.Input
-                id="email"
-                value={this.state.email}
-                onChange={this.onChangeHandler}
-                placeholder="Email"
-              />
-              <Form.Input
-                id="password"
-                type="password"
-                value={this.state.password}
-                onChange={this.onChangeHandler}
-                placeholder="Password"
-              />
-              <Form.Input
-                id="password_confirmation"
-                type="password"
-                value={this.state.password_confirmation}
-                onChange={this.onChangeHandler}
-                placeholder="Repeat password"
-              />
-              <Form.Input
-                id="username"
-                value={this.state.username}
-                onChange={this.onChangeHandler}
-                placeholder="Username"
-              />
-            </Form>
+            <Form.Input
+              id="email"
+              value={this.state.email}
+              onChange={this.onChangeHandler}
+              placeholder="Email"
+            />
+            <Form.Input
+              id="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.onChangeHandler}
+              placeholder="Password"
+            />
+            <Form.Input
+              id="password_confirmation"
+              type="password"
+              value={this.state.password_confirmation}
+              onChange={this.onChangeHandler}
+              placeholder="Repeat password"
+            />
+            <Form.Input
+              id="username"
+              value={this.state.username}
+              onChange={this.onChangeHandler}
+              placeholder="Username"
+            />
+            <Dropdown
+              clearable
+              search
+              selection
+              style={{'width': '100%'}}
+              placeholder="Select location"
+              options={LOCATION_OPTIONS}
+              id="location"
+              onChange={this.handleLocationChange}
+            />
+          </Form>
 
-            <Button className='submit-button' id="sign-up-button" onClick={this.createUser}>Sign up</Button>
-
+          <Button id="sign-up-button" onClick={this.createUser}>Sign up</Button>
         </Segment>
 
       </Sidebar.Pushable>
