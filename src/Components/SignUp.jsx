1643664902fda0b9ad8d9894284cb65383a49290
raptorf1 <raptor_f1@hotmail.com>
2019-08-11 21:51:50 +0300

    Changes URLs to production services
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -12,7 +12,7 @@ class SignUp extends Component {
     nickname: '',
     location: '',
     errors: '',
-    url: 'http://localhost:3000/login',
+    url: 'http://kattbnb.netlify.com/login',
     error_display: false
   }
 
--- src/reduxTokenAuthConfig.js
+++ src/reduxTokenAuthConfig.js
@@ -3,7 +3,7 @@ const production = 'http://katt-bnb.herokuapp.com/api/v1/auth'
 const development = 'http://localhost:3007/api/v1/auth'
 
 const config = {
-  authUrl: development,
+  authUrl: production,
   userAttributes: {
     uid: 'uid'
   },
