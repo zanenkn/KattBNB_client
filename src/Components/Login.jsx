e5dd4186b4a3e698a7df484169ad3778e200d597
zanenkn <zane.neikena@gmail.com>
2019-08-12 15:26:26 +0200

    Input fields in Login component.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,16 +1,58 @@
 import React, { Component } from 'react'
-import { Sidebar, Header } from 'semantic-ui-react'
-import WIP from './WorkInProgress'
+import { Sidebar, Header, Segment, Form, Message, Button } from 'semantic-ui-react'
 
 class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    errors: '',
+    error_display: false
+  }
+
   render() {
+    let errorDisplay
+
+    if(this.state.error_display) {
+      errorDisplay = (
+        <Message negative >
+          <ul id="message-error-list">
+            {this.state.errors.map(error => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        </Message>
+      )
+    }
+
     return (
       <Sidebar.Pushable className='content-wrapper' >
+              
         <Header as='h1'>
           Log in
         </Header>
-        
-        <WIP />
+
+        <Segment className='whitebox'>
+
+          {errorDisplay}
+
+          <Form id="login-form">
+            <Form.Input
+              id="email"
+              value={this.state.email}
+              onChange={this.onChangeHandler}
+              placeholder="Email"
+            />
+            <Form.Input
+              id="password"
+              type="password"
+              value={this.state.password}
+              onChange={this.onChangeHandler}
+              placeholder="Password"
+            />
+            <Button className="submit-button" id="log-in-button">Log in</Button>
+          </Form>
+
+        </Segment>
         
       </Sidebar.Pushable>
     )
