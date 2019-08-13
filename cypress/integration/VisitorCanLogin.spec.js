81601d0b8c21d5a59aed46b7771e526736fe2c8c
raptorf1 <raptor_f1@hotmail.com>
2019-08-13 18:07:07 +0300

    Writes a sad path acceptance test for login when not being confirmed
    Co-authored-by: zane <zane.neikena@gmail.com>

--- cypress/integration/VisitorCanLogin.spec.js
+++ cypress/integration/VisitorCanLogin.spec.js
@@ -8,8 +8,13 @@ describe('User can log in', () => {
     cy.contains('You have succesfully logged in! Please wait to be redirected.')
   })
 
-  it('with invalid credentials', () => {
+  it('unsuccessfuly with invalid credentials', () => {
     cy.login('fixture:unsuccessful_login.json', 'george@mail.com', 'wrongpassword', 401)
     cy.contains('Invalid login credentials. Please try again.')
+  })
+
+  it('unsuccessfuly without first being confirmed through email', () => {
+    cy.login('fixture:unsuccessful_login_no_confirmation.json', 'alonso@renault.com', 'wrongpassword', 401)
+    cy.contains("A confirmation email was sent to your account at 'alonso@renault.com'. You must follow the instructions in the email before your account can be activated")
   })
 })
