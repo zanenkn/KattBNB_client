cccf769bf0b3ae621b54ddfd9f612a7b97377a47
raptorf1 <raptor_f1@hotmail.com>
2019-08-18 11:36:42 +0300

    Protects UserPage component with an if statement in App.js. Refactors code in UserPage component after that.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -1,46 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Redirect } from 'react-router-dom'
 import { Header, Segment } from 'semantic-ui-react'
 
 class UserPage extends Component {
+
   render() {
-    let profile
-    if (this.props.currentUserIn) {
-      profile = (
-        <div className='content-wrapper'>
-          <Segment className='whitebox'>
-            <Header as='h2'>
-              Hi, {this.props.username}!
-            </Header>
-            <p>
-              This is your profile. Here you can update your location, picture and password.
-            </p>
-            <p>
-              <svg height='1rem' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20S3 10.87 3 7a7 7 0 1 1 14 0c0 3.87-7 13-7 13zm0-11a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" /></svg>
-              &nbsp;{this.props.location}
-            </p>
-            <p>
-              <svg height='1rem' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" /></svg>
-              &nbsp;******
-            </p>
-          </Segment>
-        </div>
-      )
-    } else {
-      return <Redirect to='/login' />
-    }
+
     return (
-      <>
-        {profile}
-      </>
+      <div className='content-wrapper'>
+        <Segment className='whitebox'>
+          <Header as='h2'>
+            Hi, {this.props.username}!
+            </Header>
+          <p>
+            This is your profile. Here you can update your location, picture and password.
+            </p>
+          <p>
+            <svg height='1rem' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20S3 10.87 3 7a7 7 0 1 1 14 0c0 3.87-7 13-7 13zm0-11a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" /></svg>
+            &nbsp;{this.props.location}
+          </p>
+          <p>
+            <svg height='1rem' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" /></svg>
+            &nbsp;******
+            </p>
+        </Segment>
+      </div>
     )
   }
 }
 
 
 const mapStateToProps = state => ({
-  currentUserIn: state.reduxTokenAuth.currentUser.isSignedIn,
   username: state.reduxTokenAuth.currentUser.attributes.username,
   location: state.reduxTokenAuth.currentUser.attributes.location
 })
