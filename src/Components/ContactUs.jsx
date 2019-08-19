1a5d48630bac7c59fd2c7643d53cd9d2bb39fd75
zanenkn <zane.neikena@gmail.com>
2019-08-19 15:17:00 +0200

    Adds 'as' prop in hopes to fix the Netlify form bug.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -17,13 +17,10 @@ class ContactUs extends Component {
                 <>
                   <p>Fill in this form to send me a message. We will get back to you as soon as we read it!</p>
 
-                  <input type='text' placeholder="Your name" name='name' required />
-                  <input type='email' placeholder="Your email address" name='email' required />
-                  <textarea placeholder="Your message" name='message' required />
-                  <button className="button">Send</button>
-
-                  {/* <Form>
+                  
+                  <Form>
                     <Form.Input
+                      as='input'
                       type='text'
                       required
                       name='name'
@@ -31,18 +28,20 @@ class ContactUs extends Component {
                     />
 
                     <Form.Input
+                      as='input'
                       type='email'
                       required
                       name='email'
                       placeholder='Your email address'
                     />
                     <TextArea
+                      as='textarea'
                       required
                       name='message'  
                       placeholder='Your message'
                     />
                   </Form>
-                  <Button className='submit-button'>Send</Button> */}
+                  <Button className='submit-button'>Send</Button>
                   {/* <div>
                     <input type='text' placeholder="Your name" name='name' required />
                     <input type='email' placeholder="Your email address" name='email' required />
