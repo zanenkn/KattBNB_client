53c1d12e5f64d8e70794a40c1f9d8718e14ab0ac
raptorf1 <raptor_f1@hotmail.com>
2019-08-19 17:52:00 +0300

    Updates error message to fit both for location and password updates
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -149,7 +149,7 @@ class UserPage extends Component {
     if (this.state.errorDisplay) {
       errorDisplay = (
         <Message negative >
-          <Message.Header textAlign='center'>Location could not be updated because of following error(s):</Message.Header>
+          <Message.Header textAlign='center'>Update action could not be completed because of following error(s):</Message.Header>
           <ul id="message-error-list">
             {this.state.errors.map(error => (
               <li key={error}>{error}</li>
