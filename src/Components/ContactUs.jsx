937849f00dd3620ae0b744974b7e2240b4857536
zanenkn <zane.neikena@gmail.com>
2019-08-19 14:26:19 +0200

    Implements NetlifyForm from netlify-form-react and places a hidden form in index.html for bots.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/ContactUs.jsx
+++ src/Components/ContactUs.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { Sidebar, Header } from 'semantic-ui-react'
-import WIP from './WorkInProgress'
+import { Sidebar, Header, Message, Form, TextArea, Button } from 'semantic-ui-react'
+import NetlifyForm from 'react-netlify-form'
 
 class ContactUs extends Component {
   render() {
@@ -10,7 +10,55 @@ class ContactUs extends Component {
           Contact us
         </Header>
 
-        <WIP />
+        <NetlifyForm name='contact'>
+          {({ error, success }) => (
+            <>
+              {!success &&
+                <>
+                  <p>Fill in this form to send me a message. We will get back to you as soon as we read it!</p>
+
+                  <Form id='login-form'>
+                    <Form.Input
+                      type='text'
+                      required
+                      name='name'
+                      placeholder='Your name'
+                    />
+
+                    <Form.Input
+                      type='email'
+                      required
+                      name='name'
+                      placeholder='Your email address'
+                    />
+                    <TextArea
+                      required
+                      name='message'  
+                      placeholder='Your message'
+                    />
+                  </Form>
+                  <Button className='submit-button'>Send</Button>
+                  {/* <div>
+                    <input type='text' placeholder="Your name" name='name' required />
+                    <input type='email' placeholder="Your email address" name='email' required />
+                    <textarea placeholder="Your message" name='message' required />
+                    <button className="button">Send</button>
+                  </div> */}
+                </>
+              }
+              {error &&
+                <Message negative textAlign='center'>
+                  Your information was not sent. Please try again later.
+                </Message>
+              }
+              {success &&
+                <Message success textAlign='center'>
+                  Thank you for your message!
+               </Message>
+              }
+            </>
+          )}
+        </NetlifyForm>
 
       </Sidebar.Pushable>
     )
