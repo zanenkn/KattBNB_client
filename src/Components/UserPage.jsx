ae7de8360e59d3dc283c86dd9c72a22a472ab822
raptorf1 <raptor_f1@hotmail.com>
2019-08-20 18:52:52 +0300

    Gets rid of yellow warnings of console. Auto formats code.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Header, Segment, Form, Dropdown, Button, Message, Grid } from 'semantic-ui-react'
+import { Header, Segment, Form, Dropdown, Button, Message } from 'semantic-ui-react'
 import { LOCATION_OPTIONS } from '../Modules/locationData'
 import axios from 'axios'
 
@@ -192,21 +192,21 @@ class UserPage extends Component {
 
     if (this.state.loading) {
       locationSubmitButton = (
-        <Button id="location-submit-button" style={{'marginTop': '1rem'}} loading>Change</Button>
+        <Button id="location-submit-button" style={{ 'marginTop': '1rem' }} loading>Change</Button>
       )
     } else {
       locationSubmitButton = (
-        <Button id="location-submit-button" style={{'marginTop': '1rem'}} onClick={this.updateLocation}>Change</Button>
+        <Button id="location-submit-button" style={{ 'marginTop': '1rem' }} onClick={this.updateLocation}>Change</Button>
       )
     }
 
     if (this.state.loading) {
       passwordSubmitButton = (
-        <Button id="password-submit-button" style={{'marginTop': '1rem'}} loading>Change</Button>
+        <Button id="password-submit-button" style={{ 'marginTop': '1rem' }} loading>Change</Button>
       )
     } else {
       passwordSubmitButton = (
-        <Button id="password-submit-button" style={{'marginTop': '1rem'}} onClick={this.updatePassword}>Change</Button>
+        <Button id="password-submit-button" style={{ 'marginTop': '1rem' }} onClick={this.updatePassword}>Change</Button>
       )
     }
 
@@ -219,7 +219,6 @@ class UserPage extends Component {
               clearable
               search
               selection
-              style={{ 'width': '100%' }}
               placeholder="Select new location"
               options={LOCATION_OPTIONS}
               id='location'
@@ -239,14 +238,14 @@ class UserPage extends Component {
             />
           </Form>
 
-          <div style={{'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-evenly', 'margin-bottom': '2em'}}>
+          <div style={{ 'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-evenly', 'margin-bottom': '2em' }}>
             <div >
-              <Button secondary id="location-cancel-button" style={{'marginTop': '1rem'}} onClick={this.locationFormHandler.bind(this)}>Cancel</Button>
+              <Button secondary id="location-cancel-button" style={{ 'marginTop': '1rem' }} onClick={this.locationFormHandler.bind(this)}>Cancel</Button>
             </div>
             <div>
               {locationSubmitButton}
             </div>
-          </div>         
+          </div>
         </>
       )
     }
@@ -285,13 +284,13 @@ class UserPage extends Component {
             />
           </Form>
 
-          <p style={{'textAlign': 'center', 'margin': 'auto', 'fontSize': 'small', 'marginTop': '1rem'}}>
+          <p style={{ 'textAlign': 'center', 'margin': 'auto', 'fontSize': 'small', 'marginTop': '1rem' }}>
             Upon successful password change you will be redirected back to login.
           </p>
 
-          <div style={{'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-evenly', 'margin-bottom': '2em'}}>
+          <div style={{ 'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-evenly', 'margin-bottom': '2em' }}>
             <div >
-              <Button secondary id="location-cancel-button" style={{'marginTop': '1rem'}} onClick={this.passwordFormHandler.bind(this)}>Cancel</Button>
+              <Button secondary id="location-cancel-button" style={{ 'marginTop': '1rem' }} onClick={this.passwordFormHandler.bind(this)}>Cancel</Button>
             </div>
             <div>
               {passwordSubmitButton}
@@ -307,46 +306,46 @@ class UserPage extends Component {
           <Header as='h2'>
             Hi, {this.props.username}!
           </Header>
-          <p style={{'textAlign': 'center'}}>
+          <p style={{ 'textAlign': 'center' }}>
             This is your profile. Here you can update your location, picture and password.
           </p>
 
-          <div style={{'width': '100%', 'margin': 'auto'}}>
+          <div style={{ 'width': '100%', 'margin': 'auto' }}>
             <p>
               <svg fill='grey' height='1rem' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13.6 13.47A4.99 4.99 0 0 1 5 10a5 5 0 0 1 8-4V5h2v6.5a1.5 1.5 0 0 0 3 0V10a8 8 0 1 0-4.42 7.16l.9 1.79A10 10 0 1 1 20 10h-.18.17v1.5a3.5 3.5 0 0 1-6.4 1.97zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" /></svg>
               &nbsp;{this.props.email}
             </p>
 
-            <div style={{'display': 'flex', 'flex-direction': 'row', 'margin-bottom': '1em'}}>
+            <div style={{ 'display': 'flex', 'flex-direction': 'row', 'margin-bottom': '1em' }}>
 
-                <p id='user-location' style={{'margin-top': 'auto', 'margin-bottom': 'auto'}}>
-                  <svg fill='grey' height='1rem' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20S3 10.87 3 7a7 7 0 1 1 14 0c0 3.87-7 13-7 13zm0-11a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" /></svg>
-                  &nbsp;{this.state.location}&nbsp;
+              <p id='user-location' style={{ 'margin-top': 'auto', 'margin-bottom': 'auto' }}>
+                <svg fill='grey' height='1rem' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20S3 10.87 3 7a7 7 0 1 1 14 0c0 3.87-7 13-7 13zm0-11a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" /></svg>
+                &nbsp;{this.state.location}&nbsp;
                 </p>
-        
-                <Header id='change-location-link' style={{'margin-top': 'auto', 'margin-bottom': 'auto'}} onClick={this.locationFormHandler.bind(this)} className='fake-link-underlined' >
-                  Change
+
+              <Header id='change-location-link' style={{ 'margin-top': 'auto', 'margin-bottom': 'auto' }} onClick={this.locationFormHandler.bind(this)} className='fake-link-underlined' >
+                Change
                 </Header>
-          
+
             </div>
             <div>
-            {locationForm}
+              {locationForm}
             </div>
-            <div style={{'display': 'flex', 'flex-direction': 'row', 'margin-bottom': '1em'}}>
+            <div style={{ 'display': 'flex', 'flex-direction': 'row', 'margin-bottom': '1em' }}>
 
-              <p style={{'margin-top': 'auto', 'margin-bottom': 'auto'}}>
+              <p style={{ 'margin-top': 'auto', 'margin-bottom': 'auto' }}>
                 <svg fill='grey' height='1rem' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" /></svg>
                 &nbsp;******&nbsp;
               </p>
 
-              <Header id='change-password-link' style={{'margin-top': 'auto', 'margin-bottom': 'auto'}} onClick={this.passwordFormHandler.bind(this)} className='fake-link-underlined' >
+              <Header id='change-password-link' style={{ 'margin-top': 'auto', 'margin-bottom': 'auto' }} onClick={this.passwordFormHandler.bind(this)} className='fake-link-underlined' >
                 Change
               </Header>
-            
+
             </div>
-            
+
             <div>
-            {passwordForm}
+              {passwordForm}
             </div>
 
           </div>
