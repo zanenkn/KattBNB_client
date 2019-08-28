f2caa6acc393c4ec0ebeee79f62f535cbfd925a8
raptorf1 <raptor_f1@hotmail.com>
2019-08-28 19:54:41 +0300

    Creates appropriate fixtures. Asignes IDs in HostProfile. Happy path test almost passing.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- cypress/integration/UserCanViewAndEditHerHostProfile.spec.js
+++ cypress/integration/UserCanViewAndEditHerHostProfile.spec.js
@@ -1,26 +1,32 @@
 describe('User can view her host profile', () => {
   beforeEach(function () {
     cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3007/api/v1/host_profiles?user_id=1',
+      status: 200,
+      response: 'fixture:host_profile_index.json'
+    })
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3007/api/v1/host_profiles/1',
+      status: 200,
+      response: 'fixture:host_profile_individual.json'
+    })
     cy.login('fixture:successful_login.json', 'george@mail.com', 'password', 200)
     cy.wait(2000)
     cy.get('#navlinks').within(() => {
       cy.get('#user-icon').click()
     })
-    cy.route({
-      method: 'GET',
-      url: 'http://localhost:3007/host_profile?user_id=1',
-      status: 200,
-      response: 'fixture:host_profile.json'
-    })
   })
 
   it('and see the saved information', () => {
     let text = [
-      ['#description', 'I hate people but I love cats! Nerd life chose me!'],
-      ['#address', 'Solvarvsgatan 32, 415 08 Göteborg, Sweden'],
-      ['#rate', '100 kr/day'],
-      ['#maxCats', '3 cats'],
-      ['#supplement', '35 kr/day per cat']
+      ['#description', 'this is a description about me!!!!'],
+      ['#address', 'Charles de Gaulle Airport'],
+      ['#rate', '100 kr/day for 1 cat'],
+      ['#maxCats', 'Maximum cats 3'],
+      ['#supplement', 'Extra 35 kr/day per cat']
     ]
 
     text.forEach(element => {
--- src/Components/HostProfile.jsx
+++ src/Components/HostProfile.jsx
@@ -54,38 +54,38 @@ class HostProfile extends Component {
           </p>
 
           <div style={{ 'width': '100%', 'margin': 'auto' }}>
-            <p>
+            <p id='description'>
               <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z" /></svg>
               &nbsp;{this.state.description}
             </p>
 
             <div className='flexbox-row'>
-              <p id='user-address' className='top-bottom-margin-auto'>
+              <p id='address' className='top-bottom-margin-auto'>
                 <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z" /></svg>
                 &nbsp;{this.state.full_address}
               </p>
             </div>
 
             <div className='flexbox-row'>
-              <p className='top-bottom-margin-auto'>
+              <p id='maxCats' className='top-bottom-margin-auto'>
                 <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 236.62 236.62"><path d="M197.023,225.545c-1.145-9.533-11.68-10.614-17.805-9.958c-6.521-24.554,16.225-61.151,17.563-69.82c1.438-9.312-6.658-63.5-7.513-90.938C188.389,26.662,147.48-4.433,140.65,0.524c-6.768,7.484,9.748,17.585,1.054,26.245c-8.398,8.367-10.588,13.99-16.824,23.46c-15.976,24.255,27.318,24.558,27.318,24.558s-33.882,25.112-41.421,37.768c-6.943,11.656-9.854,24.696-18.232,35.688c-19.094,25.051-14.791,68.729-14.791,68.729s-36.17-11.839-16.264-53.133C76.643,132.406,84.107,86.02,50.016,97.95c-13.189,4.616,2.949,14.325,5.734,17.435c9.318,10.4,1.441,27.896-4.174,38.012c-15.037,27.091-20.496,55.475,11.154,72.978c14.063,7.776,33.055,9.7,52.17,9.982l48.64,0.14C179.564,237.294,197.689,234.298,197.023,225.545z" /></svg>
-                &nbsp;Hosting maximum {this.state.maxCats}
+                &nbsp;Maximum cats {this.state.maxCats}
               </p>
             </div>
             <div className='flexbox-row'>
-              <p id='user-address' className='top-bottom-margin-auto'>
+              <p id='rate' className='top-bottom-margin-auto'>
                 <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M18 6V4H2v2h16zm0 4H2v6h16v-6zM0 4c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm4 8h4v2H4v-2z" /></svg>
                 &nbsp;{rate} kr/day for 1 cat
               </p>
             </div>
             <div className='flexbox-row'>
-              <p id='user-address' className='top-bottom-margin-auto'>
+              <p id='supplement' className='top-bottom-margin-auto'>
                 <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M11 9V5H9v4H5v2h4v4h2v-4h4V9h-4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20z" /></svg>
-                &nbsp;Extra {supplement} kr/cat/day
+                &nbsp;Extra {supplement} kr/day per cat
               </p>
             </div>
             <div className='flexbox-row'>
-              <p id='user-address' className='top-bottom-margin-auto'>
+              <p id='availability' className='top-bottom-margin-auto'>
                 <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M1 4c0-1.1.9-2 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4zm2 2v12h14V6H3zm2-6h2v2H5V0zm8 0h2v2h-2V0zM5 9h2v2H5V9zm0 4h2v2H5v-2zm4-4h2v2H9V9zm0 4h2v2H9v-2zm4-4h2v2h-2V9zm0 4h2v2h-2v-2z" /></svg>
                 &nbsp;{this.state.availability}
               </p>
