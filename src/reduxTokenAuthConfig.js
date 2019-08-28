b7bcda8e5b32e70f2f20d121fb737972ba88f1bf
raptorf1 <raptor_f1@hotmail.com>
2019-08-28 17:17:14 +0300

    Adds API URL as an environment variable
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/reduxTokenAuthConfig.js
+++ src/reduxTokenAuthConfig.js
@@ -1,7 +1,7 @@
 import { generateAuthActions } from 'redux-token-auth'
 
 
-const production = 'https://katt-bnb.herokuapp.com/api/v1/auth'
+const production = `${process.env.REACT_APP_API_ENDPOINT}/api/v1/auth`
 const development = 'http://localhost:3007/api/v1/auth'
 
 const config = {
