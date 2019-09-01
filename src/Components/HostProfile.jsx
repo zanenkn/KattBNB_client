7fda2572f983f6bdb99f139bf6ac93a0890203ef
zanenkn <zane.neikena@gmail.com>
2019-09-01 10:35:30 +0200

    Implements the neccesary code for changing max cats accepted amount in HostProfile.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/HostProfile.jsx
+++ src/Components/HostProfile.jsx
@@ -11,11 +11,13 @@ class HostProfile extends Component {
     full_address: '',
     rate: '',
     maxCats: '',
+    newMaxCats: '',
     supplement: '',
     availability: '',
     errors: '',
     errorDisplay: false,
-    editDescriptionForm: false
+    editDescriptionForm: false,
+    editMaxCatsForm: false
   }
 
   componentDidMount() {
@@ -55,7 +57,18 @@ class HostProfile extends Component {
     this.setState({
       editDescriptionForm: !this.state.editDescriptionForm,
       newDescription: this.state.description,
-      errorDisplay: false
+      errorDisplay: false,
+      editMaxCatsForm: false
+    })
+    this.props.closeLocPasForms()
+  }
+
+  maxCatsFormHandler = () => {
+    this.setState({
+      editMaxCatsForm: !this.state.editMaxCatsForm,
+      newMaxCats: this.state.maxCats,
+      errorDisplay: false,
+      editDescriptionForm: false
     })
     this.props.closeLocPasForms()
   }
@@ -95,9 +108,45 @@ class HostProfile extends Component {
     }
   }
 
+  updateMaxCats = (e) => {
+    e.preventDefault()
+    if (this.state.newMaxCats !== '' && this.state.newMaxCats !== this.state.maxCats) {
+      const path = `/api/v1/host_profiles/${this.props.id}`
+      const headers = {
+        uid: window.localStorage.getItem('uid'),
+        client: window.localStorage.getItem('client'),
+        'access-token': window.localStorage.getItem('access-token')
+      }
+      const payload = {
+        max_cats_accepted: this.state.newMaxCats
+      }
+      axios.patch(path, payload, { headers: headers })
+        .then(() => {
+          this.setState({
+            errorDisplay: false,
+            maxCats: this.state.newMaxCats,
+            editMaxCatsForm: false
+          })
+          window.alert('Your maximum amount of cats accepted was succesfully updated!')
+        })
+        .catch(error => {
+          this.setState({
+            errorDisplay: true,
+            errors: error.response.data.errors.full_messages
+          })
+        })
+    } else {
+      this.setState({
+        errorDisplay: true,
+        errors: ['The field is blank or unchanged!']
+      })
+    }
+  }
+
   render() {
 
     let editDescriptionForm
+    let editMaxCatsForm
     let errorDisplay
 
     if (this.state.errorDisplay) {
@@ -126,6 +175,27 @@ class HostProfile extends Component {
             <div className='button-wrapper'>
               <Button secondary id='description-close-button' onClick={this.descriptionFormHandler}>Close</Button>
               <Button id='description-submit-button' onClick={this.updateDescription}>Change</Button>
+            </div>
+          </Form>
+          {errorDisplay}
+        </>
+      )
+    }
+
+    if (this.state.editMaxCatsForm) {
+      editMaxCatsForm = (
+        <>
+          <Form id='update-maxCats'>
+            <Form.Input
+              required
+              type='number'
+              id='newMaxCats'
+              value={this.state.newMaxCats}
+              onChange={this.onChangeHandler}
+            />
+            <div className='button-wrapper'>
+              <Button secondary id='maxCats-close-button' onClick={this.maxCatsFormHandler}>Close</Button>
+              <Button id='maxCats-submit-button' onClick={this.updateMaxCats}>Change</Button>
             </div>
           </Form>
           {errorDisplay}
@@ -168,10 +238,11 @@ class HostProfile extends Component {
                 <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 236.62 236.62"><path d="M197.023,225.545c-1.145-9.533-11.68-10.614-17.805-9.958c-6.521-24.554,16.225-61.151,17.563-69.82c1.438-9.312-6.658-63.5-7.513-90.938C188.389,26.662,147.48-4.433,140.65,0.524c-6.768,7.484,9.748,17.585,1.054,26.245c-8.398,8.367-10.588,13.99-16.824,23.46c-15.976,24.255,27.318,24.558,27.318,24.558s-33.882,25.112-41.421,37.768c-6.943,11.656-9.854,24.696-18.232,35.688c-19.094,25.051-14.791,68.729-14.791,68.729s-36.17-11.839-16.264-53.133C76.643,132.406,84.107,86.02,50.016,97.95c-13.189,4.616,2.949,14.325,5.734,17.435c9.318,10.4,1.441,27.896-4.174,38.012c-15.037,27.091-20.496,55.475,11.154,72.978c14.063,7.776,33.055,9.7,52.17,9.982l48.64,0.14C179.564,237.294,197.689,234.298,197.023,225.545z" /></svg>
                 &nbsp;Maximum cats: {this.state.maxCats}&nbsp;
               </p>
-              <Header id='change-maxCats-link' className='fake-link-underlined top-bottom-margin-auto' >
+              <Header id='change-maxCats-link' className='fake-link-underlined top-bottom-margin-auto' onClick={this.maxCatsFormHandler} >
                 Change
               </Header>
             </div>
+            {editMaxCatsForm}
             <div className='flexbox-row'>
               <p id='rate' className='top-bottom-margin-auto'>
                 <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M18 6V4H2v2h16zm0 4H2v6h16v-6zM0 4c0-1.1.9-2 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm4 8h4v2H4v-2z" /></svg>
