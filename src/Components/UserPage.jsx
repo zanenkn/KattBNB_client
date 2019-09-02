94506d33eea889b290f67fc806277ad340cef136
raptorf1 <raptor_f1@hotmail.com>
2019-09-02 18:59:36 +0300

    Resets states when forms close
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/UserPage.jsx
+++ src/Components/UserPage.jsx
@@ -61,9 +61,12 @@ class UserPage extends Component {
       host_profile_form: false,
       location: this.props.location,
       errorDisplay: false,
-      password: ''
+      password: '',
+      current_password: '',
+      new_password: '',
+      new_password_confirmation: ''
     })
-//    this.hostProfileElement.current.closeAllForms()
+    //    this.hostProfileElement.current.closeAllForms()
   }
 
   passwordFormHandler = () => {
@@ -73,11 +76,12 @@ class UserPage extends Component {
       location: this.props.location,
       host_profile_form: false,
       errorDisplay: false,
+      password: '',
       current_password: '',
       new_password: '',
       new_password_confirmation: ''
     })
-//    this.hostProfileElement.current.closeAllForms()
+    //    this.hostProfileElement.current.closeAllForms()
   }
 
   hostProfileFormHandler = () => {
@@ -287,7 +291,7 @@ class UserPage extends Component {
       passwordForm = (
         <>
           {errorDisplay}
-          <Form style={{'display': 'table', 'margin': 'auto', 'width': 'min-content'}}>
+          <Form style={{ 'display': 'table', 'margin': 'auto', 'width': 'min-content' }}>
             <Form.Input
               required
               id='current_password'
@@ -320,7 +324,7 @@ class UserPage extends Component {
             </p>
           </Form>
 
-          
+
 
           <div className='button-wrapper'>
             <div>
@@ -384,7 +388,7 @@ class UserPage extends Component {
               <svg fill='grey' height='1em' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'><path d='M13.6 13.47A4.99 4.99 0 0 1 5 10a5 5 0 0 1 8-4V5h2v6.5a1.5 1.5 0 0 0 3 0V10a8 8 0 1 0-4.42 7.16l.9 1.79A10 10 0 1 1 20 10h-.18.17v1.5a3.5 3.5 0 0 1-6.4 1.97zM10 13a3 3 0 1 0 0-6 3 3 0 0 0 0 6z' /></svg>
               &nbsp;{this.props.email}
             </p>
-          
+
             <p id='user-location'>
               <svg fill='grey' height='1em' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'><path d='M10 20S3 10.87 3 7a7 7 0 1 1 14 0c0 3.87-7 13-7 13zm0-11a2 2 0 1 0 0-4 2 2 0 0 0 0 4z' /></svg>
               &nbsp;{this.state.location}&nbsp;
@@ -393,7 +397,7 @@ class UserPage extends Component {
               </Header>
             </p>
             {locationForm}
-  
+
             <p>
               <svg fill='grey' height='1em' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'><path d='M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z' /></svg>
               &nbsp;******&nbsp;
