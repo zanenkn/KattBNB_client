d07df2e69271ee226fbb839aa46d8c91a134ca5c
raptorf1 <raptor_f1@hotmail.com>
2019-09-03 18:46:00 +0300

    Adds address states to all form handles so that address field closes when another form opens and the relevant states get reset. Adds ENTER key listener when searching for address.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/HostProfile.jsx
+++ src/Components/HostProfile.jsx
@@ -37,7 +37,7 @@ class HostProfile extends Component {
       long: '',
       latitude: '',
       longitude: '',
-      user_input_adress: ''
+      user_input_address: ''
     }
   }
 
@@ -74,11 +74,23 @@ class HostProfile extends Component {
   closeAllForms = () => {
     this.setState({
       editDescriptionForm: false,
+      newDescription: '',
       editMaxCatsForm: false,
+      newMaxCats: '',
       editRateForm: false,
+      newRate: '',
       editSupplementForm: false,
+      newSupplement: '',
       editableCalendar: false,
-      addressSearch: false
+      newAvailability: [],
+      addressSearch: false,
+      editAddress: false,
+      newAddress: '',
+      user_input_address: '',
+      lat: '',
+      long: '',
+      latitude: '',
+      longitude: ''
     })
   }
 
@@ -96,11 +108,19 @@ class HostProfile extends Component {
       newRate: '',
       newSupplement: '',
       newAvailability: [],
+      newAddress: '',
       errorDisplay: false,
       editMaxCatsForm: false,
       editRateForm: false,
       editSupplementForm: false,
-      editableCalendar: false
+      editableCalendar: false,
+      addressSearch: false,
+      editAddress: false,
+      user_input_address: '',
+      lat: '',
+      long: '',
+      latitude: '',
+      longitude: ''
     })
     this.props.closeLocPasForms()
   }
@@ -111,11 +131,13 @@ class HostProfile extends Component {
       addressSearch: true,
       newAddress: this.state.full_address,
       user_input_address: this.state.full_address,
+      newDescription: '',
       newMaxCats: '',
       newRate: '',
       newSupplement: '',
       newAvailability: [],
       errorDisplay: false,
+      editDescriptionForm: false,
       editMaxCatsForm: false,
       editRateForm: false,
       editSupplementForm: false,
@@ -132,11 +154,19 @@ class HostProfile extends Component {
       newRate: '',
       newSupplement: '',
       newAvailability: [],
+      newAddress: '',
       errorDisplay: false,
       editDescriptionForm: false,
       editRateForm: false,
       editSupplementForm: false,
-      editableCalendar: false
+      editableCalendar: false,
+      addressSearch: false,
+      editAddress: false,
+      user_input_address: '',
+      lat: '',
+      long: '',
+      latitude: '',
+      longitude: ''
     })
     this.props.closeLocPasForms()
   }
@@ -149,11 +179,19 @@ class HostProfile extends Component {
       newMaxCats: '',
       newSupplement: '',
       newAvailability: [],
+      newAddress: '',
       errorDisplay: false,
       editDescriptionForm: false,
       editMaxCatsForm: false,
       editSupplementForm: false,
-      editableCalendar: false
+      editableCalendar: false,
+      addressSearch: false,
+      editAddress: false,
+      user_input_address: '',
+      lat: '',
+      long: '',
+      latitude: '',
+      longitude: ''
     })
     this.props.closeLocPasForms()
   }
@@ -166,11 +204,19 @@ class HostProfile extends Component {
       newMaxCats: '',
       newRate: '',
       newAvailability: [],
+      newAddress: '',
       errorDisplay: false,
       editDescriptionForm: false,
       editMaxCatsForm: false,
       editRateForm: false,
-      editableCalendar: false
+      editableCalendar: false,
+      addressSearch: false,
+      editAddress: false,
+      user_input_address: '',
+      lat: '',
+      long: '',
+      latitude: '',
+      longitude: ''
     })
     this.props.closeLocPasForms()
   }
@@ -183,11 +229,19 @@ class HostProfile extends Component {
       newMaxCats: '',
       newRate: '',
       newSupplement: '',
+      newAddress: '',
       errorDisplay: false,
       editDescriptionForm: false,
       editMaxCatsForm: false,
       editRateForm: false,
-      editSupplementForm: false
+      editSupplementForm: false,
+      addressSearch: false,
+      editAddress: false,
+      user_input_address: '',
+      lat: '',
+      long: '',
+      latitude: '',
+      longitude: ''
     })
     this.props.closeLocPasForms()
   }
@@ -278,44 +332,6 @@ class HostProfile extends Component {
     if (event.key === "Enter") {
       this.updateMaxCats(event)
     }
-  }
-
-  generateRandomNumberLat = () => {
-    const minValue = parseFloat(process.env.REACT_APP_COORDS_MIN)
-    const maxValue = parseFloat(process.env.REACT_APP_COORDS_MAX)
-    let resultNumber = (Math.random() * (maxValue - minValue) + minValue).toFixed(6)
-    return parseFloat(resultNumber)
-  }
-
-  generateRandomNumberLong = () => {
-    const minValue = parseFloat(process.env.REACT_APP_COORDS_MIN)
-    const maxValue = parseFloat(process.env.REACT_APP_COORDS_MAX)
-    let resultNumber = (Math.random() * (maxValue - minValue) + minValue).toFixed(6)
-    return parseFloat(resultNumber)
-  }
-
-  geolocationDataAddress = () => {
-    Geocode.setApiKey(process.env.REACT_APP_API_KEY_GOOGLE)
-    Geocode.fromAddress(this.state.user_input_address).then(
-      response => {
-        const { lat, lng } = response.results[0].geometry.location;
-        this.setState({
-          latitude: lat,
-          longitude: lng,
-          lat: lat - this.generateRandomNumberLat(),
-          long: lng + this.generateRandomNumberLong(),
-          newAddress: response.results[0].formatted_address,
-          addressSearch: false,
-          addressErrorDisplay: false
-        })
-      },
-      error => {
-        this.setState({
-          addressErrorDisplay: true,
-          addressError: error.message
-        })
-      }
-    )
   }
 
   updateRate = (e) => {
@@ -527,6 +543,50 @@ class HostProfile extends Component {
     }
   }
 
+  listenEnterAddressSearch = (event) => {
+    if (event.key === "Enter") {
+      this.geolocationDataAddress(event)
+    }
+  }
+
+  generateRandomNumberLat = () => {
+    const minValue = parseFloat(process.env.REACT_APP_COORDS_MIN)
+    const maxValue = parseFloat(process.env.REACT_APP_COORDS_MAX)
+    let resultNumber = (Math.random() * (maxValue - minValue) + minValue).toFixed(6)
+    return parseFloat(resultNumber)
+  }
+
+  generateRandomNumberLong = () => {
+    const minValue = parseFloat(process.env.REACT_APP_COORDS_MIN)
+    const maxValue = parseFloat(process.env.REACT_APP_COORDS_MAX)
+    let resultNumber = (Math.random() * (maxValue - minValue) + minValue).toFixed(6)
+    return parseFloat(resultNumber)
+  }
+
+  geolocationDataAddress = () => {
+    Geocode.setApiKey(process.env.REACT_APP_API_KEY_GOOGLE)
+    Geocode.fromAddress(this.state.user_input_address).then(
+      response => {
+        const { lat, lng } = response.results[0].geometry.location;
+        this.setState({
+          latitude: lat,
+          longitude: lng,
+          lat: lat - this.generateRandomNumberLat(),
+          long: lng + this.generateRandomNumberLong(),
+          newAddress: response.results[0].formatted_address,
+          addressSearch: false,
+          addressErrorDisplay: false
+        })
+      },
+      error => {
+        this.setState({
+          addressErrorDisplay: true,
+          addressError: error.message
+        })
+      }
+    )
+  }
+
 
   render() {
 
@@ -543,6 +603,7 @@ class HostProfile extends Component {
     let selectedDays
     let calendar
     const today = new Date()
+    let addressSearch
     let errorDisplay
     const rate = parseFloat(this.state.rate)
     const supplement = parseFloat(this.state.supplement)
@@ -759,8 +820,6 @@ class HostProfile extends Component {
       )
     }
 
-    let addressSearch
-
     if (this.state.editAddress) {
       if (this.state.addressSearch === true) {
         addressSearch = (
@@ -785,7 +844,7 @@ class HostProfile extends Component {
               id='user_input_address'
               value={this.state.user_input_address}
               onChange={this.onChangeHandler}
-              onKeyPress={this.listenEnterKeyAddress}
+              onKeyPress={this.listenEnterAddressSearch}
               iconPosition='right'
               icon={<Icon id='search' name='search' link onClick={this.geolocationDataAddress.bind(this)} style={{ 'color': '#c90c61' }} />}
             />
@@ -853,7 +912,7 @@ class HostProfile extends Component {
           <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z" /></svg>
           &nbsp;{this.state.full_address}&ensp;
             <Header as='strong' id='change-address-link' onClick={this.addressFormHandler} className='fake-link-underlined'>
-            Change
+              Change
             </Header>
         </p>
         {addressSearch}
@@ -862,7 +921,7 @@ class HostProfile extends Component {
           <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 236.62 236.62"><path d="M197.023,225.545c-1.145-9.533-11.68-10.614-17.805-9.958c-6.521-24.554,16.225-61.151,17.563-69.82c1.438-9.312-6.658-63.5-7.513-90.938C188.389,26.662,147.48-4.433,140.65,0.524c-6.768,7.484,9.748,17.585,1.054,26.245c-8.398,8.367-10.588,13.99-16.824,23.46c-15.976,24.255,27.318,24.558,27.318,24.558s-33.882,25.112-41.421,37.768c-6.943,11.656-9.854,24.696-18.232,35.688c-19.094,25.051-14.791,68.729-14.791,68.729s-36.17-11.839-16.264-53.133C76.643,132.406,84.107,86.02,50.016,97.95c-13.189,4.616,2.949,14.325,5.734,17.435c9.318,10.4,1.441,27.896-4.174,38.012c-15.037,27.091-20.496,55.475,11.154,72.978c14.063,7.776,33.055,9.7,52.17,9.982l48.64,0.14C179.564,237.294,197.689,234.298,197.023,225.545z" /></svg>
           &nbsp;Maximum cats: {this.state.maxCats}&ensp;
             <Header as='strong' id='change-maxCats-link' onClick={this.maxCatsFormHandler} className='fake-link-underlined'>
-            Change
+              Change
             </Header>
         </p>
         {editMaxCatsForm}
@@ -880,7 +939,7 @@ class HostProfile extends Component {
           <svg fill='grey' height='1em' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M11 9V5H9v4H5v2h4v4h2v-4h4V9h-4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20z" /></svg>
           &nbsp;Extra {supplement} kr/day per cat&ensp;
             <Header as='strong' id='change-supplement-link' onClick={this.supplementFormHandler} className='fake-link-underlined'>
-            Change
+              Change
             </Header>
         </p>
         {editSupplementForm}
