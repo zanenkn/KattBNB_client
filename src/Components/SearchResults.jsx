e7f494339748109dc415fb7c6b100e62715ba1ec
raptorf1 <raptor_f1@hotmail.com>
2019-09-26 20:33:39 +0300

    Sad path redirection when user tries to visit search-results path manually
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import { Header } from 'semantic-ui-react'
 
 class SearchResults extends Component {
 
-  
+
   render() {
     let searchMessage
 
-    if (this.props.history.location.state.searchData.length === 0) {
+    if (this.props.history.location.state === undefined) {
+      searchMessage = (
+        <Redirect to='/' />
+      )
+    } else if (this.props.history.location.state.searchData.length === 0) {
       searchMessage = (
         <Header>
           Your search did not yield any results! Try changing your search criteria or go to the map to find cat sitters in nearby areas.
