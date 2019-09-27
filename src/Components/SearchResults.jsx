f9d3409ecfc20d9b0d7245d9660bdb7b464648f4
raptorf1 <raptor_f1@hotmail.com>
2019-09-27 18:08:17 +0300

    Refactors sad path redirection with componentDidMount. Creates empty states and populates them accordingly.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -1,29 +1,49 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
 import { Header } from 'semantic-ui-react'
 
 class SearchResults extends Component {
+  state = {
+    checkInDate: '',
+    checkOutDate: '',
+    numberOfCats: '',
+    location: '',
+    searchDataLocation: ''
+  }
+
+  componentDidMount() {
+    if (this.props.history.location.state === undefined) {
+      this.props.history.push({ pathname: '/' })
+    } else {
+      this.setState({
+        checkInDate: this.props.history.location.state.from,
+        checkOutDate: this.props.history.location.state.to,
+        numberOfCats: this.props.history.location.state.cats,
+        location: this.props.history.location.state.location,
+        searchDataLocation: this.props.history.location.state.searchData
+      })
+    }
+  }
 
 
   render() {
-    let searchMessage
+    // let searchMessage
 
-    if (this.props.history.location.state === undefined) {
-      searchMessage = (
-        <Redirect to='/' />
-      )
-    } else if (this.props.history.location.state.searchData.length === 0) {
-      searchMessage = (
-        <Header>
-          Your search did not yield any results! Try changing your search criteria or go to the map to find cat sitters in nearby areas.
-        </Header>
-      )
-    }
+    // if (this.props.history.location.state === undefined) {
+    //   searchMessage = (
+    //     <Redirect to='/' />
+    //   )
+    // } else if (this.props.history.location.state.searchData.length === 0) {
+    //   searchMessage = (
+    //     <Header>
+    //       Your search did not yield any results! Try changing your search criteria or go to the map to find cat sitters in nearby areas.
+    //     </Header>
+    //   )
+    // }
 
 
     return (
       <>
-        {searchMessage}
+        yay!
       </>
     )
   }
