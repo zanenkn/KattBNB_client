07af6d1cd68d82d7c746222dc4a6adeb4681c4eb
raptorf1 <raptor_f1@hotmail.com>
2019-09-27 18:43:07 +0300

    Writes a test for sad path when manually visiting the search-results path
    Co-authored-by: zane <zane.neikena@gmail.com>

--- cypress/integration/VisitorCanSearchForCatSitters.spec.js
+++ cypress/integration/VisitorCanSearchForCatSitters.spec.js
@@ -34,7 +34,7 @@ describe('Visitor can search for cat sitters on landing page', () => {
     cy.get('#cats').type('1')
     cy.get('.ui > #search-form > .required > #location > .default').click()
     cy.get('#search-form > .required > #location > .visible > .item:nth-child(30)').click()
-    cy.get('#search-form > .required > .InputFromTo:nth-child(2) > .DayPickerInput > input').click({force: true})
+    cy.get('#search-form > .required > .InputFromTo:nth-child(2) > .DayPickerInput > input').click({ force: true })
     cy.get('.DayPickerInput-Overlay > .DayPicker > .DayPicker-wrapper > .DayPicker-NavBar > .DayPicker-NavButton--next').click()
     cy.get('.DayPickerInput-Overlay > .DayPicker > .DayPicker-wrapper > .DayPicker-NavBar > .DayPicker-NavButton--next').click()
     cy.get('.DayPickerInput-Overlay > .DayPicker > .DayPicker-wrapper > .DayPicker-NavBar > .DayPicker-NavButton--next').click()
@@ -42,5 +42,10 @@ describe('Visitor can search for cat sitters on landing page', () => {
     cy.get('.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(6) > .DayPicker-Day:nth-child(3)').click()
     cy.get('#search-button').click()
     cy.contains('Your search did not yield any results! Try changing your search criteria or go to the map to find cat sitters in nearby areas.')
+  })
+
+  it('but cannot navigate manually to the search results path, without first filling the search form', () => {
+    cy.visit('http://localhost:3000/search-results')
+    cy.contains('Find a cat sitter!')
   })
 })
