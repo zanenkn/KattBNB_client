3c2316138ea5d4ad5e34f11d735f34349458aff8
zanenkn <zane.neikena@gmail.com>
2019-09-30 17:04:00 +0200

    Creates new component List and refactors the SearchResults component.
    Co-authored-by: george <raptor_f1@hotmail.com>

---
+++ src/Components/List.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import { Container, Header } from 'semantic-ui-react'
+
+class List extends Component {
+  render() {
+    let searchMessage
+
+    if (this.props.searchDataLocation !== '' && this.props.searchDataLocation.length === 0) {
+      searchMessage = (
+        <Header>
+          Your search did not yield any results! Try changing your search criteria or go to the map to find cat sitters in nearby areas.
+        </Header>
+      )
+    }
+    return (
+      <Container style={{'background': '#ECECEC', 'height': '100vh'}}>
+        {searchMessage} 
+      </Container>
+    )
+  }
+}
+
+export default List
