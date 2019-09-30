e5297c91a4604a6c6fa043ee4bc076cb9877ebde
zanenkn <zane.neikena@gmail.com>
2019-09-30 17:35:11 +0200

    Test for viewing search results as a list.
    Co-authored-by: george <raptor_f1@hotmail.com>

---
+++ cypress/integration/VisitorCanViewSearchResultsAsList.spec.js
@@ -0,0 +1,31 @@
+describe('Visitor can view search results', () => {
+  beforeEach(function () {
+    cy.server()
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('as a list', () => {
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3007/api/v1/host_profiles?location=Dorotea',
+      status: 200,
+      response: 'fixture:search_results_list.json'
+    })
+    cy.get('#search-form > .required > .InputFromTo:nth-child(2) > .DayPickerInput > input').click()
+    cy.get('.DayPickerInput-Overlay > .DayPicker > .DayPicker-wrapper > .DayPicker-NavBar > .DayPicker-NavButton--next').click()
+    cy.get('.DayPickerInput-Overlay > .DayPicker > .DayPicker-wrapper > .DayPicker-NavBar > .DayPicker-NavButton--next').click()
+    cy.get('.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(2) > .DayPicker-Day:nth-child(2)').click()
+    cy.get('#search-form > .required > .InputFromTo:nth-child(3) > .DayPickerInput > input').click()
+    cy.get('.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(2) > .DayPicker-Day:nth-child(8)').click()
+    cy.get('.ui > #search-form > .required > #location > .default').click()
+    cy.get('.ui > #search-form > .required > #location > .search').type('Stock')
+    cy.get('#search-form > .required > #location > .visible > .selected').click()
+    cy.get('.ui > #search-form > .required > .ui > #cats').click()
+    cy.get('.ui > #search-form > .required > .ui > #cats').type('2')
+    cy.get('.content-wrapper > .ui > .button-wrapper > div > #search-button').click()
+
+    cy.contains('carla')
+    cy.contains('1120 kr')
+  })
+  
+})
