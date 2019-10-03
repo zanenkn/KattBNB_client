e301bf7ef16d3e6b60a6efe0b4427592bce463ff
raptorf1 <raptor_f1@hotmail.com>
2019-10-03 19:47:31 +0300

    Results indicator in second header is now aligned with location and number of cats
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -71,7 +71,7 @@ class SearchResults extends Component {
     return (
       <>
         <Grid style={{ 'height': '25vh', 'margin': '0', 'paddingLeft': '10vw', 'paddingRight': '10vw', 'paddingBottom': '2rem', 'paddingTop': '2rem', 'position': 'fixed', 'top': '10vh', 'overflow': 'hidden', 'background': 'white', 'width': '100%', 'zIndex': '100', 'boxShadow': '0 0 20px -5px rgba(0,0,0,.2)' }}>
-          <Grid.Row columns='equal' style={{ 'padding': '0', 'marginBottom': '1rem' }}>
+          <Grid.Row columns='equal' style={{ 'padding': '0', 'marginBottom': '1rem', 'alignItems': 'baseline' }}>
             <Grid.Column style={{ 'padding': '0' }}>
               <p style={{ 'color': '#c90c61', 'textAlign': 'left' }}>
                 <svg fill='#c90c61' height='1em' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'><path d='M10 20S3 10.87 3 7a7 7 0 1 1 14 0c0 3.87-7 13-7 13zm0-11a2 2 0 1 0 0-4 2 2 0 0 0 0 4z' /></svg>
