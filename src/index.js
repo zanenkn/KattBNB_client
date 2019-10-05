84f7b60d8432f6892b3a73d74a1c8c9e9b93f62d
raptorf1 <raptor_f1@hotmail.com>
2019-10-05 18:31:47 +0300

    Changes default URLs to production
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import axios from 'axios'
 const development = 'http://localhost:3007'
 const production = process.env.REACT_APP_API_ENDPOINT
 
-axios.defaults.baseURL = development
+axios.defaults.baseURL = production
 
 const store = configureStore()
 verifyCredentials(store)
