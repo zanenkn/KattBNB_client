4438446306622234b94190ecf4967c7f488457a2
zanenkn <zane.neikena@gmail.com>
2019-10-11 19:07:56 +0200

    Styling of the SearchResults - List and Map.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -97,23 +97,27 @@ class SearchResults extends Component {
 
     if (this.state.listResults === true) {
       results = (
-        <List
-          finalAvailableHosts={finalAvailableHosts}
-          numberOfCats={this.state.numberOfCats}
-          checkInDate={this.state.checkInDate}
-          checkOutDate={this.state.checkOutDate}
-        />
+        <Container style={{ 'background': '#ECECEC', 'minHeight': '64vh', 'marginTop': '26vh' }}>
+          <List
+            finalAvailableHosts={finalAvailableHosts}
+            numberOfCats={this.state.numberOfCats}
+            checkInDate={this.state.checkInDate}
+            checkOutDate={this.state.checkOutDate}
+          />
+        </Container>
       )
     } else {
       results = (
-        <GoogleMap
-          numberOfCats={this.state.numberOfCats}
-          checkInDate={this.state.checkInDate}
-          checkOutDate={this.state.checkOutDate}
-          mapCenterLat={this.state.locationLat}
-          mapCenterLong={this.state.locationLong}
-          allAvailableHosts={this.state.allAvailableHosts}
-        />
+        <Container style={{ 'background': '#ECECEC', 'height': '64vh', 'marginTop': '26vh' }}>
+          <GoogleMap
+            numberOfCats={this.state.numberOfCats}
+            checkInDate={this.state.checkInDate}
+            checkOutDate={this.state.checkOutDate}
+            mapCenterLat={this.state.locationLat}
+            mapCenterLong={this.state.locationLong}
+            allAvailableHosts={this.state.allAvailableHosts}
+          />
+        </Container>
       )
     }
 
@@ -124,7 +128,7 @@ class SearchResults extends Component {
           <div style={{ 'width': 'min-content', 'margin': 'auto' }}>
             <p style={{ 'color': '#c90c61', 'textAlign': 'left' }}>
               <svg fill='#c90c61' height='1em' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'><path d='M10 20S3 10.87 3 7a7 7 0 1 1 14 0c0 3.87-7 13-7 13zm0-11a2 2 0 1 0 0-4 2 2 0 0 0 0 4z' /></svg>
-              &nbsp;{this.state.location}&ensp;
+              &nbsp;{this.state.location}&emsp;
               <svg fill='#c90c61' height='1em' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 236.62 236.62'><path d='M197.023,225.545c-1.145-9.533-11.68-10.614-17.805-9.958c-6.521-24.554,16.225-61.151,17.563-69.82c1.438-9.312-6.658-63.5-7.513-90.938C188.389,26.662,147.48-4.433,140.65,0.524c-6.768,7.484,9.748,17.585,1.054,26.245c-8.398,8.367-10.588,13.99-16.824,23.46c-15.976,24.255,27.318,24.558,27.318,24.558s-33.882,25.112-41.421,37.768c-6.943,11.656-9.854,24.696-18.232,35.688c-19.094,25.051-14.791,68.729-14.791,68.729s-36.17-11.839-16.264-53.133C76.643,132.406,84.107,86.02,50.016,97.95c-13.189,4.616,2.949,14.325,5.734,17.435c9.318,10.4,1.441,27.896-4.174,38.012c-15.037,27.091-20.496,55.475,11.154,72.978c14.063,7.776,33.055,9.7,52.17,9.982l48.64,0.14C179.564,237.294,197.689,234.298,197.023,225.545z' /></svg>
               &nbsp;{this.state.numberOfCats}
             </p>
@@ -160,10 +164,7 @@ class SearchResults extends Component {
             </Grid>
           </div>
         </div>
-
-        <Container style={{ 'background': '#ECECEC', 'height': '64vh', 'marginTop': '26vh' }}>
-          {results}
-        </Container>
+        {results}
       </>
     )
   }
