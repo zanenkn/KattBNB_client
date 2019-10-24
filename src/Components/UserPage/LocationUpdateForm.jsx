d512d62dfb002fd09ce19c6943f7c62349996012
raptorf1 <raptor_f1@hotmail.com>
2019-10-24 18:30:42 +0300

    Creates empty component to house location update form
    Co-authored-by: zane <zane.neikena@gmail.com>

---
+++ src/Components/UserPage/LocationUpdateForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class LocationUpdateForm extends Component {
+
+  state = {
+
+  }
+
+  render() {
+
+    return (
+      <>
+      </>
+    )
+  }
+}
+
+export default LocationUpdateForm
