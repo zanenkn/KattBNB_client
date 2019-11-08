699e545ead76dc649b1626ab732782bcef8ebbbd
raptorf1 <raptor_f1@hotmail.com>
2019-11-08 20:52:14 +0200

    Imports axios to make a POST request to create a booking. Handles errors.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/RequestToBook.jsx
+++ src/Components/RequestToBook.jsx
@@ -1,23 +1,88 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Header, Form } from 'semantic-ui-react'
+import { Header, Form, Button, Message } from 'semantic-ui-react'
 import moment from 'moment'
+import axios from 'axios'
 
 class RequestToBook extends Component {
   state = {
-    message: ''
+    message: '',
+    loading: false,
+    errorDisplay: false,
+    errors: ''
   }
 
   onChangeHandler = (e) => {
     this.setState({ [e.target.id]: e.target.value })
   }
 
+  createBooking = (e) => {
+    e.preventDefault()
+    this.setState({ loading: true })
+    if (this.state.message === '') {
+      this.setState({
+        loading: false,
+        errors: ['Writting a message to the host is obligatory!'],
+        errorDisplay: true
+      })
+    } else {
+      let booking = []
+      let startDate = this.props.location.state.checkInDate
+      let stopDate = this.props.location.state.checkOutDate
+      let currentDate = startDate
+      while (currentDate <= stopDate) {
+        booking.push(currentDate)
+        currentDate = currentDate + 86400000
+      }
+      const path = '/api/v1/bookings'
+      const payload = {
+        number_of_cats: this.props.location.state.numberOfCats,
+        message: this.state.message,
+        dates: booking,
+        host_nickname: this.props.location.state.nickname,
+        user_id: this.props.id
+      }
+      const headers = {
+        uid: window.localStorage.getItem('uid'),
+        client: window.localStorage.getItem('client'),
+        'access-token': window.localStorage.getItem('access-token')
+      }
+      axios.post(path, payload, { headers: headers })
+        .then(() => {
+          this.setState({ errorDisplay: false })
+          window.alert('You have successfully created a booking!')
+          //     setTimeout(function () { window.location.replace('/user-page') }, 500)
+        })
+        .catch(error => {
+          this.setState({
+            loading: false,
+            errors: error.response.data.error,
+            errorDisplay: true
+          })
+        })
+    }
+  }
+
 
   render() {
     let checkIn = moment(this.props.location.state.checkInDate).format('l')
     let checkOut = moment(this.props.location.state.checkOutDate).format('l')
-    
-    return(
+    let errorDisplay
+
+    if (this.state.errorDisplay) {
+      errorDisplay = (
+        <Message negative >
+          <Message.Header>Booking could not be requested because of following error(s):</Message.Header>
+          <ul>
+            {this.state.errors.map(error => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        </Message>
+      )
+    }
+
+    return (
       <>
         <Header>
           Request to book
@@ -36,13 +101,18 @@ class RequestToBook extends Component {
             onChange={this.onChangeHandler}
           />
         </Form>
+        {errorDisplay}
+        <Button
+          style={{ 'marginTop': '0' }}
+          onClick={this.createBooking}
+        >
+          Request to book
+      </Button>
       </>
     )
   }
 }
 
-const mapStateToProps = state => ({
-  currentUserIn: state.reduxTokenAuth.currentUser.isSignedIn
-})
+const mapStateToProps = state => ({ id: state.reduxTokenAuth.currentUser.attributes.id })
 
 export default connect(mapStateToProps)(RequestToBook)
