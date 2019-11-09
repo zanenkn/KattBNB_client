592a857cacbf9719836a3bfa1d1028f1579d14c6
raptorf1 <raptor_f1@hotmail.com>
2019-11-09 21:01:00 +0200

    Succesfully redirects to landing page when visitor navigates manualy to successful-request path without following the proper flow. Had to convert to a class component. Sorry Zane.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/SuccessfulRequest.jsx
+++ src/Components/SuccessfulRequest.jsx
@@ -1,27 +1,48 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { Header, Segment } from 'semantic-ui-react'
 import moment from 'moment'
 
-const SuccessfulRequest = (props) => {
-  let checkIn = moment(props.location.state.checkInDate).format('l')
-  let checkOut = moment(props.location.state.checkOutDate).format('l')
+class SuccessfulRequest extends Component {
 
-  return (
-    <div className='content-wrapper' >
-      <Header as='h1'>
-        Success!
-      </Header>
-      <Segment className='whitebox' style={{ 'textAlign': 'center' }}>
-        <p>
-          You have successfully requested a booking for <strong style={{ 'color': '#c90c61' }}>{props.location.state.numberOfCats} {props.location.state.numberOfCats > 1 ? 'cats' : 'cat'}</strong> with <strong style={{ 'color': '#c90c61' }}>{props.location.state.nickname}</strong> during the dates of <strong style={{ 'color': '#c90c61' }}>{checkIn}</strong> until <strong style={{ 'color': '#c90c61' }}>{checkOut}</strong>.
-        </p>
-        <p>
-          <strong style={{ 'color': '#c90c61' }}>{props.location.state.nickname}</strong> now has 3 days to accept or decline your request. We will let you know by email. Questions? Check out <Header as={Link} to='faq' className='fake-link-underlined'>our FAQ</Header>.
-        </p>
-      </Segment>
-    </div>
-  )
+  state = {
+    checkIn: '',
+    checkOut: '',
+    numberOfCats: '',
+    nickname: ''
+  }
+
+  componentDidMount() {
+    if (this.props.history.location.state === undefined) {
+      this.props.history.push({ pathname: '/' })
+    } else {
+      this.setState({
+        checkIn: moment(this.props.location.state.checkInDate).format('l'),
+        checkOut: moment(this.props.location.state.checkOutDate).format('l'),
+        numberOfCats: this.props.location.state.numberOfCats,
+        nickname: this.props.location.state.nickname
+      })
+    }
+  }
+
+  render() {
+
+    return (
+      <div className='content-wrapper' >
+        <Header as='h1'>
+          Success!
+        </Header>
+        <Segment className='whitebox' style={{ 'textAlign': 'center' }}>
+          <p>
+            You have successfully requested a booking for <strong style={{ 'color': '#c90c61' }}>{this.state.numberOfCats} {this.state.numberOfCats > 1 ? 'cats' : 'cat'}</strong> with <strong style={{ 'color': '#c90c61' }}>{this.state.nickname}</strong> during the dates of <strong style={{ 'color': '#c90c61' }}>{this.state.checkIn}</strong> until <strong style={{ 'color': '#c90c61' }}>{this.state.checkOut}</strong>.
+          </p>
+          <p>
+            <strong style={{ 'color': '#c90c61' }}>{this.state.nickname}</strong> now has 3 days to accept or decline your request. We will let you know by email. Questions? Check out <Header as={Link} to='faq' className='fake-link-underlined'>our FAQ</Header>.
+          </p>
+        </Segment>
+      </div>
+    )
+  }
 }
 
 export default SuccessfulRequest
