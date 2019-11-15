8140d0ee10d547e196dd95964a8e626dfce17aae
raptorf1 <raptor_f1@hotmail.com>
2019-11-15 19:59:53 +0200

    Connects AllBookings component with redux store. Performs an axios call when component mounts to fetch all outgoing bookings of logged in user.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Bookings/AllBookings.jsx
+++ src/Components/Bookings/AllBookings.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import { connect } from 'react-redux'
 
 class AllBookings extends Component {
+
+  state = {
+    outgoingBookings: []
+  }
+
+  componentDidMount() {
+    const path = `/api/v1/bookings?user_id=${this.props.id}`
+    const headers = {
+      uid: window.localStorage.getItem('uid'),
+      client: window.localStorage.getItem('client'),
+      'access-token': window.localStorage.getItem('access-token')
+    }
+    axios.get(path, { headers: headers }).then(response => {
+      this.setState({ outgoingBookings: response.data })
+    })
+  }
+
   render() {
-    return(
-      'yay'
+    return (
+      <>
+        {this.state.outgoingBookings.length}
+      </>
     )
   }
 }
 
-export default AllBookings
+const mapStateToProps = state => ({
+  username: state.reduxTokenAuth.currentUser.attributes.username,
+  id: state.reduxTokenAuth.currentUser.attributes.id
+})
+
+export default connect(mapStateToProps)(AllBookings)
