9e89c3fb8c0b4298092c359ef4d526a61f770382
zanenkn <zane.neikena@gmail.com>
2019-11-16 22:26:11 +0100

    Adjusts the NoAccess component to render a link to the Login.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/ReusableComponents/NoAccess.jsx
+++ src/Components/ReusableComponents/NoAccess.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { Container, Icon } from 'semantic-ui-react'
+import { Header, Segment, Icon } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 const NoAccess = () => {
   return (
-    <Container
-      className='content-wrapper'
-      textAlign='center'>
-      <Icon
-        name='stop circle'
-        size='massive'
-        color='red' />
-      <h1> You cannot access this page or the page does not exist! </h1>
-    </Container>
+    <div className='content-wrapper' >
+      <Header as='h1'>
+        Hello!
+      </Header>
+      <Segment className='whitebox' style={{ 'textAlign': 'center' }}>
+        <p>Don't be a stranger. Please <Header as={Link} to='login' className='fake-link' >log in</Header> to view this section!</p>
+      </Segment>
+    </div>
   )
 }
 
