ea10249dcca7514891bdd0e083ac8c517f1b7dd2
zanenkn <zane.neikena@gmail.com>
2019-11-20 18:59:28 +0100

    Refactors the OutgoingBookings into a class component. Implements three buttons for viewing different categories.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Bookings/OutgoingBookings.jsx
+++ src/Components/Bookings/OutgoingBookings.jsx
@@ -1,39 +1,49 @@
-import React from 'react'
-import { Header } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Header, Button } from 'semantic-ui-react'
 import OutgoingRequests from './OutgoingRequests'
 import OutgoingUpcoming from './OutgoingUpcoming'
 import OutgoingHistory from './OutgoingHistory'
 
-const OutgoingBookings = (props) => {
-
-  return (
-    <div className='expanding-wrapper'>
-      <Header as='h1' style={{ 'marginBottom': '0' }}>
-        Outgoing bookings
-      </Header>
-      <p style={{ 'textAlign': 'center' }}>
-        You booking your cat(s) to stay with hosts.
-      </p>
-      <Header as='h2' style={{ 'marginBottom': '0' }}>
-        Requests
-      </Header>
-      <OutgoingRequests
-        requests={props.location.state.outgoingRequests}
-      />
-      <Header as='h2' style={{ 'marginBottom': '0' }}>
-        Upcoming
-      </Header>
-      <OutgoingUpcoming
-        upcoming={props.location.state.outgoingUpcoming}
-      />
-      <Header as='h2' style={{ 'marginBottom': '0' }}>
-        History
-      </Header>
-      <OutgoingHistory
-        history={props.location.state.outgoingHistory}
-      />
-    </div>
-  )
+class OutgoingBookings extends Component {
+  render() {
+    return (
+      <div className='expanding-wrapper'>
+        <Header as='h1' style={{ 'marginBottom': '0' }}>
+          Outgoing bookings
+        </Header>
+        <p style={{ 'textAlign': 'center' }}>
+          You booking your cat(s) to stay with hosts.
+        </p>
+        <Button>
+          View Requests
+        </Button>
+        <Button>
+          View Upcoming
+        </Button>
+        <Button>
+          View History
+        </Button>
+        <Header as='h2' style={{ 'marginBottom': '0' }}>
+          Requests
+        </Header>
+        <OutgoingRequests
+          requests={this.props.location.state.outgoingRequests}
+        />
+        <Header as='h2' style={{ 'marginBottom': '0' }}>
+          Upcoming
+        </Header>
+        <OutgoingUpcoming
+          upcoming={this.props.location.state.outgoingUpcoming}
+        />
+        <Header as='h2' style={{ 'marginBottom': '0' }}>
+          History
+        </Header>
+        <OutgoingHistory
+          history={this.props.location.state.outgoingHistory}
+        />
+      </div>
+    )
+  }
 }
 
 export default OutgoingBookings
