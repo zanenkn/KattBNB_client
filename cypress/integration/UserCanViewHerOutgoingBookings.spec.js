47b98ea015e5965b00525c153ef8480f92cd2e80
raptorf1 <raptor_f1@hotmail.com>
2019-11-21 19:03:27 +0200

    Moves unused div to wrap each booking. Adjusts relevant test in order for first() and last() functions operate as supposed to. All tests passing.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- cypress/integration/UserCanViewHerOutgoingBookings.spec.js
+++ cypress/integration/UserCanViewHerOutgoingBookings.spec.js
@@ -25,19 +25,19 @@ describe('User can view her outgoing bookings', () => {
     const now = new Date(2019, 11, 20).getTime()
     cy.clock(now)
     cy.get('#view-outgoing-bookings').click()
-    cy.get('#outgoing-upcoming').first().contains('You have successfully booked a stay with Accepted1 for your 1 cat for the dates of 2019-11-26 until 2019-11-30.')
-    cy.get('#outgoing-upcoming').last().contains('You have successfully booked a stay with Accepted2 for your 1 cat for the dates of 2019-11-25 until 2019-11-29.')
+    cy.get('[data-cy=outgoing-upcoming]').first().contains('You have successfully booked a stay with Accepted1 for your 1 cat for the dates of 2019-11-26 until 2019-11-30.')
+    cy.get('[data-cy=outgoing-upcoming]').last().contains('You have successfully booked a stay with Accepted2 for your 1 cat for the dates of 2019-11-25 until 2019-11-29.')
   })
 
   it('and see her requested bookings displayed in correct chronological order', () => {
     cy.get('#view-outgoing-bookings').click()
-    cy.get('#outgoing-requests').first().contains('You have requested to book a stay with Pending1 for your 1 cat during the dates of 2019-11-26 until 2019-11-27.')
-    cy.get('#outgoing-requests').last().contains('You have requested to book a stay with Pending2 for your 1 cat during the dates of 2019-11-26 until 2019-11-27.')
+    cy.get('[data-cy=outgoing-requests]').first().contains('You have requested to book a stay with Pending1 for your 1 cat during the dates of 2019-11-26 until 2019-11-27.')
+    cy.get('[data-cy=outgoing-requests]').last().contains('You have requested to book a stay with Pending2 for your 1 cat during the dates of 2019-11-26 until 2019-11-27.')
   })
 
   it('and see her bookings history displayed in correct chronological order', () => {
     cy.get('#view-outgoing-bookings').click()
-    cy.get('#outgoing-history').first().contains('Your cat(s) stayed with AcceptedOfThePast during the dates of 2019-11-26 until 2019-11-19.')
-    cy.get('#outgoing-history').last().contains('Your request to book a stay with Canceled1 for your 1 cat during the dates of 2019-11-25 until 2019-11-30 got canceled.')
+    cy.get('[data-cy=outgoing-history]').first().contains('Your cat(s) stayed with AcceptedOfThePast during the dates of 2019-11-26 until 2019-11-19.')
+    cy.get('[data-cy=outgoing-history]').last().contains('Your request to book a stay with Canceled1 for your 1 cat during the dates of 2019-11-25 until 2019-11-30 got canceled.')
   })
 })
--- src/Components/Bookings/OutgoingRequests.jsx
+++ src/Components/Bookings/OutgoingRequests.jsx
@@ -17,20 +17,18 @@ const OutgoingRequests = (props) => {
         <p style={{ 'textAlign': 'center' }}>
           These are requests to book you have made awaiting confirmation from host(s).
         </p>
-        <div id='outgoing-requests'>
-          {sortedRequests.map(request => {
-            return (
-              <Container style={{ 'backgroundColor': '#e8e8e8', 'marginBottom': '3rem', 'padding': '2rem' }} id={request.id} key={request.id}>
-                <p className='small-centered-paragraph'>
-                  You have requested to book a stay with <strong>{request.host_nickname}</strong> for your <strong>{request.number_of_cats} {request.number_of_cats > 1 ? 'cats' : 'cat'}</strong> during the dates of <strong>{moment(request.dates[0]).format('YYYY-MM-DD')}</strong> until <strong>{moment(request.dates[request.dates.length - 1]).format('YYYY-MM-DD')}</strong>.
-                </p>
-                <p className='small-centered-paragraph'>
-                  You will receive a reply before <strong>{moment(request.created_at).add(3, 'days').format('YYYY-MM-DD')}</strong>.
-                </p>
-              </Container>
-            )
-          })}
-        </div>
+        {sortedRequests.map(request => {
+          return (
+            <Container style={{ 'backgroundColor': '#e8e8e8', 'marginBottom': '3rem', 'padding': '2rem' }} id={request.id} data-cy='outgoing-requests' key={request.id}>
+              <p className='small-centered-paragraph'>
+                You have requested to book a stay with <strong>{request.host_nickname}</strong> for your <strong>{request.number_of_cats} {request.number_of_cats > 1 ? 'cats' : 'cat'}</strong> during the dates of <strong>{moment(request.dates[0]).format('YYYY-MM-DD')}</strong> until <strong>{moment(request.dates[request.dates.length - 1]).format('YYYY-MM-DD')}</strong>.
+              </p>
+              <p className='small-centered-paragraph'>
+                You will receive a reply before <strong>{moment(request.created_at).add(3, 'days').format('YYYY-MM-DD')}</strong>.
+              </p>
+            </Container>
+          )
+        })}
       </>
     )
   } else {
