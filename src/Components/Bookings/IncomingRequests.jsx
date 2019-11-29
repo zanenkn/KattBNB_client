c83c7a240c91e4646e900c2a97df6baf78ae103b
zanenkn <zane.neikena@gmail.com>
2019-11-29 20:19:03 +0100

    Styling for the incoming booking requests.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Bookings/IncomingRequests.jsx
+++ src/Components/Bookings/IncomingRequests.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import moment from 'moment'
-import { Container } from 'semantic-ui-react'
+import { Segment, Header, Grid, Icon } from 'semantic-ui-react'
 
 const IncomingRequests = (props) => {
   let sortedRequests = props.requests
@@ -19,14 +19,31 @@ const IncomingRequests = (props) => {
         </p>
         {sortedRequests.map(request => {
           return (
-            <Container style={{ 'backgroundColor': '#e8e8e8', 'marginBottom': '3rem', 'padding': '2rem' }} id={request.id} data-cy='incoming-requests' key={request.id}>
+            <Segment className='whitebox'>
+            
+                <Grid className='topbox'>
+                  <Grid.Row style={{'alignItems': 'center'}} >
+                  <Grid.Column width={8}>
+                      <Header as='h2' style={{ 'color': 'white', 'marginBottom': '0', 'textAlign': 'left' }}>{request.price_total} kr</Header>
+                    </Grid.Column>
+                    <Grid.Column width={8}>
+                      <Icon name='plus circle' style={{'color': '#ffffff', 'opacity': '0.6', 'transform': 'rotate(45deg)', 'float': 'right'}} size='big'/>
+                      <Icon name='check circle' style={{'color': '#ffffff', 'float': 'right'}} size='big'/>
+                    </Grid.Column>
+                  </Grid.Row>
+                  <div>
+                    <p style={{'color': '#ffffff', 'fontSize': 'small', 'marginBottom': '1rem', 'marginTop': '-0.5rem'}}>
+                      You must reply before <strong>{moment(request.created_at).add(3, 'days').format('YYYY-MM-DD')}</strong>.
+                    </p>
+                  </div>
+                </Grid>
               <p className='small-centered-paragraph'>
-                <strong>{request.user.nickname}</strong> wants to book a stay for their <strong>{request.number_of_cats} {request.number_of_cats > 1 ? 'cats' : 'cat'}</strong> during the dates of <strong>{moment(request.dates[0]).format('YYYY-MM-DD')}</strong> until <strong>{moment(request.dates[request.dates.length - 1]).format('YYYY-MM-DD')}</strong>.
+                <strong style={{ 'color': '#c90c61' }}>{request.user.nickname}</strong> wants to book a stay for their <strong style={{ 'color': '#c90c61' }}>{request.number_of_cats} {request.number_of_cats > 1 ? 'cats' : 'cat'}</strong> during the dates of <strong style={{ 'color': '#c90c61' }}>{moment(request.dates[0]).format('YYYY-MM-DD')}</strong> until <strong style={{ 'color': '#c90c61' }}>{moment(request.dates[request.dates.length - 1]).format('YYYY-MM-DD')}</strong>.
               </p>
-              <p className='small-centered-paragraph'>
-                You must reply before <strong>{moment(request.created_at).add(3, 'days').format('YYYY-MM-DD')}</strong>.
+              <p className='fake-link-underlined'>
+                View message
               </p>
-            </Container>
+            </Segment>
           )
         })}
       </>
