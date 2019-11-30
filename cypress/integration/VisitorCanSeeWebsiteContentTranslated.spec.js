9155b7429b61d447563f4da269586624892234e1
clalil <cliljander@gmail.com>
2019-11-30 14:32:21 +0100

    Refactor test and module Test passing.

--- cypress/integration/VisitorCanSeeWebsiteContentTranslated.spec.js
+++ cypress/integration/VisitorCanSeeWebsiteContentTranslated.spec.js
@@ -1,18 +1,22 @@
 import stubLanguages from '../support/stubLanguages'
 
 describe('User can view the website in their browser detected language', () => {
-  beforeEach(() => {
-    cy.server(),
-    cy.visit('http://localhost:3000')
-  })
 
   it('successfully shows swedish UI', () => {
-    stubLanguages(['sv', 'sv-SE'])
+    cy.server(),
+    cy.visit(
+      "http://localhost:3000",
+      stubLanguages(['sv', 'sv-SE'])
+    )
     cy.get('h1').should('contain', 'KattBNB kommer snart!')
   })
 
-  xit('successfully shows english UI', () => {
-    stubLanguages(['en', 'en-GB'])
+  it('successfully shows english UI', () => {
+    cy.server(),
+    cy.visit(
+      "http://localhost:3000",
+      stubLanguages(['en', 'en-GB'])
+    )
     cy.get('h1').should('contain', 'KattBNB is coming soon!')
   })
 })
--- cypress/support/stubLanguages.js
+++ cypress/support/stubLanguages.js
@@ -3,13 +3,6 @@ const stubLanguages = language => {
     onBeforeLoad(win) { 
       Object.defineProperty(
         win.navigator, 
-        'language', { 
-          value: language[0]
-        }
-      )
-
-      Object.defineProperty(
-        win.navigator, 
         "languages", {
           value: [language[0], language[1]]
         }
