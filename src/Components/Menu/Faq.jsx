cc3f1ac07e44e3495234098d7b27551279289b64
zanenkn <zane.neikena@gmail.com>
2019-11-30 16:23:16 +0100

    Refactoring class components to stateless and implementing the translations.

--- src/Components/Menu/Faq.jsx
+++ src/Components/Menu/Faq.jsx
@@ -1,55 +1,54 @@
-  
-import React, { Component } from 'react'
+import React from 'react'
 import { Sidebar, Header } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 
-class Faq extends Component {
-  render() {
-    return (
-      <Sidebar.Pushable className='content-wrapper' >
-        <Header as='h1'>
-          FAQ
-        </Header>
-        
-        <Header as='h3' style={{'textAlign': 'left'}} >
-          What is KattBNB?
-        </Header>
+const Faq = () =>  {
+  const { t } = useTranslation()
+  return (
+    <Sidebar.Pushable className='content-wrapper' >
+      <Header as='h1'>
+        {t('menu.faq')}
+      </Header>
+      
+      <Header as='h3' style={{'textAlign': 'left'}} >
+        {t('faq.header1')}
+      </Header>
 
-        <p>
-          Our idea of KattBNB is a platform that lets cat sitters offer their services and cat owners book a sitter that is most convenient to them. Think that you could check your cat in with someone you can trust as easy you check yourself in a hotel! That’s what we are building. A full range service that allows you to not only find the right person to look after your cat but also handles the booking, payment, staying in touch and all the other stuff. So you don’t have to worry while you are away.
-        </p>
+      <p>
+        {t('faq.p1')}
+      </p>
 
-        <Header as='h3' style={{'textAlign': 'left'}} >
-          When is KattBNB going live?
-        </Header>
+      <Header as='h3' style={{'textAlign': 'left'}} >
+        {t('faq.header2')}
+      </Header>
 
-        <p>
-          We are planning and working hard towards a release on spring 2020. 
-        </p>
+      <p>
+        {t('faq.p2')} 
+      </p>
 
-        <Header as='h3' style={{'textAlign': 'left'}} >
-          Who we are?
-        </Header>
+      <Header as='h3' style={{'textAlign': 'left'}} >
+        {t('faq.header3')}
+      </Header>
 
-        <p>
-          We are couple of friends passionate about cats and coding. We believe in solving problems that matter and having fun while at it. More <Header  as={Link} to='about-us' className='fake-link-underlined'>here</Header>. 
-        </p>
+      <p>
+        We are couple of friends passionate about cats and coding. We believe in solving problems that matter and having fun while at it. More <Header  as={Link} to='about-us' className='fake-link-underlined'>here</Header>. 
+      </p>
 
-        <Header as='h3' style={{'textAlign': 'left'}} >
-          I want to contribute!
-        </Header>
+      <Header as='h3' style={{'textAlign': 'left'}} >
+        {t('faq.header4')}
+      </Header>
 
-        <p>
-          Good! If you have an idea how to improve our code, you are welcome to open a pull request <a href='https://github.com/zanenkn/KattBNB_client' target='_blank' rel='noopener noreferrer' style={{'textDecoration': 'underline'}}>on GitHub</a>. 
-        </p>
+      <p>
+        Good! If you have an idea how to improve our code, you are welcome to open a pull request <a href='https://github.com/zanenkn/KattBNB_client' target='_blank' rel='noopener noreferrer' style={{'textDecoration': 'underline'}}>on GitHub</a>. 
+      </p>
 
-        <p>
-          For any other suggestions and feedback, please use <Header  as={Link} to='contact-us' className='fake-link-underlined'>this form</Header> to get in touch with us. 
-        </p>
+      <p>
+        For any other suggestions and feedback, please use <Header  as={Link} to='contact-us' className='fake-link-underlined'>this form</Header> to get in touch with us. 
+      </p>
 
-      </Sidebar.Pushable>
-    )
-  }
+    </Sidebar.Pushable>
+  )
 }
 
 export default Faq
