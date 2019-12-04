34576640a285fe7366597203dd6bc1437bcb284b
zanenkn <zane.neikena@gmail.com>
2019-12-04 21:08:59 +0100

    Styling, implementation of onChangeHandler and buttons in DeclineRequestPopup.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Bookings/DeclineRequestPopup.jsx
+++ src/Components/Bookings/DeclineRequestPopup.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
-import { Header, Form } from 'semantic-ui-react'
+import { Header, Form, Button } from 'semantic-ui-react'
 
 class DeclineRequestPopup extends Component {
+  state = {
+    message: ''
+  }
+
+  onChangeHandler = (e) => {
+    this.setState({ [e.target.id]: e.target.value })
+  }
+
   render() {
     return (
       <>
@@ -9,18 +17,23 @@ class DeclineRequestPopup extends Component {
           Decline a request
         </Header>
         <p className='small-centered-paragraph'>
-          You are about to decline a booking request from <strong>{this.props.nickname}</strong> for the dates of <strong>{this.props.startDate}</strong> until <strong>{this.props.endDate}</strong>
+          You are about to decline a booking request from <strong style={{ 'color': '#c90c61' }}>{this.props.nickname}</strong> for the dates of <strong style={{ 'color': '#c90c61' }}>{this.props.startDate}</strong> until <strong style={{ 'color': '#c90c61' }}>{this.props.endDate}</strong>
         </p>
         <Form>
           <Form.TextArea
+            style={{ 'minHeight': '150px' }}
             label='Message'
             placeholder='Let them know why..'
             required
             id='message'
-            //value={this.state.message}
-            //onChange={this.onChangeHandler}
+            value={this.state.message}
+            onChange={this.onChangeHandler}
           />
         </Form>
+        <div className='button-wrapper'>
+          <Button secondary>Back</Button>
+          <Button>Decline</Button>
+        </div>
       </>
     )
   }
