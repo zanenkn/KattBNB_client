aeb523cccd689a0c214722ee7acb12bfaa1cd7f2
raptorf1 <raptor_f1@hotmail.com>
2019-12-05 19:38:46 +0200

    Sad path tests for unsuccessful booking decline. All 3 examples passing.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- cypress/integration/UserCanDeclineHerBookingRequests.spec.js
+++ cypress/integration/UserCanDeclineHerBookingRequests.spec.js
@@ -34,4 +34,17 @@ describe('User can decline her booking request', () => {
     cy.contains('Hi, GeorgeTheGreek!')
     cy.contains('Here you can manage your bookings.')
   })
+
+  it('unsuccessfully cause she enters no message', () => {
+    cy.get('#decline').click()
+    cy.get('#decline-button').click()
+    cy.contains("Message can't be blank or more than 200 characters!")
+  })
+
+  it('successfully cause the message is more than 200 characters', () => {
+    cy.get('#decline').click()
+    cy.get('#message').type('I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!I decline!!')
+    cy.get('#decline-button').click()
+    cy.contains("Message can't be blank or more than 200 characters!")
+  })
 })
