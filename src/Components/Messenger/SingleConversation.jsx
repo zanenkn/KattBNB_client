47539c62ed12ebf76c06155364eb0a4d837cb2f1
zanenkn <zane.neikena@gmail.com>
2019-12-25 17:01:17 +0100

    axios call to get all the messages of the conversation.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Messenger/SingleConversation.jsx
+++ src/Components/Messenger/SingleConversation.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Conversation extends Component {
+  state = {
+    messages: ''
+  }
+
+  componentDidMount() {
+    const headers = {
+      uid: window.localStorage.getItem('uid'),
+      client: window.localStorage.getItem('client'),
+      'access-token': window.localStorage.getItem('access-token')
+    }
+    const path = `/api/v1/conversations/${this.props.location.state.id}`
+    axios.get(path, { headers: headers })
+      .then(response => {
+        const sortedResponse = response.data.message.sort(function (a, b) {
+          let dateA = new Date(a.created_at), dateB = new Date(b.created_at)
+          return dateA - dateB
+        })
+        this.setState({ messages: sortedResponse })
+      })
+  }
+
   render() {
     return(
       <>
-      {this.props.location.state.id}
+        hej
       </>
     )
   }
