f9ad6eaf3142f1455af8a817cb168a709ce4fc2a
raptorf1 <raptor_f1@hotmail.com>
2019-12-27 12:22:56 +0200

    Sad path tests for creating a message
    Co-authored-by: zane <zane.neikena@gmail.com>

--- cypress/integration/UserCanViewMessagesOfIndividualConversation.spec.js
+++ cypress/integration/UserCanViewMessagesOfIndividualConversation.spec.js
@@ -68,4 +68,55 @@ describe('User can see messages of individual conversation', () => {
     cy.wait(1000)
     cy.get('[data-cy=all-messages-individual-conversation]').last().contains('another test')
   })
+
+  it('and not create a new message cause she enters more than 1000 characters', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3007/api/v1/conversations?user_id=1',
+      status: 200,
+      response: 'fixture:all_user_conversations.json'
+    })
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3007/api/v1/conversations/1',
+      status: 200,
+      response: 'fixture:user_messages.json'
+    })
+    cy.login('fixture:successful_login.json', 'george@mail.com', 'password', 200)
+    cy.wait(2000)
+    cy.get('#navlinks').within(() => {
+      cy.get('#messenger-icon').click()
+    })
+    cy.get('#1').click()
+    cy.get('#newMessage').type('another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!another test!anothe')
+    cy.contains('Remaining characters:')
+    cy.get('#send').click()
+    cy.contains('The message cannot be empty or exceed 1000 characters!')
+  })
+
+  it('and not create a new message cause she enters no message', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3007/api/v1/conversations?user_id=1',
+      status: 200,
+      response: 'fixture:all_user_conversations.json'
+    })
+    cy.route({
+      method: 'GET',
+      url: 'http://localhost:3007/api/v1/conversations/1',
+      status: 200,
+      response: 'fixture:user_messages.json'
+    })
+    cy.login('fixture:successful_login.json', 'george@mail.com', 'password', 200)
+    cy.wait(2000)
+    cy.get('#navlinks').within(() => {
+      cy.get('#messenger-icon').click()
+    })
+    cy.get('#1').click()
+    cy.get('#send').should('not.be.visible')
+    cy.get('#newMessage').type('{enter}')
+    cy.contains('The message cannot be empty or exceed 1000 characters!')
+  })
 })
