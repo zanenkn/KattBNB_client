500706acdb012ae6c1da14e82b1a53019eaa6f73
Joel Öhman <j.ohman@hotmail.com>
2020-01-06 18:40:44 +0100

    Extracts SignUp section from translation.json as a seperate namespace and adds namespace SignUp to config

--- src/i18n.js
+++ src/i18n.js
@@ -12,7 +12,7 @@ i18n
 
   .init({
     fallbackLng,
-    ns: ['translation', 'AboutUs', 'SearchResults', 'Search', 'Faq', 'WorkInProgress', 'ContactUs', 'ChangePassword', 'Login', 'PasswordReset', 'PasswordResetSuccess'],
+    ns: ['translation', 'AboutUs', 'SearchResults', 'Search', 'Faq', 'WorkInProgress', 'ContactUs', 'ChangePassword', 'Login', 'PasswordReset', 'PasswordResetSuccess', 'SignUp'],
     lng: 'en',
     debug: true,
     whitelist: availableLanguages,
