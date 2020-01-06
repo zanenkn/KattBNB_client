48442296b24f442b0fffc438737f40ec2b117cf8
Joel Öhman <j.ohman@hotmail.com>
2020-01-06 18:27:53 +0100

    Extracts Login section from translation.json as a seperate namespace and adds namespace Login to config

--- src/i18n.js
+++ src/i18n.js
@@ -12,7 +12,7 @@ i18n
 
   .init({
     fallbackLng,
-    ns: ['translation', 'AboutUs', 'SearchResults', 'Search', 'Faq', 'WorkInProgress', 'ContactUs', 'ChangePassword'],
+    ns: ['translation', 'AboutUs', 'SearchResults', 'Search', 'Faq', 'WorkInProgress', 'ContactUs', 'ChangePassword', 'Login'],
     lng: 'en',
     debug: true,
     whitelist: availableLanguages,
