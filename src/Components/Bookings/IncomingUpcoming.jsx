2be2de3d257b1ecff2ffff7d4b0025a3dbfff711
raptorf1 <raptor_f1@hotmail.com>
2020-01-06 17:12:54 +0200

    Turns IncomingUpcoming into a class component. Connects it to redux. Applies withRouter. Implements function to create conversation.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Bookings/IncomingUpcoming.jsx
+++ src/Components/Bookings/IncomingUpcoming.jsx
@@ -1,47 +1,96 @@
-import React from 'react'
+import React, { Component } from 'react'
 import moment from 'moment'
 import { Container } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
-const IncomingUpcoming = (props) => {
-  let sortedUpcoming = props.upcoming
-  sortedUpcoming.sort((a, b) => (b.dates[0] - a.dates[0]))
+class IncomingUpcoming extends Component {
 
-  if (props.upcoming.length > 0) {
+  messageUser = (e, userId, userAvatar, userLocation, userNickname) => {
+    e.preventDefault()
+    const path = '/api/v1/conversations'
+    const payload = {
+      user1_id: this.props.id,
+      user2_id: userId
+    }
+    const headers = {
+      uid: window.localStorage.getItem('uid'),
+      client: window.localStorage.getItem('client'),
+      'access-token': window.localStorage.getItem('access-token')
+    }
+    axios.post(path, payload, { headers: headers })
+      .then(response => {
+        this.props.history.push({
+          pathname: '/conversation',
+          state: {
+            id: response.data.id,
+            user: {
+              avatar: userAvatar,
+              id: userId,
+              location: userLocation,
+              nickname: userNickname
+            }
+          }
+        })
+      })
+      .catch(error => {
+        this.setState({ errors: error.response.data.errors.full_messages })
+      })
+  }
+
+  render() {
+
+    let sortedUpcoming = this.props.upcoming
+    sortedUpcoming.sort((a, b) => (b.dates[0] - a.dates[0]))
+    let page
+
+    if (this.props.upcoming.length > 0) {
+      page = (
+        <>
+          <p className='small-centered-paragraph'>
+            <strong>
+              You have {this.props.upcoming.length} upcoming {this.props.upcoming.length > 1 ? 'bookings' : 'booking'}.
+            </strong>
+          </p>
+          <p style={{ 'textAlign': 'center' }}>
+            These are finalized bookings coming up soon. Get in touch with these cat owners to organize the drop-off and pick-up.
+          </p>
+          {sortedUpcoming.map(upcoming => {
+            return (
+              <Container style={{ 'backgroundColor': '#e8e8e8', 'marginTop': '2rem', 'padding': '2rem' }} id={upcoming.id} data-cy='incoming-upcoming' key={upcoming.id}>
+                <p className='small-centered-paragraph'>
+                  You have approved a stay for <strong>{upcoming.user.nickname}'s</strong> <strong>{upcoming.number_of_cats} {upcoming.number_of_cats > 1 ? 'cats' : 'cat'}</strong> for the dates of <strong>{moment(upcoming.dates[0]).format('YYYY-MM-DD')}</strong> until <strong>{moment(upcoming.dates[upcoming.dates.length - 1]).format('YYYY-MM-DD')}</strong>.
+                </p>
+                <p className='fake-link-underlined' onClick={(e) => this.messageUser(e, upcoming.user_id, upcoming.user.avatar, upcoming.user.location, upcoming.user.nickname)}>
+                  Message {upcoming.user.nickname}
+                </p>
+              </Container>
+            )
+          })}
+        </>
+      )
+    } else {
+      page = (
+        <>
+          <p className='small-centered-paragraph'>
+            <strong>
+              You don't have any upcoming bookings.
+          </strong>
+          </p>
+        </>
+      )
+    }
+
     return (
       <>
-        <p className='small-centered-paragraph'>
-          <strong>
-            You have {props.upcoming.length} upcoming {props.upcoming.length > 1 ? 'bookings' : 'booking'}.
-          </strong>
-        </p>
-        <p style={{ 'textAlign': 'center' }}>
-          These are finalized bookings coming up soon. Get in touch with these cat owners to organize the drop-off and pick-up.
-        </p>
-        {sortedUpcoming.map(upcoming => {
-          return (
-            <Container style={{ 'backgroundColor': '#e8e8e8', 'marginTop': '2rem', 'padding': '2rem' }} id={upcoming.id} data-cy='incoming-upcoming' key={upcoming.id}>
-              <p className='small-centered-paragraph'>
-                You have approved a stay for <strong>{upcoming.user.nickname}'s</strong> <strong>{upcoming.number_of_cats} {upcoming.number_of_cats > 1 ? 'cats' : 'cat'}</strong> for the dates of <strong>{moment(upcoming.dates[0]).format('YYYY-MM-DD')}</strong> until <strong>{moment(upcoming.dates[upcoming.dates.length - 1]).format('YYYY-MM-DD')}</strong>.
-              </p>
-              <p className='fake-link-underlined'>
-                Message {upcoming.user.nickname}
-              </p>
-            </Container>
-          )
-        })}
-      </>
-    )
-  } else {
-    return (
-      <>
-        <p className='small-centered-paragraph'>
-          <strong>
-            You don't have any upcoming bookings.
-          </strong>
-        </p>
+        {page}
       </>
     )
   }
 }
 
-export default IncomingUpcoming
+
+const mapStateToProps = state => ({ id: state.reduxTokenAuth.currentUser.attributes.id })
+
+export default withRouter(connect(mapStateToProps)(IncomingUpcoming))
