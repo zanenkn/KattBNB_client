80b1661538fa77c7b8c84be22312357ceb2fddf4
raptorf1 <raptor_f1@hotmail.com>
2020-01-09 18:09:54 +0200

    Creates Spinner component and changes its color to our default color
    Co-authored-by: zane <zane.neikena@gmail.com>

---
+++ src/Components/ReusableComponents/Spinner.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Spinner = () => {
+
+  return (
+    <div className='content-wrapper' >
+      <div class='lds-spinner'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Spinner
