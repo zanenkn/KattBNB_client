16bb75f0031674336db8231055cae4b28ed60795
zanenkn <zane.neikena@gmail.com>
2020-01-17 08:32:08 +0100

    Implementation of smoothscroll-polyfill.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ import * as serviceWorker from './serviceWorker'
 import { verifyCredentials } from './reduxTokenAuthConfig'
 import axios from 'axios'
 import './i18n'
+import smoothscroll from 'smoothscroll-polyfill';
+
 
 axios.defaults.baseURL = (process.env.NODE_ENV === 'development' ? 'http://localhost:3007' : process.env.REACT_APP_API_ENDPOINT)
 
 const store = configureStore()
 verifyCredentials(store)
+smoothscroll.polyfill()
 
 ReactDOM.render(
   <Provider store={store}>
