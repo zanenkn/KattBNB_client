df02d9f4a884932cbf92830066a1f53d446d4314
raptorf1 <raptor_f1@hotmail.com>
2020-01-23 20:30:34 +0200

    Implements Spinner component in HostProfileViewWrapper component while fetching host profile data. Runs all tests. All passing.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/HostProfileView/HostProfileViewWrapper.jsx
+++ src/Components/HostProfileView/HostProfileViewWrapper.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import HostProfileView from './HostProfileView'
+import Spinner from '../ReusableComponents/Spinner'
 
 class HostProfileViewWrapper extends Component {
 
   state = {
     hostProfile: [],
     lat: null,
-    long: null
+    long: null,
+    loading: true
   }
 
   async componentDidMount() {
@@ -16,40 +18,49 @@ class HostProfileViewWrapper extends Component {
       this.setState({
         hostProfile: response.data[0],
         lat: response.data[0].lat,
-        long: response.data[0].long
+        long: response.data[0].long,
+        loading: false
       })
+    } else {
+      this.setState({ loading: false })
     }
   }
 
   render() {
     let hostProfileView
 
-    if (this.state.lat !== null && this.state.long !== null) {
+    if (this.state.loading) {
       hostProfileView = (
-        <div style={{ 'height': '100%' }}>
-          <HostProfileView
-            numberOfCats={0}
-            hostId={this.props.location.state.userId}
-            avatar={this.props.location.state.avatar}
-            nickname={this.props.location.state.nickname}
-            location={this.props.location.state.location}
-            rate={parseFloat(this.state.hostProfile.price_per_day_1_cat)}
-            supplement={parseFloat(this.state.hostProfile.supplement_price_per_cat_per_day)}
-            description={this.state.hostProfile.description}
-            lat={this.state.lat}
-            long={this.state.long}
-            noMessage={this.props.location.state.noMessage}
-          />
-        </div>
+        <Spinner />
       )
     } else {
-      hostProfileView = (
-        <div className='content-wrapper' >
-          <p style={{ 'textAlign': 'center', 'fontStyle': 'italic' }}>
-            This user has no host profile.
-          </p>
-        </div>
-      )
+      if (this.state.lat !== null && this.state.long !== null) {
+        hostProfileView = (
+          <div style={{ 'height': '100%' }}>
+            <HostProfileView
+              numberOfCats={0}
+              hostId={this.props.location.state.userId}
+              avatar={this.props.location.state.avatar}
+              nickname={this.props.location.state.nickname}
+              location={this.props.location.state.location}
+              rate={parseFloat(this.state.hostProfile.price_per_day_1_cat)}
+              supplement={parseFloat(this.state.hostProfile.supplement_price_per_cat_per_day)}
+              description={this.state.hostProfile.description}
+              lat={this.state.lat}
+              long={this.state.long}
+              noMessage={this.props.location.state.noMessage}
+            />
+          </div>
+        )
+      } else {
+        hostProfileView = (
+          <div className='content-wrapper' >
+            <p style={{ 'textAlign': 'center', 'fontStyle': 'italic' }}>
+              This user has no host profile.
+            </p>
+          </div>
+        )
+      }
     }
 
     return (
