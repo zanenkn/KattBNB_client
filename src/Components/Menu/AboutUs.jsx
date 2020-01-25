e76c234821f48ed7922647ced0002d8f636c83ff
zanenkn <zane.neikena@gmail.com>
2020-01-25 13:07:04 +0100

    Stateless components refactored for translations.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Menu/AboutUs.jsx
+++ src/Components/Menu/AboutUs.jsx
@@ -2,82 +2,84 @@ import React from 'react'
 import { Header, Image, Divider } from 'semantic-ui-react'
 import { useTranslation, Trans } from 'react-i18next'
 
-const AboutUs = () => {
-  const { t } = useTranslation()
-  return (
-    <div className='content-wrapper' >
-      <Header as='h1'>
-        {t('reusable:title.about')}
-      </Header>
-      <Header as='h2' >
-        {t('AboutUs:idea-title')}
-      </Header>
-      <p>
-        {t('AboutUs:idea-p1')}
-      </p>
-      <p>
-        {t('AboutUs:idea-p2')}
-      </p>
-      <p>
-        {t('AboutUs:idea-p3')}
-      </p>
-      <Divider hidden />
-      <Divider hidden />
-      <Header as='h2'>
-        {t('AboutUs:behind-scenes-title')}
-      </Header>
-      <div>
-        <Image style={{ 'margin': 'auto' }} src='zane.png' size='small'></Image>
+function AboutUs() {
+  const { t, ready } = useTranslation('AboutUs', { useSuspense: false })
+  if(ready){
+    return(
+      <div className='content-wrapper' >
+        <Header as='h1'>
+          {t('reusable:title.about')}
+        </Header>
+        <Header as='h2' >
+          {t('AboutUs:idea-title')}
+        </Header>
+        <p>
+          {t('AboutUs:idea-p1')}
+        </p>
+        <p>
+          {t('AboutUs:idea-p2')}
+        </p>
+        <p>
+          {t('AboutUs:idea-p3')}
+        </p>
+        <Divider hidden />
+        <Divider hidden />
+        <Header as='h2'>
+          {t('AboutUs:behind-scenes-title')}
+        </Header>
+        <div>
+          <Image style={{ 'margin': 'auto' }} src='zane.png' size='small'></Image>
+        </div>
+        <Header style={{ 'marginBottom': 0 }}>
+          <a href='https://www.linkedin.com/in/zane-neikena' target='_blank' rel='noopener noreferrer'>Zane Neikena</a>
+        </Header>
+        <Header as='h4' style={{ 'marginTop': 0 }}>
+          {t('AboutUs:zane-title')}
+        </Header>
+        <p style={{ 'textAlign': 'center' }}>
+          {t('AboutUs:zane-txt')}
+        </p>
+        <Divider hidden />
+        <Divider hidden />
+        <div>
+          <Image style={{ 'margin': 'auto' }} src='george.png' size='small'></Image>
+        </div>
+        <Header style={{ 'marginBottom': 0 }}>
+          <a href='https://www.linkedin.com/in/george-tomaras-05833730/' target='_blank' rel='noopener noreferrer'>George Tomaras</a>
+        </Header>
+        <Header as='h4' style={{ 'marginTop': 0 }}>
+          {t('AboutUs:george-title')}
+        </Header>
+        <p style={{ 'textAlign': 'center' }}>
+          {t('AboutUs:george-txt')}
+        </p>
+        <Divider hidden />
+        <Divider hidden />
+        <div>
+          <Image style={{ 'margin': 'auto' }} src='felix.png' size='small'></Image>
+        </div>
+        <Header style={{ 'marginBottom': 0 }}>
+          <a href='https://www.linkedin.com/in/felix-bonnier-90b4561/' target='_blank' rel='noopener noreferrer'>Felix Bonnier</a>
+        </Header>
+        <Header as='h4' style={{ 'marginTop': 0 }}>
+          {t('AboutUs:felix-title')}
+        </Header>
+        <p style={{ 'textAlign': 'center' }}>
+          {t('AboutUs:felix-txt')}
+        </p>
+        <Divider hidden />
+        <Divider hidden />
+        <Header as='h2'>
+          {t('AboutUs:acknowledgements-title')}
+        </Header>
+        <p>
+          <Trans i18nKey='AboutUs:acknowledgements-txt'>
+            None of this would be possible without <a href='https://craftacademy.se/english/' target="_blank" rel='noopener noreferrer'>Craft Academy</a>. These people taught us to code and all things beyond that.
+          </Trans>
+        </p>
       </div>
-      <Header style={{ 'marginBottom': 0 }}>
-        <a href='https://www.linkedin.com/in/zane-neikena' target='_blank' rel='noopener noreferrer'>Zane Neikena</a>
-      </Header>
-      <Header as='h4' style={{ 'marginTop': 0 }}>
-        {t('AboutUs:zane-title')}
-      </Header>
-      <p style={{ 'textAlign': 'center' }}>
-        {t('AboutUs:zane-txt')}
-      </p>
-      <Divider hidden />
-      <Divider hidden />
-      <div>
-        <Image style={{ 'margin': 'auto' }} src='george.png' size='small'></Image>
-      </div>
-      <Header style={{ 'marginBottom': 0 }}>
-        <a href='https://www.linkedin.com/in/george-tomaras-05833730/' target='_blank' rel='noopener noreferrer'>George Tomaras</a>
-      </Header>
-      <Header as='h4' style={{ 'marginTop': 0 }}>
-        {t('AboutUs:george-title')}
-      </Header>
-      <p style={{ 'textAlign': 'center' }}>
-        {t('AboutUs:george-txt')}
-      </p>
-      <Divider hidden />
-      <Divider hidden />
-      <div>
-        <Image style={{ 'margin': 'auto' }} src='felix.png' size='small'></Image>
-      </div>
-      <Header style={{ 'marginBottom': 0 }}>
-        <a href='https://www.linkedin.com/in/felix-bonnier-90b4561/' target='_blank' rel='noopener noreferrer'>Felix Bonnier</a>
-      </Header>
-      <Header as='h4' style={{ 'marginTop': 0 }}>
-        {t('AboutUs:felix-title')}
-      </Header>
-      <p style={{ 'textAlign': 'center' }}>
-        {t('AboutUs:felix-txt')}
-      </p>
-      <Divider hidden />
-      <Divider hidden />
-      <Header as='h2'>
-        {t('AboutUs:acknowledgements-title')}
-      </Header>
-      <p>
-        <Trans i18nKey='AboutUs:acknowledgements-txt'>
-          None of this would be possible without <a href='https://craftacademy.se/english/' target="_blank" rel='noopener noreferrer'>Craft Academy</a>. These people taught us to code and all things beyond that.
-        </Trans>
-      </p>
-    </div>
-  )
+    )
+  } else {return null}
 }
 
 export default AboutUs
--- src/Components/ReusableComponents/WorkInProgress.jsx
+++ src/Components/ReusableComponents/WorkInProgress.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Header, Segment, Icon, Container } from 'semantic-ui-react'
 import { useTranslation } from 'react-i18next'
-import i18n from '../../i18n'
 
 const WIP = () => {
-  const { t } = useTranslation()
-  i18n.loadNamespaces(['WorkInProgress'])
-  return (
-    <Segment className='whitebox' >
-      <Header as='h4'>
-        <Container padded>
-          <Icon loading name='cog' style={{ 'color': '#80808069', 'fontSize': '3rem', 'marginBottom': '1rem' }} />
-        </Container>
-        <Container text padded>
-          {t('WorkInProgress:message')}
-        </Container>
-      </Header>
-    </Segment>
-  )
+  const { t, ready } = useTranslation('Faq', { useSuspense: false })
+  if(ready) {
+    return (
+      <Segment className='whitebox' >
+        <Header as='h4'>
+          <Container padded>
+            <Icon loading name='cog' style={{ 'color': '#80808069', 'fontSize': '3rem', 'marginBottom': '1rem' }} />
+          </Container>
+          <Container text padded>
+            {t('WorkInProgress:message')}
+          </Container>
+        </Header>
+      </Segment>
+    )
+  } else {return null}
 }
 
 export default WIP
