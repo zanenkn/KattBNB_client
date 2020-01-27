9d66869dc865866f30640f4b023b3a985012b8dd
raptorf1 <raptor_f1@hotmail.com>
2020-01-27 16:55:21 +0200

    After checkbox toggle implementation for Terms & Conditions, writes another test and adjusts the rest of the SignUp component.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- cypress/integration/VisitorCanSignup.spec.js
+++ cypress/integration/VisitorCanSignup.spec.js
@@ -34,6 +34,7 @@ describe('Visitor can sign up', () => {
     cy.get('#location').click()
     cy.get('.visible > .selected > .text').click()
 
+    cy.get('.fitted > label').click()
     cy.get('#sign-up-button').click()
     cy.visit('http://localhost:3000/signup-success')
     cy.contains('Successful signup!')
@@ -58,51 +59,31 @@ describe('Visitor can sign up', () => {
       })
     })
 
+    cy.get('.fitted > label').click()
     cy.get('#sign-up-button').click()
 
     cy.contains("You didn't input the captcha phrase correctly, please try again!")
   })
 
+  it('and gets a disabled sign up button if she does not accept Terms & Conditions', () => {
+    cy.get('#hamburger').within(() => {
+      cy.get('.icon').click()
+    })
+    cy.get('#signup').click()
+    cy.get('#signup-form').within(() => {
 
-  // Comment out previous test block for the below block to pass
+      let text = [
+        ['#email', 'zane@mail'],
+        ['#password', 'pass'],
+        ['#passwordConfirmation', 'pass'],
+        ['#nickname', 'KittenPrincess']
+      ]
 
-  // it('and gets error message if email is not valid and/or passwords do not match', () => {
-  //   cy.route({
-  //     method: 'POST',
-  //     url: 'http://localhost:3007/api/v1/auth',
-  //     status: 422,
-  //     response: 'fixture:unsuccessful_signup.json',
-  //   })
+      text.forEach(element => {
+        cy.get(element[0]).type(element[1])
+      })
+    })
 
-  //   cy.get('#hamburger').within(() => {
-  //     cy.get('.icon').click()
-  //   })
-  //   cy.get('#signup').click()
-  //   cy.get('#signup-form').within(() => {
-
-  //     let text = [
-  //       ['#email', 'zane@mail'],
-  //       ['#password', 'pass'],
-  //       ['#passwordConfirmation', 'passw'],
-  //       ['#nickname', 'KittenPrincess']
-  //     ]
-
-  //     text.forEach(element => {
-  //       cy.get(element[0]).type(element[1])
-  //     })
-  //   })
-
-  //   cy.get('#sign-up-button').click()
-
-  //   let text = [
-  //     "Password confirmation doesn't match Password",
-  //     'Password is too short (minimum is 6 characters)',
-  //     'Email is not an email',
-  //     "Location can't be blank",
-  //   ]
-
-  //   text.forEach(error => {
-  //     cy.contains(error)
-  //   })
-  // })
+    cy.get('#sign-up-button').should('have.class', 'disabled')
+  })
 })
