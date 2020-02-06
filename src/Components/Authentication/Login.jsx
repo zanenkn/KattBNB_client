b1d375aadf7037e48a3e36d51d535454c2b9f356
zanenkn <zane.neikena@gmail.com>
2020-02-06 14:51:09 +0100

    Refactors Login with hooks

--- src/Components/Authentication/Login.jsx
+++ src/Components/Authentication/Login.jsx
@@ -1,139 +1,106 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Header, Segment, Form, Message, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { signInUser } from '../../reduxTokenAuthConfig'
 import { Link } from 'react-router-dom'
-import { withTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import Spinner from '../ReusableComponents/Spinner'
 
-class Login extends Component {
+const Login = (props) => {
+  const { t, ready } = useTranslation('Login')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [errorDisplay, setErrorDisplay] = useState(false)
+  const [successDisplay, setSuccessDisplay] = useState(false)
+  const [errors, setErrors] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  state = {
-    email: '',
-    password: '',
-    errors: '',
-    errorDisplay: false,
-    successDisplay: false,
-    loading: false
-  }
-
-  onChangeHandler = (e) => {
-    this.setState({ [e.target.id]: e.target.value })
-  }
-
-  logInUser = (e) => {
-    this.setState({ loading: true })
-    e.preventDefault()
-    const { history, signInUser } = this.props
-    const {
-      email,
-      password
-    } = this.state
+  const logInUser = () => {
+    setLoading(true)
+    const { history, signInUser } = props
     signInUser({ email, password })
       .then(() => {
-        this.setState({
-          successDisplay: true,
-          errorDisplay: false
-        })
+        setSuccessDisplay(true)
+        setErrorDisplay(false)
         if (history.length <= 2) {
           history.push('/')
         } else {
           history.go(-1)
         }
       }).catch(error => {
-        this.setState({
-          errorDisplay: true,
-          errors: error.response.data.errors[0],
-          loading: false
-        })
+        setErrorDisplay(true)
+        setErrors(error.response.data.errors[0])
+        setLoading(false)
       })
   }
 
-  listenEnterKey = (event) => {
+  const listenEnterKey = (event) => {
     if (event.key === 'Enter') {
-      this.logInUser(event)
+      logInUser()
     }
   }
 
-  render() {
-    const { t } = this.props
-    let errorDisplay, successDisplay, notRegisteredLinks, forgotPassword
-
-    if (this.props.tReady) {
-      if (this.state.errorDisplay) {
-        errorDisplay = (
-          <Message negative style={{ 'textAlign': 'center' }} >
-            {this.state.errors}
-          </Message>
-        )
-      }
-
-      if (this.state.successDisplay) {
-        successDisplay = (
-          <Message success style={{ 'textAlign': 'center' }} >
-            {t('Login:success-msg')}
-          </Message>
-        )
-
-      } else {
-        notRegisteredLinks = (
-          <p style={{ 'textAlign': 'center', 'marginTop': '2rem' }}>
-            {t('Login:no-acc')}
-            <br></br>
-            <Header as={Link} to='sign-up' className='fake-link'>
-              {t('Login:signup-link')}
-            </Header>
-          </p>
-        )
-
-        forgotPassword = (
-          <div style={{ 'textAlign': 'right' }}>
-            <Header id='password-reset-link' as={Link} to='password-reset' className='fake-link-underlined' >
-              {t('Login:forgot-link')}
-            </Header>
-          </div>
-        )
-      }
-      return (
-        <div className='content-wrapper' >
-          <Header as='h1'>
+  if (ready) {
+    return (
+      <div className='content-wrapper' >
+        <Header as='h1'>
+          {t('Login:title')}
+        </Header>
+        <Segment className='whitebox'>
+          <Form id='login-form'>
+            <Form.Input
+              required
+              id='email'
+              label={t('reusable:plch.email')}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder={t('reusable:plch.email')}
+              onKeyPress={e => listenEnterKey(e)}
+            />
+            <Form.Input
+              required
+              id='password'
+              type='password'
+              label={t('reusable:plch.password')}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder={t('reusable:plch.password')}
+              onKeyPress={e => listenEnterKey(e)}
+            />
+            {successDisplay === false ?
+              <div style={{ 'textAlign': 'right' }}>
+                <Header id='password-reset-link' as={Link} to='password-reset' className='fake-link-underlined' >
+                  {t('Login:forgot-link')}
+                </Header>
+              </div>
+              : () => { }}
+          </Form>
+          {errorDisplay ?
+            <Message negative style={{ 'textAlign': 'center' }} >
+              {errors}
+            </Message>
+            : () => { }}
+          {successDisplay ?
+            <Message success style={{ 'textAlign': 'center' }} >
+              {t('Login:success-msg')}
+            </Message>
+            : () => { }}
+          <Button className='submit-button' id='log-in-button' loading={loading} onClick={() => logInUser()}>
             {t('Login:title')}
-          </Header>
-          <Segment className='whitebox'>
-            <Form id='login-form'>
-              <Form.Input
-                required
-                id='email'
-                label={t('reusable:plch.email')}
-                value={this.state.email}
-                onChange={this.onChangeHandler}
-                placeholder={t('reusable:plch.email')}
-                onKeyPress={this.listenEnterKey}
-              />
-              <Form.Input
-                required
-                id='password'
-                type='password'
-                label={t('reusable:plch.password')}
-                value={this.state.password}
-                onChange={this.onChangeHandler}
-                placeholder={t('reusable:plch.password')}
-                onKeyPress={this.listenEnterKey}
-              />
-              {forgotPassword}
-            </Form>
-            {errorDisplay}
-            {successDisplay}
-            <Button className='submit-button' id='log-in-button' loading={this.state.loading ? true : false} onClick={this.logInUser}>
-              {t('Login:title')}
-            </Button>
-            {notRegisteredLinks}
-          </Segment>
-        </div>
-      )
-    } else { return <Spinner /> }
-
-  }
+          </Button>
+          {successDisplay === false ?
+            <p style={{ 'textAlign': 'center', 'marginTop': '2rem' }}>
+              {t('Login:no-acc')}
+              <br></br>
+              <Header as={Link} to='sign-up' className='fake-link'>
+                {t('Login:signup-link')}
+              </Header>
+            </p>
+            : () => { }}
+        </Segment>
+      </div>
+    )
+  } else { return <Spinner /> }
 }
 
-export default withTranslation('Login')(connect(null, { signInUser })(Login))
+export default connect(null, { signInUser })(Login)
