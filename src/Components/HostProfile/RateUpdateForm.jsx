8a47bacce209b0b864bd4042fee26c71e23b8750
zanenkn <zane.neikena@gmail.com>
2020-02-06 11:13:55 +0100

    RateUpdateForm refactored with hooks.

--- src/Components/HostProfile/RateUpdateForm.jsx
+++ src/Components/HostProfile/RateUpdateForm.jsx
@@ -1,103 +1,82 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import { Divider, Form, Button, Message } from 'semantic-ui-react'
 
-class RateUpdateForm extends Component {
+const RateUpdateForm = (props) => {
 
-  state = {
-    errorDisplay: false,
-    errors: '',
-    loading: false,
-    newRate: this.props.rate
-  }
+  const [errorDisplay, setErrorDisplay] = useState(false)
+  const [errors, setErrors] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [newRate, setNewRate] = useState(props.rate)
 
-  onChangeHandler = (e) => {
-    this.setState({ [e.target.id]: e.target.value })
-  }
-
-  updateRate = (e) => {
-    e.preventDefault()
-    this.setState({ loading: true })
-    if (this.state.newRate !== '' && this.state.newRate !== this.props.rate && this.state.newRate >= 0.01) {
-      const path = `/api/v1/host_profiles/${this.props.id}`
+  const updateRate = () => {
+    setLoading(true)
+    if (newRate !== '' && newRate !== props.rate && newRate >= 0.01) {
+      const path = `/api/v1/host_profiles/${props.id}`
       const headers = {
         uid: window.localStorage.getItem('uid'),
         client: window.localStorage.getItem('client'),
         'access-token': window.localStorage.getItem('access-token')
       }
-      const payload = { price_per_day_1_cat: this.state.newRate }
+      const payload = { price_per_day_1_cat: newRate }
       axios.patch(path, payload, { headers: headers })
         .then(() => {
-          this.setState({
-            loading: false,
-            errorDisplay: false
-          })
           window.alert('Your daily rate for 1 cat was succesfully updated!')
           window.location.reload()
+          setLoading(false)
+          setErrorDisplay(false)
         })
         .catch(error => {
-          this.setState({
-            loading: false,
-            errorDisplay: true,
-            errors: error.response.data.errors.full_messages
-          })
+          setLoading(false)
+          setErrorDisplay(true)
+          setErrors(error.response.data.errors.full_messages)
         })
     } else {
-      this.setState({
-        loading: false,
-        errorDisplay: true,
-        errors: ['The field is blank, unchanged or the number is invalid!']
-      })
+      setLoading(false)
+      setErrorDisplay(true)
+      setErrors(['The field is blank, unchanged or the number is invalid!'])
     }
   }
 
-  listenEnterRateUpdate = (event) => {
+  const listenEnterRateUpdate = (event) => {
     if (event.key === 'Enter') {
-      this.updateRate(event)
+      updateRate()
     }
   }
 
-  render() {
-    let errorDisplay
-
-    if (this.state.errorDisplay) {
-      errorDisplay = (
+  return (
+    <>
+      <Divider />
+      <p className='small-centered-paragraph'>
+        Enter how much you would like to get paid per day when hosting 1 cat.
+        </p>
+      <Form id='update-rate' style={{ 'margin': 'auto', 'maxWidth': '194px' }}>
+        <Form.Input
+          required
+          type='number'
+          id='newRate'
+          value={newRate}
+          onChange={e => setNewRate(e.target.value)}
+          onKeyPress={e => listenEnterRateUpdate(e)}
+        />
+      </Form>
+      {errorDisplay ?
         <Message negative >
           <Message.Header style={{ 'textAlign': 'center' }} >Update action could not be completed because of following error(s):</Message.Header>
           <ul id='message-error-list'>
-            {this.state.errors.map(error => (
+            {errors.map(error => (
               <li key={error}>{error}</li>
             ))}
           </ul>
         </Message>
-      )
-    }
-
-    return (
-      <>
-        <Divider />
-        <p className='small-centered-paragraph'>
-          Enter how much you would like to get paid per day when hosting 1 cat.
-        </p>
-        <Form id='update-rate' style={{ 'margin': 'auto', 'maxWidth': '194px' }}>
-          <Form.Input
-            required
-            type='number'
-            id='newRate'
-            value={this.state.newRate}
-            onChange={this.onChangeHandler}
-            onKeyPress={this.listenEnterRateUpdate}
-          />
-        </Form>
-        {errorDisplay}
-        <div className='button-wrapper'>
-          <Button secondary id='rate-close-button' className='cancel-button' onClick={this.props.closeAllForms}>Close</Button>
-          <Button id='rate-submit-button' className='submit-button' loading={this.state.loading ? true : false} onClick={this.updateRate}>Save</Button>
-        </div>
-        <Divider style={{ 'marginBottom': '2rem' }} />
-      </>
-    )
-  }
+        : () => { }}
+      <div className='button-wrapper'>
+        <Button secondary id='rate-close-button' className='cancel-button' onClick={props.closeAllForms}>Close</Button>
+        <Button id='rate-submit-button' className='submit-button' loading={loading} onClick={() => updateRate()}>Save</Button>
+      </div>
+      <Divider style={{ 'marginBottom': '2rem' }} />
+    </>
+  )
 }
 
 export default RateUpdateForm
