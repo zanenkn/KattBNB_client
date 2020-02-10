685d17330a2e7a93f47f389e8e6708b7ee84524d
raptorf1 <raptor_f1@hotmail.com>
2020-02-10 20:47:26 +0200

    Imports useState and first states. Imports action buttons and checkbox. Imports translations.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/UserPage/NotificationsUpdateForm.jsx
+++ src/Components/UserPage/NotificationsUpdateForm.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Checkbox, Divider, Button } from 'semantic-ui-react'
+import { useTranslation } from 'react-i18next'
 
-const NotificationsUpdateForm = () => {
+const NotificationsUpdateForm = (props) => {
+
+  const { t, ready } = useTranslation()
+  const [loading, setLoading] = useState(false)
+
+  const updateMessageNotification = () => {
+
+  }
+
   return (
-    'yay'
+    <>
+      <div style={{ 'display': 'inline-flex', 'paddingTop': '1em' }}>
+        <Checkbox toggle />
+        <label style={{ 'paddingLeft': '1.3em' }}>Receive notifications for every message</label>
+      </div>
+      <div className='button-wrapper'>
+        <Button secondary className='cancel-button' onClick={() => props.closeLocationAndPasswordForms()}>{t('reusable:cta.close')}</Button>
+        <Button id='location-submit-button' className='submit-button' loading={loading} onClick={() => updateMessageNotification()}>{t('reusable:cta.change')}</Button>
+      </div>
+    </>
   )
 }
 
