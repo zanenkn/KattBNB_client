b447dd0938cee824abcc0c933b2d9835b13bfb5f
zanenkn <zane.neikena@gmail.com>
2020-02-15 21:24:52 +0100

    Hamburger menu animation package attempt.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,32 +2,25 @@ import React from 'react'
 import { Grid, Image, Container } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { HamburgerSqueeze } from 'react-animated-burgers'
 
 const Navbar = (props) => {
   let noAvatar = `https://ui-avatars.com/api/?name=${props.username}&size=150&length=3&font-size=0.3&rounded=true&background=d8d8d8&color=c90c61&uppercase=false`
-  let hamburgerIcon
 
-  if (props.menuVisible) {
-    hamburgerIcon = (
-      <Grid.Column style={{ 'padding': 0 }} id='hamburger' width={4}>
-        <svg className='icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' onClick={() => props.dispatch({ type: 'CHANGE_VISIBILITY' })}><path fill='#FFFFFF' d='M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z' /></svg>
-      </Grid.Column>
-    )
-  } else {
-    hamburgerIcon = (
-      <Grid.Column style={{ 'padding': 0 }} id='hamburger' width={4}>
-        <svg className='icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20' onClick={() => props.dispatch({ type: 'CHANGE_VISIBILITY' })}><path fill='#FFFFFF' d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' /></svg>
-      </Grid.Column>
-    )
-  }
+  const height = parseInt((window.innerHeight*5)/100)
+  console.log(height)
 
   return (
     <>
       <Grid id='navbar' verticalAlign='middle' columns={2}>
-        {hamburgerIcon}
+        <Grid.Column style={{ 'padding': '0' }} id='hamburger' width={4}>
+          <div style={{'paddingTop': '1vh'}}>
+            <HamburgerSqueeze buttonWidth={height} barColor='white' buttonStyle={{'padding': '0', 'width': '5vh'}} isActive={props.menuVisible} toggleButton={() => props.dispatch({ type: 'CHANGE_VISIBILITY' })} />
+          </div>
+        </Grid.Column>
         <Grid.Column style={{ 'padding': 0 }} width={12}>
           <Grid id='navlinks'>
-            <Grid.Column width={4} style={{ 'padding': '1rem 0' }}>
+            <Grid.Column width={4} style={{ 'margin': 'auto 0', 'padding': '0' }}>
               <Container
                 className='navlink'
                 as={Link} to='/'
@@ -35,7 +28,7 @@ const Navbar = (props) => {
                 <svg className='icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'><path fill='#FFFFFF' d='M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z' /></svg>
               </Container>
             </Grid.Column>
-            <Grid.Column width={4} style={{ 'padding': '1rem 0' }}>
+            <Grid.Column width={4} style={{ 'margin': 'auto 0', 'padding': '0' }}>
               <Container
                 className='navlink'
                 as={Link} to={props.currentUserIn ? '/messenger' : '/login'}
@@ -43,7 +36,7 @@ const Navbar = (props) => {
                 <svg id='messenger-icon' className='icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'><path fill='#FFFFFF' d='M17 11v3l-3-3H8a2 2 0 0 1-2-2V2c0-1.1.9-2 2-2h10a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2h-1zm-3 2v2a2 2 0 0 1-2 2H6l-3 3v-3H2a2 2 0 0 1-2-2V8c0-1.1.9-2 2-2h2v3a4 4 0 0 0 4 4h6z' /></svg>
               </Container>
             </Grid.Column>
-            <Grid.Column width={4} style={{ 'padding': '1rem 0' }}>
+            <Grid.Column width={4} style={{ 'margin': 'auto 0', 'padding': '0' }}>
               <Container
                 className='navlink'
                 as={Link} to={props.currentUserIn ? '/all-bookings' : '/login'}
@@ -51,7 +44,7 @@ const Navbar = (props) => {
                 <svg id='bookings-icon' className='icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'><path fill='#FFFFFF' d='M1 4c0-1.1.9-2 2-2h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4zm2 2v12h14V6H3zm2-6h2v2H5V0zm8 0h2v2h-2V0zM5 9h2v2H5V9zm0 4h2v2H5v-2zm4-4h2v2H9V9zm0 4h2v2H9v-2zm4-4h2v2h-2V9zm0 4h2v2h-2v-2z' /></svg>
               </Container>
             </Grid.Column>
-            <Grid.Column width={4} style={{ 'padding': '1rem 0' }}>
+            <Grid.Column width={4} style={{ 'margin': 'auto 0', 'padding': '0' }}>
               <Container
                 className='navlink'
                 as={Link} to={props.currentUserIn ? '/user-page' : '/login'}
