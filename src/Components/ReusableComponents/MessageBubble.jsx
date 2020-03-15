7cb79df7c995cc6d380c3fad6d5dbd77e57831b3
raptorf1 <raptor_f1@hotmail.com>
2020-03-15 11:49:05 +0200

    Creates ENG and SV locales for MessageBubble component. Implements code to access translations.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/ReusableComponents/MessageBubble.jsx
+++ src/Components/ReusableComponents/MessageBubble.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Spinner from './Spinner'
+import { useTranslation } from 'react-i18next'
 import { Image } from 'semantic-ui-react'
 import timeFormat from '../../Modules/dateFormatting'
 import moment from 'moment'
 
 const MessageBubble = (props) => {
+
+  const { t, ready } = useTranslation('MessageBubble')
+
   let textAlign, flexDirection, margin, border, avatar, nickname, content
   content = (window.navigator.userAgent.includes('Firefox') ? '-moz-fit-content' : 'fit-content')
 
@@ -18,7 +23,7 @@ const MessageBubble = (props) => {
     margin = '0'
     border = '1rem 1rem 1rem 0'
     avatar = 'https://ui-avatars.com/api/?name=[x]&size=150&length=3&font-size=0.3&rounded=true&background=d8d8d8&color=c90c61&uppercase=false'
-    nickname = 'Deleted user'
+    nickname = t('MessageBubble:nickname-deleted')
   } else if (props.currentUsername === props.message.user.nickname) {
     textAlign = 'right'
     flexDirection = 'row-reverse'
@@ -35,46 +40,48 @@ const MessageBubble = (props) => {
     nickname = props.message.user.nickname
   }
 
-  return (
-    <div style={{ 'textAlign': textAlign }} data-cy='all-messages-individual-conversation'>
-      <div style={{ 'display': 'flex', 'flexDirection': flexDirection, 'marginBottom': '0.5rem', 'alignItems': 'center' }}>
-        <Image src={avatar === null ? `https://ui-avatars.com/api/?name=${nickname}&size=150&length=3&font-size=0.3&rounded=true&background=d8d8d8&color=c90c61&uppercase=false` : avatar} size='mini' style={{ 'borderRadius': '50%', 'height': '2rem', 'width': '2rem' }}></Image>
-        <p style={{ 'color': '#c90c61', 'margin': '0 0.5rem' }}>
-          <strong>
-            {nickname}
-          </strong>
+  if (ready) {
+    return (
+      <div style={{ 'textAlign': textAlign }} data-cy='all-messages-individual-conversation'>
+        <div style={{ 'display': 'flex', 'flexDirection': flexDirection, 'marginBottom': '0.5rem', 'alignItems': 'center' }}>
+          <Image src={avatar === null ? `https://ui-avatars.com/api/?name=${nickname}&size=150&length=3&font-size=0.3&rounded=true&background=d8d8d8&color=c90c61&uppercase=false` : avatar} size='mini' style={{ 'borderRadius': '50%', 'height': '2rem', 'width': '2rem' }}></Image>
+          <p style={{ 'color': '#c90c61', 'margin': '0 0.5rem' }}>
+            <strong>
+              {nickname}
+            </strong>
+          </p>
+        </div>
+        <div style={{
+          'backgroundColor': '#eeeeee',
+          'margin': margin,
+          'borderRadius': border,
+          'display': props.message.body === '' && 'flex',
+          'justifyContent': props.message.body === '' && 'center',
+          'alignItems': props.message.body === '' && 'center',
+          'overflow': props.message.body === '' && 'hidden',
+          'padding': props.message.body === '' ? 'none' : '1rem',
+          'height': 'min-content',
+          'width': content,
+          'maxWidth': '70%'
+        }}>
+          {props.message.body === '' ?
+            <a href={props.message.image} target='_blank' rel='noopener noreferrer' style={{ 'lineHeight': '0' }}>
+              <img
+                onLoad={(e) => onImageLoad(e)}
+                alt={t('MessageBubble:image-alt-txt')}
+                style={{ 'flexShrink': '0', 'minWidth': '100%', 'minHeight': '100%' }}
+                src={props.message.image}>
+              </img>
+            </a>
+            : <p>{props.message.body} </p>
+          }
+        </div>
+        <p style={{ 'fontSize': 'small', 'marginBottom': '1rem' }}>
+          {moment(props.message.created_at).format(timeFormat(props.message.created_at))}
         </p>
       </div>
-      <div style={{
-        'backgroundColor': '#eeeeee',
-        'margin': margin,
-        'borderRadius': border,
-        'display': props.message.body === '' && 'flex',
-        'justifyContent': props.message.body === '' && 'center',
-        'alignItems': props.message.body === '' && 'center',
-        'overflow': props.message.body === '' && 'hidden',
-        'padding': props.message.body === '' ? 'none' : '1rem',
-        'height': 'min-content',
-        'width': content,
-        'maxWidth': '70%'
-      }}>
-        {props.message.body === '' ?
-          <a href={props.message.image} target='_blank' rel='noopener noreferrer' style={{ 'lineHeight': '0' }}>
-            <img
-              onLoad={(e) => onImageLoad(e)}
-              alt='uploaded file'
-              style={{ 'flexShrink': '0', 'minWidth': '100%', 'minHeight': '100%' }}
-              src={props.message.image}>
-            </img>
-          </a>
-          : <p>{props.message.body} </p>
-        }
-      </div>
-      <p style={{ 'fontSize': 'small', 'marginBottom': '1rem' }}>
-        {moment(props.message.created_at).format(timeFormat(props.message.created_at))}
-      </p>
-    </div>
-  )
+    )
+  } else { return <Spinner /> }
 }
 
 export default MessageBubble
