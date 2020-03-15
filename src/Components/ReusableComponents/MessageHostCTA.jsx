39bf47224490a30037a92727626502b869fb92b4
raptorf1 <raptor_f1@hotmail.com>
2020-03-15 11:28:18 +0200

    Loads specific translation file and Spinner until translations are ready for MessageHostCTA component
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/ReusableComponents/MessageHostCTA.jsx
+++ src/Components/ReusableComponents/MessageHostCTA.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
+import Spinner from './Spinner'
 import { Button, Header } from 'semantic-ui-react'
 import { Trans, useTranslation } from 'react-i18next'
 
 const MessageHostCTA = (props) => {
-  const { t } = useTranslation()
+
+  const { t, ready } = useTranslation('HostProfileView')
+
   const [loading, setLoading] = useState(false)
 
   const handleOnClick = (e) => {
@@ -12,25 +15,28 @@ const MessageHostCTA = (props) => {
     props.messageHost(e)
   }
 
-  return (
-    <>
-      <Header id='total' as='h3' style={{ 'marginBottom': '0' }}>
-        {t('HostProfileView:questions')}
-      </Header>
-      <p className='small-centered-paragraph' style={{ 'marginBottom': '1rem' }}>
-        <Trans i18nKey='HostProfileView:send-msg' values={{ host: props.nickname }} >
-          You can send a message to <strong style={{ 'color': '#c90c61' }}>{props.nickname}</strong> and find out.
-        </Trans>
-      </p>
-      <Button
-        id='send-message'
-        style={{ 'marginTop': '0', 'marginBottom': '2rem' }}
-        loading={loading}
-        disabled={loading}
-        onClick={(e) => handleOnClick(e)}>
-        {t('HostProfileView:send-cta')}
-      </Button>
-    </>
-  )
+  if (ready) {
+    return (
+      <>
+        <Header id='total' as='h3' style={{ 'marginBottom': '0' }}>
+          {t('HostProfileView:questions')}
+        </Header>
+        <p className='small-centered-paragraph' style={{ 'marginBottom': '1rem' }}>
+          <Trans i18nKey='HostProfileView:send-msg' values={{ host: props.nickname }} >
+            You can send a message to <strong style={{ 'color': '#c90c61' }}>{props.nickname}</strong> and find out.
+          </Trans>
+        </p>
+        <Button
+          id='send-message'
+          style={{ 'marginTop': '0', 'marginBottom': '2rem' }}
+          loading={loading}
+          disabled={loading}
+          onClick={(e) => handleOnClick(e)}>
+          {t('HostProfileView:send-cta')}
+        </Button>
+      </>
+    )
+  } else { return <Spinner /> }
 }
+
 export default MessageHostCTA
