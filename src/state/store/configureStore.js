f63abdb5413a90277c731f44f3b5519c5bfe087c
raptorf1 <raptor_f1@hotmail.com>
2020-03-21 14:00:34 +0200

    Uses ternary to not use redux logger in production environment
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/state/store/configureStore.js
+++ src/state/store/configureStore.js
@@ -3,11 +3,8 @@ import rootReducer from '../reducers/rootReducer'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 
-const configureStore = () => {
-  return createStore(
-    rootReducer,
-    applyMiddleware(thunk, logger)
-  )
-}
+const settings = process.env.NODE_ENV === 'development' ? (createStore(rootReducer, applyMiddleware(thunk, logger))) : (createStore(rootReducer, applyMiddleware(thunk)))
+
+const configureStore = () => { return settings }
 
 export default configureStore
