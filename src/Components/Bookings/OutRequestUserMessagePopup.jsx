42b3576760b2d70e28b4e5a006dc9621e4153307
raptorf1 <raptor_f1@hotmail.com>
2020-03-24 16:13:38 +0200

    Implements code to access translation files. Returns Spinner component while translations are loading. OutRequestUserMessagePopup component.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Bookings/OutRequestUserMessagePopup.jsx
+++ src/Components/Bookings/OutRequestUserMessagePopup.jsx
@@ -1,26 +1,34 @@
 import React from 'react'
 import { Header, Image } from 'semantic-ui-react'
+import Spinner from '../ReusableComponents/Spinner'
+import { Trans, useTranslation } from 'react-i18next'
 
 const OutRequestUserMessagePopup = (props) => {
 
-  return (
-    <>
-      <div style={{ 'margin': '-2rem -2rem 2rem', 'background': '#c90c61', 'padding': '2rem' }}>
-        <p style={{ 'color': '#ffffff', 'fontSize': 'small' }}>
-          Your booking request for the dates of <strong>{props.startDate}</strong> until <strong>{props.endDate}</strong>.
+  const { ready } = useTranslation('OutRequestUserMessagePopup')
+
+  if (ready) {
+    return (
+      <>
+        <div style={{ 'margin': '-2rem -2rem 2rem', 'background': '#c90c61', 'padding': '2rem' }}>
+          <p style={{ 'color': '#ffffff', 'fontSize': 'small' }}>
+            <Trans i18nKey='OutRequestUserMessagePopup:main-title'>
+              Your booking request for the dates of <strong>{{ startDate: props.startDate }}</strong> until <strong>{{ endDate: props.endDate }}</strong>.
+            </Trans>
+          </p>
+        </div>
+        <div style={{ 'display': 'flex', 'alignItems': 'center' }}>
+          <Image src={props.avatar === null ? `https://ui-avatars.com/api/?name=${props.nickname}&size=150&length=3&font-size=0.3&rounded=true&background=d8d8d8&color=c90c61&uppercase=false` : props.avatar} size='small' style={{ 'borderRadius': '50%', 'width': '3rem', 'height': '3rem' }}></Image>
+          <Header style={{ 'margin': '0 1rem' }}>
+            {props.nickname}
+          </Header>
+        </div>
+        <p style={{ 'fontSize': 'small', 'fontStyle': 'italic', 'margin': '1rem 0 0' }}>
+          {props.message}
         </p>
-      </div>
-      <div style={{ 'display': 'flex', 'alignItems': 'center' }}>
-        <Image src={props.avatar === null ? `https://ui-avatars.com/api/?name=${props.nickname}&size=150&length=3&font-size=0.3&rounded=true&background=d8d8d8&color=c90c61&uppercase=false` : props.avatar} size='small' style={{ 'borderRadius': '50%', 'width': '3rem', 'height': '3rem' }}></Image>
-        <Header style={{ 'margin': '0 1rem' }}>
-          {props.nickname}
-        </Header>
-      </div>
-      <p style={{ 'fontSize': 'small', 'fontStyle': 'italic', 'margin': '1rem 0 0' }}>
-        {props.message}
-      </p>
-    </>
-  )
+      </>
+    )
+  } else { return <Spinner /> }
 }
 
 export default OutRequestUserMessagePopup
