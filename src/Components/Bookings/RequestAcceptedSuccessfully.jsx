ce83384383d1acd2fb006ea14ecc0c1796c5de85
raptorf1 <raptor_f1@hotmail.com>
2020-03-24 16:26:53 +0200

    Implements code to access translation files. Returns Spinner component while translations are loading. RequestAcceptedSuccessfully component.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Bookings/RequestAcceptedSuccessfully.jsx
+++ src/Components/Bookings/RequestAcceptedSuccessfully.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Header, Segment } from 'semantic-ui-react'
+import Spinner from '../ReusableComponents/Spinner'
+import { Trans, withTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
 
 class RequestAcceptedSuccessfully extends Component {
@@ -11,19 +13,26 @@ class RequestAcceptedSuccessfully extends Component {
   }
 
   render() {
-    return (
-      <div className='content-wrapper'>
-        <Header as='h1'>
-          Success!
-        </Header>
-        <Segment className='whitebox' style={{ 'textAlign': 'center' }}>
-          <p>
-            You have successfully accepted a booking request. The person who requested this booking has been notified about your decision and has received an access to your full address. You can message them using your Incoming Bookings dashboard. Questions? Check out our <Header as={Link} to='faq' className='fake-link'>FAQ</Header>.
-          </p>
-        </Segment>
-      </div>
-    )
+
+    const { t } = this.props
+
+    if (this.props.tReady) {
+      return (
+        <div className='content-wrapper'>
+          <Header as='h1'>
+            {t('RequestAcceptedSuccessfully:title')}
+          </Header>
+          <Segment className='whitebox' style={{ 'textAlign': 'center' }}>
+            <p>
+              <Trans i18nKey='RequestAcceptedSuccessfully:desc'>
+                You have successfully accepted a booking request. The person who requested this booking has been notified about your decision and has received an access to your full address. You can message them using your Incoming Bookings dashboard. Questions? Check out our <Header as={Link} to='faq' className='fake-link'>FAQ</Header>.
+              </Trans>
+            </p>
+          </Segment>
+        </div>
+      )
+    } else { return <Spinner /> }
   }
 }
 
-export default RequestAcceptedSuccessfully
+export default withTranslation('RequestAcceptedSuccessfully')(RequestAcceptedSuccessfully)
