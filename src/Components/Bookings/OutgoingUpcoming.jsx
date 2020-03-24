99026ef3de1f4776058ff5a1d07ff27495da97e0
raptorf1 <raptor_f1@hotmail.com>
2020-03-24 15:37:56 +0200

    Implements code to access translation files. Returns Spinner component while translations are loading. OutgoingUpcoming component.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Bookings/OutgoingUpcoming.jsx
+++ src/Components/Bookings/OutgoingUpcoming.jsx
@@ -1,66 +1,77 @@
 import React from 'react'
 import moment from 'moment'
 import { Container } from 'semantic-ui-react'
+import Spinner from '../ReusableComponents/Spinner'
+import { Trans, useTranslation } from 'react-i18next'
 import { withRouter } from 'react-router-dom'
 
 const OutgoingUpcoming = (props) => {
-  let sortedUpcoming = props.upcoming
-  sortedUpcoming.sort((a, b) => (a.dates[0] - b.dates[0]))
 
-  if (props.upcoming.length > 0) {
-    return (
-      <>
-        <p className='small-centered-paragraph'>
-          <strong>You have {props.upcoming.length} upcoming {props.upcoming.length > 1 ? 'bookings' : 'booking'}.</strong>
-        </p>
-        <p style={{ 'textAlign': 'center' }}>
-          These are finalized bookings coming up soon. Get in touch with these hosts to organize the drop-off and pick-up.
-        </p>
-        {sortedUpcoming.map(upcoming => {
-          return (
-            <Container style={{ 'backgroundColor': '#e8e8e8', 'marginTop': '2rem', 'padding': '2rem' }} id={upcoming.id} data-cy='outgoing-upcoming' key={upcoming.id}>
-              <p className='small-centered-paragraph'>
-                You have successfully booked a stay with <strong>{upcoming.host_nickname}</strong> for your <strong>{upcoming.number_of_cats} {upcoming.number_of_cats > 1 ? 'cats' : 'cat'}</strong> for the dates of <strong>{moment(upcoming.dates[0]).format('YYYY-MM-DD')}</strong> until <strong>{moment(upcoming.dates[upcoming.dates.length - 1]).format('YYYY-MM-DD')}</strong>.
-              </p>
-              <p
-                className='fake-link-underlined'
-                onClick={() => {
-                  const { history } = props
-                  history.push({
-                    pathname: '/booking-details',
-                    state: {
-                      hostId: upcoming.host_id,
-                      avatar: upcoming.host_avatar,
-                      description: upcoming.host_description,
-                      nickname: upcoming.host_nickname,
-                      startDate: moment(upcoming.dates[0]).format('YYYY-MM-DD'),
-                      endDate: moment(upcoming.dates[upcoming.dates.length - 1]).format('YYYY-MM-DD'),
-                      priceTotal: upcoming.price_total,
-                      address: upcoming.host_full_address,
-                      lat: upcoming.host_real_lat,
-                      long: upcoming.host_real_long,
-                      location: upcoming.host_location,
-                      numberOfCats: upcoming.number_of_cats
-                    }
-                  })
-                }}
-              >
-                View details
-              </p>
-            </Container>
-          )
-        })}
-      </>
-    )
-  } else {
-    return (
-      <>
-        <p className='small-centered-paragraph'>
-          <strong>You don't have any upcoming bookings.</strong>
-        </p>
-      </>
-    )
-  }
+  const { t, ready } = useTranslation('OutgoingUpcoming')
+
+  if (ready) {
+    let sortedUpcoming = props.upcoming
+    sortedUpcoming.sort((a, b) => (a.dates[0] - b.dates[0]))
+
+    if (props.upcoming.length > 0) {
+      return (
+        <>
+          <p className='small-centered-paragraph'>
+            <Trans count={parseInt(props.upcoming.length)} i18nKey='OutgoingUpcoming:main-header'>
+              <strong>You have {{ count: props.upcoming.length }} upcoming booking.</strong>
+            </Trans>
+          </p>
+          <p style={{ 'textAlign': 'center' }}>
+            {t('OutgoingUpcoming:desc')}
+          </p>
+          {sortedUpcoming.map(upcoming => {
+            return (
+              <Container style={{ 'backgroundColor': '#e8e8e8', 'marginTop': '2rem', 'padding': '2rem' }} id={upcoming.id} data-cy='outgoing-upcoming' key={upcoming.id}>
+                <p className='small-centered-paragraph'>
+                  <Trans count={parseInt(upcoming.number_of_cats)} i18nKey='OutgoingUpcoming:upc-desc'>
+                    You have successfully booked a stay with <strong>{{ nickname: upcoming.host_nickname }}</strong> for your <strong>{{ count: upcoming.number_of_cats }} cat</strong> for the dates of <strong>{{ startDate: moment(upcoming.dates[0]).format('YYYY-MM-DD') }}</strong> until <strong>{{ endDate: moment(upcoming.dates[upcoming.dates.length - 1]).format('YYYY-MM-DD') }}</strong>.
+                  </Trans>
+                </p>
+                <p
+                  className='fake-link-underlined'
+                  onClick={() => {
+                    const { history } = props
+                    history.push({
+                      pathname: '/booking-details',
+                      state: {
+                        hostId: upcoming.host_id,
+                        avatar: upcoming.host_avatar,
+                        description: upcoming.host_description,
+                        nickname: upcoming.host_nickname,
+                        startDate: moment(upcoming.dates[0]).format('YYYY-MM-DD'),
+                        endDate: moment(upcoming.dates[upcoming.dates.length - 1]).format('YYYY-MM-DD'),
+                        priceTotal: upcoming.price_total,
+                        address: upcoming.host_full_address,
+                        lat: upcoming.host_real_lat,
+                        long: upcoming.host_real_long,
+                        location: upcoming.host_location,
+                        numberOfCats: upcoming.number_of_cats
+                      }
+                    })
+                  }}
+                >
+                  {t('OutgoingUpcoming:details')}
+                </p>
+              </Container>
+            )
+          })}
+        </>
+      )
+    } else {
+      return (
+        <>
+          <p className='small-centered-paragraph'>
+            <strong>{t('OutgoingUpcoming:no-upc')}</strong>
+          </p>
+        </>
+      )
+    }
+  } else { return <Spinner /> }
 }
 
 export default withRouter(OutgoingUpcoming)
