e54351337ef05de3a53b5214a2808d48043544dd
zanenkn <zane.neikena@gmail.com>
2020-03-29 19:53:26 +0200

    Terms implemented.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Menu/Legal.jsx
+++ src/Components/Menu/Legal.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Header } from 'semantic-ui-react'
-import WIP from '../ReusableComponents/WorkInProgress'
-import { useTranslation } from 'react-i18next'
+import { useTranslation, Trans } from 'react-i18next'
 
 const Legal = () => {
   const { t } = useTranslation('Legal')
   return (
     <>
-      <div className='content-wrapper' >
+      <div className='content-wrapper' style={{ 'marginBottom': '2rem' }}>
         <Header as='h1'>
           {t('reusable:title.legal')}
         </Header>
@@ -71,8 +70,16 @@ const Legal = () => {
         <p>{t('Legal:booking.p2')}</p>
         <p>{t('Legal:booking.p3')}</p>
         <p>{t('Legal:booking.p4')}</p>
-        <p>{t('Legal:booking.p5')}</p>
-        <p>{t('Legal:booking.p6')}</p>
+        <p>
+          <Trans i18nKey='Legal:booking.p5'>
+          It is Cat Sitter's own responsibility to declare any eventual income generated from fulfilling Services. If you take care for pets as a business activity you may be eligible for approval of Swedish F-tax. <a href='https://skatteverket.se/' target='_blank' rel='noopener noreferrer'>Please refer to Swedish Tax Agency for more information.</a>
+          </Trans>
+        </p>
+        <p>
+          <Trans i18nKey='Legal:booking.p6'>
+            Cat Sitters who professionally care for cats may need a permit. <a href='https://www.riksdagen.se/sv/dokument-lagar/dokument/svensk-forfattningssamling/djurskyddslag-20181192_sfs-2018-1192' target='_blank' rel='noopener noreferrer'>Please refer to Swedish Animal Welfare Act chapter 6.1 for more information.</a> Users are themselves responsible to determine if they require permit to perform the Service and obtain such permit if necessary.
+          </Trans>
+        </p>
         <p>{t('Legal:booking.p7')}</p>
         <Header as='h2' >
           {t('Legal:during-service.title')}
@@ -81,12 +88,20 @@ const Legal = () => {
         <p>{t('Legal:during-service.p2')}</p>
         <p>
           <ul>
-            <li>{t('Legal:during-service.li1')}</li>
+            <li>
+              <Trans i18nKey='Legal:during-service.li1'>
+                attending to Cat Owner's cat(s) at least twice a day according to <a href='https://www.riksdagen.se/sv/dokument-lagar/dokument/svensk-forfattningssamling/lag-20071150-om-tillsyn-over-hundar-och_sfs-2007-1150' target='_blank' rel='noopener noreferrer'>Swedish Law on the supervision of dogs and cats</a>,
+              </Trans>
+            </li>
             <li>{t('Legal:during-service.li2')}</li>
             <li>{t('Legal:during-service.li3')}</li>
           </ul>
         </p>
-        <p>{t('Legal:during-service.p3')}</p>
+        <p>
+          <Trans i18nKey='Legal:during-service.p3'>
+            It is both Cat Owner's and Cat Sitter's responsibility to examine the cat(s) before Service commences and agree on relevant matters. Users can use <a href='https://kattbnb.se/guidelines' target='_blank' rel='noopener noreferrer'>KattBNB Guidelines</a> to facilitate this agreement.
+          </Trans>
+        </p>
         <Header as='h3' style={{ 'textAlign': 'left' }} >
           {t('Legal:during-service.emergency.title')}
         </Header>
@@ -115,7 +130,11 @@ const Legal = () => {
         </p>
         <p>{t('Legal:personal-data.in-general.p3')}</p>
         <p>{t('Legal:personal-data.in-general.p4')}</p>
-        <p>{t('Legal:personal-data.in-general.p5')}</p>
+        <p>
+          <Trans i18nKey='Legal:personal-data.in-general.p5'>
+            KattBNB is responsible to protect your personal data according to the <a href='https://www.riksdagen.se/sv/dokument-lagar/dokument/svensk-forfattningssamling/lag-2018218-med-kompletterande-bestammelser_sfs-2018-218' target='_blank' rel='noopener noreferrer'>Swedish Data Protection act</a>.
+          </Trans>
+        </p>
         <p>{t('Legal:personal-data.in-general.p6')}</p>
         <Header as='h3' style={{ 'textAlign': 'left' }} >
           {t('Legal:personal-data.in-detail.title')}
@@ -128,11 +147,19 @@ const Legal = () => {
         <p>{t('Legal:personal-data.in-detail.p6')}</p>
         <p>{t('Legal:personal-data.in-detail.p7')}</p>
         <p>{t('Legal:personal-data.in-detail.p8')}</p>
-        <p>{t('Legal:personal-data.in-detail.p9')}</p>
+        <p>
+          <Trans i18nKey='Legal:personal-data.in-detail.p9'>
+            For a more detailed flow of what happens when Users request an account deletion, please refer to our <a href='https://kattbnb.se/faq' target='_blank' rel='noopener noreferrer'>FAQ page</a>.
+          </Trans>
+        </p>
         <Header as='h2' >
           {t('Legal:contact-us.title')}
         </Header>
-        <p>{t('Legal:contact-us.p1')}</p>
+        <p>
+          <Trans i18nKey='Legal:contact-us.p1'>
+            If you have any questions or suggestions about our Terms, do not hesitate to <a href='https://kattbnb.se/contact-us' target='_blank' rel='noopener noreferrer'>contact us</a>.
+          </Trans>
+        </p>
       </div>
     </>
   )
