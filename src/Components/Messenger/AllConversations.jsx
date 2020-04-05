93311cd339fb146ca6a8d173c296e087272e5a5e
raptorf1 <raptor_f1@hotmail.com>
2020-04-05 13:06:40 +0300

    Handles 401 + 500 server errors & no user connection in Messenger folder components
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Messenger/AllConversations.jsx
+++ src/Components/Messenger/AllConversations.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Spinner from '../ReusableComponents/Spinner'
 import { connect } from 'react-redux'
-import { Image, Header, Grid, Divider } from 'semantic-ui-react'
+import { Image, Header, Grid, Divider, Message } from 'semantic-ui-react'
 import timeFormat from '../../Modules/dateFormatting'
 import moment from 'moment'
 import axios from 'axios'
@@ -13,35 +13,65 @@ class AllConversations extends Component {
   state = {
     conversations: '',
     scrollYPosition: 0,
-    loading: true
+    loading: true,
+    errorDisplay: false,
+    errors: []
   }
 
   componentDidMount() {
+    const { t } = this.props
     window.addEventListener('scroll', this.handleScroll)
-    const lang = detectLanguage()
-    const headers = {
-      uid: window.localStorage.getItem('uid'),
-      client: window.localStorage.getItem('client'),
-      'access-token': window.localStorage.getItem('access-token')
-    }
-    const path = `/api/v1/conversations?user_id=${this.props.id}&locale=${lang}`
-    axios.get(path, { headers: headers })
-      .then(response => {
-        const shownConversations = []
-        response.data.map(conversation => {
-          if (conversation.hidden !== this.props.id) {
-            shownConversations.push(conversation)
+    if (window.navigator.onLine === false) {
+      this.setState({
+        loading: false,
+        errorDisplay: true,
+        errors: ['reusable:errors:window-navigator']
+      })
+    } else {
+      const lang = detectLanguage()
+      const headers = {
+        uid: window.localStorage.getItem('uid'),
+        client: window.localStorage.getItem('client'),
+        'access-token': window.localStorage.getItem('access-token')
+      }
+      const path = `/api/v1/conversations?user_id=${this.props.id}&locale=${lang}`
+      axios.get(path, { headers: headers })
+        .then(response => {
+          const shownConversations = []
+          response.data.map(conversation => {
+            if (conversation.hidden !== this.props.id) {
+              shownConversations.push(conversation)
+            }
+          })
+          const sortedResponse = shownConversations.sort(function (a, b) {
+            let dateA = new Date(a.msg_created), dateB = new Date(b.msg_created)
+            return dateB - dateA
+          })
+          this.setState({
+            conversations: sortedResponse,
+            loading: false,
+            errorDisplay: false,
+            errors: []
+          })
+        }).catch(error => {
+          if (error.response.status === 500) {
+            this.setState({
+              loading: false,
+              errorDisplay: true,
+              errors: ['reusable:errors:500']
+            })
+          } else if (error.response.status === 401) {
+            window.alert(t('reusable:errors:401'))
+            window.location.replace('/login')
+          } else {
+            this.setState({
+              loading: false,
+              errorDisplay: true,
+              errors: error.response.data.error
+            })
           }
         })
-        const sortedResponse = shownConversations.sort(function (a, b) {
-          let dateA = new Date(a.msg_created), dateB = new Date(b.msg_created)
-          return dateB - dateA
-        })
-        this.setState({
-          conversations: sortedResponse,
-          loading: false
-        })
-      })
+    }
   }
 
   componentWillUnmount() { window.removeEventListener('scroll', this.handleScroll) }
@@ -59,6 +89,18 @@ class AllConversations extends Component {
 
     if (this.state.loading) {
       return <Spinner />
+    } else if (this.state.errorDisplay) {
+      return (
+        <div className='content-wrapper' >
+          <Message negative >
+            <ul id='message-error-list'>
+              {this.state.errors.map(error => (
+                <li key={error}>{t(error)}</li>
+              ))}
+            </ul>
+          </Message>
+        </div>
+      )
     } else {
       return (
         <>
