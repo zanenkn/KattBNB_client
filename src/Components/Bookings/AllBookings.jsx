b006072fae73e8343b5e095b364acce84caaa594
raptorf1 <raptor_f1@hotmail.com>
2020-04-09 13:29:46 +0300

    Implements 503 error handling in Bookings folder components
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Bookings/AllBookings.jsx
+++ src/Components/Bookings/AllBookings.jsx
@@ -52,6 +52,8 @@ class AllBookings extends Component {
             errorDisplay: true,
             errors: ['reusable:errors:500']
           })
+        } else if (error.response.status === 503) {
+          wipeCredentials('/is-not-available?atm')
         } else if (error.response.status === 401) {
           window.alert(t('reusable:errors:401'))
           wipeCredentials('/')
@@ -79,6 +81,8 @@ class AllBookings extends Component {
             errorDisplay: true,
             errors: ['reusable:errors:500']
           })
+        } else if (error.response.status === 503) {
+          wipeCredentials('/is-not-available?atm')
         } else if (error.response.status === 401) {
           window.alert(t('reusable:errors:401'))
           wipeCredentials('/')
