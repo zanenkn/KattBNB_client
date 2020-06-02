d476f49ff374cbdf3e5e19c1816f4026d16d6ed2
raptorf1 <raptor_f1@hotmail.com>
2020-06-02 18:47:59 +0300

    Protects LeaveReview component from POP history action
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Bookings/IncomingHistory.jsx
+++ src/Components/Bookings/IncomingHistory.jsx
@@ -158,7 +158,6 @@ const IncomingHistory = (props) => {
               )
             }
           })}
-
         </>
       )
     } else {
--- src/Components/Reviews/LeaveReview.jsx
+++ src/Components/Reviews/LeaveReview.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Header } from 'semantic-ui-react'
 
 const LeaveReview = (props) => {
+
+  useEffect(() => {
+    if (props.location.state === undefined || props.history.action === 'POP') {
+      window.location.replace('/all-bookings')
+    }
+  }, [props.location.state, props.history.action])
+
   return (
     <div className='content-wrapper' >
       <Header as='h1'>
