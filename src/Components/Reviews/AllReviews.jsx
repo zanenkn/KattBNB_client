9765a7234460261c7ba32dd7310e0c817dd3f2da
raptorf1 <raptor_f1@hotmail.com>
2020-06-13 18:55:28 +0300

    AllReviews component taking shape!
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Reviews/AllReviews.jsx
+++ src/Components/Reviews/AllReviews.jsx
@@ -1,25 +1,66 @@
 import React, { useEffect, useState } from 'react'
+import { Message } from 'semantic-ui-react'
+import { Trans, useTranslation } from 'react-i18next'
+import Spinner from '../ReusableComponents/Spinner'
 import { detectLanguage } from '../../Modules/detectLanguage'
+import { wipeCredentials } from '../../Modules/wipeCredentials'
 import axios from 'axios'
-
-
 
 const AllReviews = (props) => {
 
+  const { t, ready } = useTranslation('')
+
   const [reviews, setReviews] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [errors, setErrors] = useState([])
+  const [errorDisplay, setErrorDisplay] = useState(false)
 
   useEffect(() => {
-
+    setLoading(true)
     const lang = detectLanguage()
-    const path = `/api/v1/reviews?host_profile_id=${props.id}&locale=${lang}`
-    axios.get(path)
-      .then(resp => {
-        setReviews(resp.data)
-      })
+    if (window.navigator.onLine === false) {
+      setLoading(false)
+      setErrors(['reusable:errors:window-navigator'])
+      setErrorDisplay(true)
+    } else {
+      axios.get(`/api/v1/reviews?host_profile_id=${props.id}&locale=${lang}`)
+        .then(resp => {
+          setReviews(resp.data)
+          setLoading(false)
+        })
+        .catch(error => {
+          if (error.response === undefined) {
+            wipeCredentials('/is-not-available?atm')
+          } else if (error.response.status === 500) {
+            setLoading(false)
+            setErrorDisplay(true)
+            setErrors(['reusable:errors:500'])
+          } else if (error.response.status === 503) {
+            wipeCredentials('/is-not-available?atm')
+          } else if (error.response.status === 401) {
+            window.alert(t('reusable:errors:401'))
+            wipeCredentials('/')
+          } else {
+            setLoading(false)
+            setErrorDisplay(true)
+            setErrors([error.response.data.error])
+          }
+        })
+    }
   }, [props.id])
 
   return (
     <>
+      {
+        errorDisplay &&
+        <Message negative >
+          <ul id='message-error-list'>
+            {errors.map(error => (
+              <li key={error}>{t(error)}</li>
+            ))}
+          </ul>
+        </Message>
+      }
       {
         reviews.length > 0 &&
         <>
