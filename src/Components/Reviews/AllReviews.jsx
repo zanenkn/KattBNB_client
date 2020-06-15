eaa21dbf9dec0cafd5c57eaa6e541fef7c7fa133
raptorf1 <raptor_f1@hotmail.com>
2020-06-15 10:56:23 +0300

    Resolves react console yellow warnings
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Components/Reviews/AllReviews.jsx
+++ src/Components/Reviews/AllReviews.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Message, Header, Image, Divider } from 'semantic-ui-react'
-import { Trans, useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import Spinner from '../ReusableComponents/Spinner'
 import { detectLanguage } from '../../Modules/detectLanguage'
 import { wipeCredentials } from '../../Modules/wipeCredentials'
@@ -51,7 +51,7 @@ const AllReviews = (props) => {
           }
         })
     }
-  }, [props.hostProfileId, t])
+  }, [props.hostProfileId, lang, t])
 
   if (ready && loading === false) {
     moment.locale(lang)
