f851a6b3e00aacd2072803b9ab41f67185ea7c11
zanenkn <zane.neikena@gmail.com>
2020-06-20 12:55:47 +0200

    __css-refactoring.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ class App extends Component {
         <Sticky context={this.contextRef}>
           <Navbar />
         </Sticky>
-        <div onClick={this.props.menuVisible ? () => { this.props.dispatch({ type: 'CHANGE_VISIBILITY' }) } : () => { }} style={{ 'minHeight': '90vh', 'overflow': 'hidden' }}>
+        <div onClick={this.props.menuVisible ? () => { this.props.dispatch({ type: 'CHANGE_VISIBILITY' }) } : () => { }}>
           <Sidebar.Pushable
             as={Container}
             id='app-content'
