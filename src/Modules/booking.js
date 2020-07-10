2dde82f70a6c9eb450d9da8c9a987889f82877b0
raptorf1 <raptor_f1@hotmail.com>
2020-07-10 12:23:15 +0300

    Removes bookingSearch from modules and its reference in SearchResults component. No longer needed, sorting is done in the API.
    Co-authored-by: zane <zane.neikena@gmail.com>

--- src/Modules/booking.js
+++ src/Modules/booking.js
@@ -10,28 +10,4 @@ const getBookingLength = (checkIn, checkOut) => {
   return dateArray.length
 }
 
-const bookingSearch = (hosts, checkIn, checkOut) => {
-  let booking = []
-  let startDate = checkIn
-  let stopDate = checkOut
-  let currentDate = startDate
-  while (currentDate <= stopDate) {
-    booking.push(currentDate)
-    currentDate = currentDate + 86400000
-  }
-  let availableHosts = []
-  hosts.map(host => {
-    let matcher = []
-    booking.map(date => {
-      if ((host.availability).includes(date)) {
-        matcher.push(date)
-      }
-    })
-    if (JSON.stringify(matcher) === JSON.stringify(booking)) {
-      availableHosts.push(host)
-    }
-  })
-  return availableHosts
-}
-
-export { getBookingLength, bookingSearch }
+export { getBookingLength }
