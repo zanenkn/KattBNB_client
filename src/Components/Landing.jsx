1c7b7ef9ca54c93d7108646920b17c4b84eb8747
zanenkn <zane.neikena@gmail.com>
2020-08-01 21:06:01 +0200

    Translations for landing page, some styling for desktop.
    Co-authored-by: george <raptor_f1@hotmail.com>

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,8 +1,14 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import KattBNBLogomark from './Icons/KattBNBLogomark'
+import Spinner from './ReusableComponents/Spinner'
+import { useTranslation } from 'react-i18next'
+import { Header, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   const [dimentions, setDimentions] = useState('mobile')
+
+  const { t, ready } = useTranslation('Landing')
 
   useEffect(() => {
     window.innerHeight > window.innerWidth ? setDimentions('mobile') : setDimentions('desktop')
@@ -10,12 +16,26 @@ const Landing = () => {
       window.innerHeight > window.innerWidth ? setDimentions('mobile') : setDimentions('desktop')
     })
   }, [])
+  if (ready) {
+    return (
+      <div className={`landing-hero-wrapper ${dimentions}`}>
+        <div className='landing-desktop-content'>
+          <KattBNBLogomark width={'100px'} />
+          <Header as='h1'>{t('Landing:title')}</Header>
+          <p style={{ 'textAlign': 'center', 'maxWidth': '300px' }} dangerouslySetInnerHTML={{ __html: t('Landing:text') }}></p>
+          <div style={{ 'width': '165px' }}>
+            <Link to={'/search'}>
+              <Button style={{ 'width': '100%' }}>{t('Landing:cta-find')}</Button>
+            </Link>
+            <Link to={'/sign-up'}>
+              <Button style={{ 'width': '100%' }}>{t('Landing:cta-become')}</Button>
+            </Link>
+          </div>
 
-  return (
-    <div className={`landing-hero-wrapper ${dimentions}`}>
-      <KattBNBLogomark width={'100px'} />
-    </div> 
-  )
+        </div>
+      </div>
+    )
+  } else { return <Spinner /> }
 }
 
 export default Landing
