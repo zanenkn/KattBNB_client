bb78ab9a020faa7b0de83afbf47da6c64b7de50b
Laura Reale <laaru1988@gmail.com>
2020-10-02 16:40:47 +0200

    refactor DeclineRequestPopup to function component

--- src/Components/Bookings/DeclineRequestPopup.jsx
+++ src/Components/Bookings/DeclineRequestPopup.jsx
@@ -1,43 +1,39 @@
-import React, { Component } from 'react'
+import React, { useState }  from 'react'
 import { Header, Form, Button, Message } from 'semantic-ui-react'
 import Spinner from '../ReusableComponents/Spinner'
-import { withTranslation, Trans } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import axios from 'axios'
 import { detectLanguage } from '../../Modules/detectLanguage'
 import { wipeCredentials } from '../../Modules/wipeCredentials'
 
-class DeclineRequestPopup extends Component {
+const DeclineRequestPopup = (props) => {
+  const { t, ready } = useTranslation('DeclineRequestPopup')
 
-  state = {
-    message: '',
-    loading: false,
-    errorDisplay: false,
-    errors: ''
-  }
+  const [loading, setLoading] = useState(false)
+  const [message, setMessage] = useState('')
+  const [errorDisplay, setErrorDisplay] = useState(false)
+  const [errors, setErrors] = useState('')
 
-  declineBooking = (e) => {
+  const declineBooking = (e) => {
     e.preventDefault()
-    const { t } = this.props
     const lang = detectLanguage()
-    this.setState({ loading: true })
+    setLoading(true)
     if (window.navigator.onLine === false) {
-      this.setState({
-        loading: false,
-        errorDisplay: true,
-        errors: ['reusable:errors:window-navigator']
-      })
-      this.props.declModalCloseState(true)
+      setLoading(false)
+      setErrorDisplay(true)
+      setErrors(['reusable:errors:window-navigator'])
+      props.declModalCloseState(true)
     } else {
       if (window.confirm(t('DeclineRequestPopup:confirm-decline'))) {
-        if (this.state.message !== '' && this.state.message.length < 201) {
-          const path = `/api/v1/bookings/${this.props.id}`
+        if (message !== '' && message.length < 201) {
+          const path = `/api/v1/bookings/${props.id}`
           const headers = {
             uid: window.localStorage.getItem('uid'),
             client: window.localStorage.getItem('client'),
             'access-token': window.localStorage.getItem('access-token')
           }
           const payload = {
-            host_message: this.state.message,
+            host_message: message,
             status: 'declined',
             locale: lang
           }
@@ -49,99 +45,80 @@ class DeclineRequestPopup extends Component {
               if (error.response === undefined) {
                 wipeCredentials('/is-not-available?atm')
               } else if (error.response.status === 500) {
-                this.setState({
-                  loading: false,
-                  errorDisplay: true,
-                  errors: ['reusable:errors:500']
-                })
-                this.props.declModalCloseState(true)
+                setLoading(false)
+                setErrorDisplay(true)
+                setErrors(['reusable:errors:500'])
+                props.declModalCloseState(true)
               } else if (error.response.status === 503) {
                 wipeCredentials('/is-not-available?atm')
               } else if (error.response.status === 401) {
                 window.alert(t('reusable:errors:401'))
                 wipeCredentials('/')
               } else {
-                this.setState({
-                  loading: false,
-                  errorDisplay: true,
-                  errors: error.response.data.error
-                })
-                this.props.declModalCloseState(true)
+                setLoading(false)
+                setErrorDisplay(true)
+                setErrors(error.response.data.error)
+                props.declModalCloseState(true)
               }
             })
         } else {
-          this.setState({
-            loading: false,
-            errorDisplay: true,
-            errors: ['DeclineRequestPopup:decline-error']
-          })
-          this.props.declModalCloseState(true)
+            setLoading(false)
+            setErrorDisplay(true)
+            setErrors(['DeclineRequestPopup:decline-error'])
+            props.declModalCloseState(true)
         }
       } else {
-        this.setState({ loading: false })
-        this.props.declModalCloseState(true)
+        setLoading(false)
+        props.declModalCloseState(true)
       }
     }
   }
 
-  declineCTA = (e) => {
-    this.props.declModalCloseState(false)
-    this.declineBooking(e)
+  const declineCTA = (e) => {
+    props.declModalCloseState(false)
+    declineBooking(e)
   }
 
-  onChangeHandler = (e) => {
-    this.setState({ [e.target.id]: e.target.value })
-  }
-
-  render() {
-    const { t } = this.props
-
-    if (this.props.tReady) {
-      let errorDisplay
-
-      if (this.state.errorDisplay) {
-        errorDisplay = (
-          <Message negative >
-            <Message.Header style={{ 'textAlign': 'center' }} >{t('DeclineRequestPopup:error-message-header')}</Message.Header>
-            <ul id='message-error-list'>
-              {this.state.errors.map(error => (
-                <li key={error}>{t(error)}</li>
-              ))}
-            </ul>
-          </Message>
-        )
-      }
-
-      return (
-        <>
-          <Header as='h2'>
-            {t('DeclineRequestPopup:page-header')}
-          </Header>
-          <p className='small-centered-paragraph'>
-            <Trans i18nKey='DeclineRequestPopup:page-desc' >
-              You are about to decline a booking request from <strong style={{ 'color': '#c90c61' }}>{{ nickname: this.props.nickname }}</strong> for the dates of <strong style={{ 'color': '#c90c61' }}>{{ startDate: this.props.startDate }}</strong> until <strong style={{ 'color': '#c90c61' }}>{{ endDate: this.props.endDate }}</strong>
-            </Trans>
-          </p>
-          <Form>
-            <Form.TextArea
-              style={{ 'minHeight': '120px' }}
-              label={t('DeclineRequestPopup:text-area-label')}
-              placeholder={t('DeclineRequestPopup:text-area-plch')}
-              required
-              id='message'
-              value={this.state.message}
-              onChange={this.onChangeHandler}
-            />
-          </Form>
-          <p style={{ 'textAlign': 'end', 'fontSize': 'smaller', 'fontStyle': 'italic' }}>
-            {t('DeclineRequestPopup:remaining')} {200 - this.state.message.length}
-          </p>
-          {errorDisplay}
-          <Button id='decline-button' disabled={this.state.loading} loading={this.state.loading} onClick={this.declineCTA}>{t('DeclineRequestPopup:decline-cta')}</Button>
-        </>
-      )
-    } else { return <Spinner /> }
-  }
+  if (ready) { 
+    return (
+    <>
+      <Header as='h2'>
+        {t('DeclineRequestPopup:page-header')}
+      </Header>
+      <p className='small-centered-paragraph'>
+        <Trans i18nKey='DeclineRequestPopup:page-desc' >
+          You are about to decline a booking request from <strong style={{ 'color': '#c90c61' }}>{{ nickname: props.nickname }}</strong> for the dates of <strong style={{ 'color': '#c90c61' }}>{{ startDate: props.startDate }}</strong> until <strong style={{ 'color': '#c90c61' }}>{{ endDate: props.endDate }}</strong>
+        </Trans>
+      </p>
+      <Form>
+        <Form.TextArea
+          style={{ 'minHeight': '120px' }}
+          label={t('DeclineRequestPopup:text-area-label')}
+          placeholder={t('DeclineRequestPopup:text-area-plch')}
+          required
+          id='message'
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </Form>
+      <p style={{ 'textAlign': 'end', 'fontSize': 'smaller', 'fontStyle': 'italic' }}>
+        {t('DeclineRequestPopup:remaining')} {200 - message.length}
+      </p>
+      {errorDisplay && 
+        <Message negative >
+          <Message.Header style={{ 'textAlign': 'center' }} >
+            {t('DeclineRequestPopup:error-message-header')}
+          </Message.Header>
+          <ul id='message-error-list'>
+            {errors.map(error => (
+              <li key={error}>{t(error)}</li>
+            ))}
+          </ul>
+        </Message>
+      }    
+      <Button id='decline-button' disabled={loading} loading={loading} onClick={declineCTA}>{t('DeclineRequestPopup:decline-cta')}</Button>
+    </>
+  )} else return <Spinner />
 }
 
-export default withTranslation('DeclineRequestPopup')(DeclineRequestPopup)
+export default DeclineRequestPopup
