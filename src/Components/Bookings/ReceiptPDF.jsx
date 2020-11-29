26f1c304d68811d7ebc6fe50d04b173ad12e5cd3
raptorf1 <raptor_f1@hotmail.com>
2020-11-29 18:31:04 +0200

    Props destructuring
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- src/Components/Bookings/ReceiptPDF.jsx
+++ src/Components/Bookings/ReceiptPDF.jsx
@@ -8,6 +8,7 @@ const ReceiptPDF = (props) => {
   const { t, ready } = useTranslation('Receipt')
 
   if (ready) {
+    const { createdAt, bookingId, numberOfCats, nickname, startDate, endDate, priceTotal } = props
     return (
       <Document>
         <Page size='A4'>
@@ -15,26 +16,26 @@ const ReceiptPDF = (props) => {
             <Image src='/KattBNB_logo.png' style={{ 'width': '200px', marginBottom: '20px' }}></Image>
             <Text>KattBNB AB</Text>
             <Text>Reg. nr. 559252-4481</Text>
-            <Text>{t('Receipt:issued', { date: props.createdAt })}</Text>
+            <Text>{t('Receipt:issued', { date: createdAt })}</Text>
             <Text style={{ paddingTop: '60px', paddingBottom: '30px', textAlign: 'center', fontSize: '25pt' }}>
-              {t('Receipt:header', { nr: props.bookingId })}
+              {t('Receipt:header', { nr: bookingId })}
             </Text>
             <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'justify-between', width: '475px' }}>
               <View style={{ width: '400px' }}>
                 <Text>
                   <Trans
                     i18nKey='Receipt:info'
-                    count={parseInt(props.numberOfCats)}
+                    count={parseInt(numberOfCats)}
                   >
-                    A stay for {{ count: props.numberOfCats }} cat with
-                    {{ host: props.nickname }} between
-                    {{ checkin: props.startDate }} until
-                    {{ checkout: props.endDate }}:
+                    A stay for {{ count: numberOfCats }} cat with
+                    {{ host: nickname }} between
+                    {{ checkin: startDate }} until
+                    {{ checkout: endDate }}:
                   </Trans>
                 </Text>
               </View>
               <View style={{ width: '75px', textAlign: 'right' }}>
-                <Text>{formatPrice(props.priceTotal)} kr</Text>
+                <Text>{formatPrice(priceTotal)} kr</Text>
               </View>
             </View>
             <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'justify-between', width: '475px', marginTop: '20px' }}>
@@ -44,7 +45,7 @@ const ReceiptPDF = (props) => {
                 </Text>
               </View>
               <View style={{ width: '75px', textAlign: 'right' }}>
-                <Text>{formatPrice(props.priceTotal * 0.17)} kr</Text>
+                <Text>{formatPrice(priceTotal * 0.17)} kr</Text>
               </View>
             </View>
             <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'justify-between', width: '475px', marginTop: '20px' }}>
@@ -54,7 +55,7 @@ const ReceiptPDF = (props) => {
                 </Text>
               </View>
               <View style={{ width: '75px', textAlign: 'right' }}>
-                <Text>{formatPrice((props.priceTotal * 0.17) * 0.25)} kr</Text>
+                <Text>{formatPrice((priceTotal * 0.17) * 0.25)} kr</Text>
               </View>
             </View>
             <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'justify-between', width: '475px', paddingTop: '20px', marginTop: '20px', borderTopColor: '#000', borderTopWidth: 1 }}>
@@ -64,7 +65,7 @@ const ReceiptPDF = (props) => {
                 </Text>
               </View>
               <View style={{ width: '150px', textAlign: 'right' }}>
-                <Text style={{ fontSize: '25pt' }}>{priceOfOneAmount(props.priceTotal)} kr</Text>
+                <Text style={{ fontSize: '25pt' }}>{priceOfOneAmount(priceTotal)} kr</Text>
               </View>
             </View>
           </View>
