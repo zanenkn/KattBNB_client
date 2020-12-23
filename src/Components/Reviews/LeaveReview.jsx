f41ace2a9c5da5a8c898cffe61fa1db2f967853a
raptorf1 <raptor_f1@hotmail.com>
2020-12-23 13:30:04 +0200

    Applies prettier in all Reviews folder components
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- src/Components/Reviews/LeaveReview.jsx
+++ src/Components/Reviews/LeaveReview.jsx
@@ -1,79 +1,77 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from 'react'
-import withAuth from '../../HOC/withAuth'
-import { Header, Segment, Form, Message, Button } from 'semantic-ui-react'
-import { Trans, useTranslation } from 'react-i18next'
-import Spinner from '../ReusableComponents/Spinner'
-import axios from 'axios'
-import { detectLanguage } from '../../Modules/detectLanguage'
-import { wipeCredentials } from '../../Modules/wipeCredentials'
-import ReviewScore from '../ReusableComponents/ReviewScore'
-import queryString from 'query-string'
+import React, { useState, useEffect } from 'react';
+import withAuth from '../../HOC/withAuth';
+import { Header, Segment, Form, Message, Button } from 'semantic-ui-react';
+import { Trans, useTranslation } from 'react-i18next';
+import Spinner from '../ReusableComponents/Spinner';
+import axios from 'axios';
+import { detectLanguage } from '../../Modules/detectLanguage';
+import { wipeCredentials } from '../../Modules/wipeCredentials';
+import ReviewScore from '../ReusableComponents/ReviewScore';
+import queryString from 'query-string';
 
 const LeaveReview = (props) => {
+  const { t, ready } = useTranslation('LeaveReview');
 
-  const { t, ready } = useTranslation('LeaveReview')
-
-  const [loading, setLoading] = useState(false)
-  const [errors, setErrors] = useState([])
-  const [errorDisplay, setErrorDisplay] = useState(false)
-  const [reviewBody, setReviewBody] = useState('')
-  const [reviewScore, setReviewScore] = useState(0)
-
-  const [hostNickname, setHostNickname] = useState(null)
-  const [userId, setUserId] = useState(null)
-  const [bookingId, setBookingId] = useState(null)
-  const [profileId, setProfileId] = useState(null)
-  const [bookingStart, setBookingStart] = useState(null)
-  const [bookingEnd, setBookingEnd] = useState(null)
+  const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState([]);
+  const [errorDisplay, setErrorDisplay] = useState(false);
+  const [reviewBody, setReviewBody] = useState('');
+  const [reviewScore, setReviewScore] = useState(0);
+  const [hostNickname, setHostNickname] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [bookingId, setBookingId] = useState(null);
+  const [profileId, setProfileId] = useState(null);
+  const [bookingStart, setBookingStart] = useState(null);
+  const [bookingEnd, setBookingEnd] = useState(null);
 
   useEffect(() => {
     window.onpopstate = (e) => {
-      props.history.push('/all-bookings')
-    }
+      props.history.push('/all-bookings');
+    };
     if (props.location.state) {
-      setHostNickname(props.location.state.hostNickname)
-      setUserId(props.location.state.userId)
-      setBookingId(props.location.state.bookingId)
-      setProfileId(props.location.state.hostProfileId)
-      setBookingStart(props.location.state.startDate)
-      setBookingEnd(props.location.state.endDate)
+      setHostNickname(props.location.state.hostNickname);
+      setUserId(props.location.state.userId);
+      setBookingId(props.location.state.bookingId);
+      setProfileId(props.location.state.hostProfileId);
+      setBookingStart(props.location.state.startDate);
+      setBookingEnd(props.location.state.endDate);
     } else {
-      setHostNickname(queryString.parse(props.location.search).hostNickname)
-      setUserId(queryString.parse(props.location.search).userId)
-      setBookingId(queryString.parse(props.location.search).bookingId)
-      setProfileId(queryString.parse(props.location.search).hostProfileId)
-      setBookingStart(queryString.parse(props.location.search).startDate)
-      setBookingEnd(queryString.parse(props.location.search).endDate)
+      setHostNickname(queryString.parse(props.location.search).hostNickname);
+      setUserId(queryString.parse(props.location.search).userId);
+      setBookingId(queryString.parse(props.location.search).bookingId);
+      setProfileId(queryString.parse(props.location.search).hostProfileId);
+      setBookingStart(queryString.parse(props.location.search).startDate);
+      setBookingEnd(queryString.parse(props.location.search).endDate);
     }
-  }, [])
+  }, []);
 
   const onScoreClick = (e) => {
-    setReviewScore(parseInt(e.currentTarget.id))
-  }
+    setReviewScore(parseInt(e.currentTarget.id));
+  };
 
   const createReview = () => {
-    const lang = detectLanguage()
-    setLoading(true)
+    const lang = detectLanguage();
+    setLoading(true);
     if (window.navigator.onLine === false) {
-      setLoading(false)
-      setErrors(['reusable:errors:window-navigator'])
-      setErrorDisplay(true)
+      setLoading(false);
+      setErrors(['reusable:errors:window-navigator']);
+      setErrorDisplay(true);
     } else {
       if (reviewScore < 1) {
-        setLoading(false)
-        setErrors(['LeaveReview:error-no-score'])
-        setErrorDisplay(true)
+        setLoading(false);
+        setErrors(['LeaveReview:error-no-score']);
+        setErrorDisplay(true);
       } else if (reviewBody === '') {
-        setLoading(false)
-        setErrors(['LeaveReview:error-empty'])
-        setErrorDisplay(true)
+        setLoading(false);
+        setErrors(['LeaveReview:error-empty']);
+        setErrorDisplay(true);
       } else if (reviewBody.length > 1000) {
-        setLoading(false)
-        setErrors(['LeaveReview:error-length'])
-        setErrorDisplay(true)
+        setLoading(false);
+        setErrors(['LeaveReview:error-length']);
+        setErrorDisplay(true);
       } else {
-        const path = '/api/v1/reviews'
+        const path = '/api/v1/reviews';
         const payload = {
           score: reviewScore,
           body: reviewBody,
@@ -81,60 +79,65 @@ const LeaveReview = (props) => {
           user_id: userId,
           booking_id: bookingId,
           host_profile_id: profileId,
-          locale: lang
-        }
+          locale: lang,
+        };
         const headers = {
           uid: window.localStorage.getItem('uid'),
           client: window.localStorage.getItem('client'),
-          'access-token': window.localStorage.getItem('access-token')
-        }
-        axios.post(path, payload, { headers: headers })
+          'access-token': window.localStorage.getItem('access-token'),
+        };
+        axios
+          .post(path, payload, { headers: headers })
           .then(() => {
-            props.history.push('/successful-review')
+            props.history.push('/successful-review');
           })
-          .catch(error => {
+          .catch((error) => {
             if (error.response === undefined) {
-              wipeCredentials('/is-not-available?atm')
+              wipeCredentials('/is-not-available?atm');
             } else if (error.response.status === 500) {
-              setLoading(false)
-              setErrorDisplay(true)
-              setErrors(['reusable:errors:500'])
+              setLoading(false);
+              setErrorDisplay(true);
+              setErrors(['reusable:errors:500']);
             } else if (error.response.status === 422) {
-              window.alert(t('LeaveReview:error-no-host'))
-              props.history.push('/all-bookings')
+              window.alert(t('LeaveReview:error-no-host'));
+              props.history.push('/all-bookings');
             } else if (error.response.status === 503) {
-              wipeCredentials('/is-not-available?atm')
+              wipeCredentials('/is-not-available?atm');
             } else if (error.response.status === 401) {
-              window.alert(t('reusable:errors:401'))
-              wipeCredentials('/')
+              window.alert(t('reusable:errors:401'));
+              wipeCredentials('/');
             } else {
-              setLoading(false)
-              setErrorDisplay(true)
-              setErrors([error.response.data.error])
+              setLoading(false);
+              setErrorDisplay(true);
+              setErrors([error.response.data.error]);
             }
-          })
+          });
       }
     }
-  }
+  };
 
   if (ready) {
     return (
-      <div className='content-wrapper' >
-        <Header as='h1'>
-          {t('LeaveReview:title')}
-        </Header>
+      <div className='content-wrapper'>
+        <Header as='h1'>{t('LeaveReview:title')}</Header>
         <Segment className='whitebox'>
-          <p className='small-centered-paragraph' style={{ 'marginBottom': '2rem' }}>
+          <p className='small-centered-paragraph' style={{ marginBottom: '2rem' }}>
             <Trans i18nKey='LeaveReview:desc'>
-              Your cat(s) stayed with <strong style={{ 'color': '#c90c61' }}>{{ host: hostNickname }}</strong> during the dates of <strong style={{ 'color': '#c90c61' }}>{{ startDate: bookingStart }}</strong> until <strong style={{ 'color': '#c90c61' }}>{{ endDate: bookingEnd }}</strong>. Help us to improve KattBNB community by reviewing your experience.
+              Your cat(s) stayed with <strong style={{ color: '#c90c61' }}>{{ host: hostNickname }}</strong> during the
+              dates of <strong style={{ color: '#c90c61' }}>{{ startDate: bookingStart }}</strong> until
+              <strong style={{ color: '#c90c61' }}>{{ endDate: bookingEnd }}</strong>. Help us to improve KattBNB
+              community by reviewing your experience.
             </Trans>
           </p>
           <Form>
-            <div className='required field' >
-              <label>
-                {t('LeaveReview:label')}
-              </label>
-              <ReviewScore setScore={(e) => onScoreClick(e)} score={reviewScore} clickable={true} displayNumerical={true} />
+            <div className='required field'>
+              <label>{t('LeaveReview:label')}</label>
+              <ReviewScore
+                setScore={(e) => onScoreClick(e)}
+                score={reviewScore}
+                clickable={true}
+                displayNumerical={true}
+              />
               <Form.TextArea
                 placeholder={t('LeaveReview:placeholder')}
                 required
@@ -144,23 +147,27 @@ const LeaveReview = (props) => {
               />
             </div>
           </Form>
-          <p style={{ 'textAlign': 'end', 'fontSize': 'smaller', 'fontStyle': 'italic' }}>
+          <p style={{ textAlign: 'end', fontSize: 'smaller', fontStyle: 'italic' }}>
             {t('reusable:remaining-chars')} {1000 - reviewBody.length}
           </p>
-          {errorDisplay &&
-            <Message negative >
+          {errorDisplay && (
+            <Message negative>
               <ul id='message-error-list'>
-                {errors.map(error => (
+                {errors.map((error) => (
                   <li key={error}>{t(error)}</li>
                 ))}
               </ul>
             </Message>
-          }
-          <Button onClick={() => createReview()} className='submit-button' loading={loading} disabled={loading}>{t('LeaveReview:cta')}</Button>
+          )}
+          <Button onClick={() => createReview()} className='submit-button' loading={loading} disabled={loading}>
+            {t('LeaveReview:cta')}
+          </Button>
         </Segment>
       </div>
-    )
-  } else { return <Spinner /> }
-}
+    );
+  } else {
+    return <Spinner />;
+  }
+};
 
-export default withAuth(LeaveReview)
+export default withAuth(LeaveReview);
