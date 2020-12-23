c01c5f100288cb554c9db0275187f3da9faf0a92
raptorf1 <raptor_f1@hotmail.com>
2020-12-23 13:10:11 +0200

    Applies prettier in all Icons folder components
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- src/Components/Icons/HostProfile.jsx
+++ src/Components/Icons/HostProfile.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const HostProfile = (props) => {
   return (
-    <svg height={props.height} fill={props.fill} className={props.class} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
+    <svg
+      height={props.height}
+      fill={props.fill}
+      className={props.class}
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 20 20'
+    >
       <path d='M10,0,0,10H3V20H17V10h3ZM7.89,9.3a2.11,2.11,0,0,1,4.22,0v.85a2.11,2.11,0,1,1-4.22,0Zm6.33,7.33H5.78v-2.4a8.44,8.44,0,0,1,8.44,0Z' />
     </svg>
-  )
-}
+  );
+};
 
-export default HostProfile
+export default HostProfile;
--- src/Components/Icons/Price.jsx
+++ src/Components/Icons/Price.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Price = (props) => {
   return (
     <svg fill={props.fill} height={props.height} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
       <path d='M0 10V2l2-2h8l10 10-10 10L0 10zm4.5-4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z' />
     </svg>
-  )
-}
+  );
+};
 
-export default Price
+export default Price;
