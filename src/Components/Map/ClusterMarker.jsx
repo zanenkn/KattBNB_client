fd8e59efab3c1f616579096b4f2876fe4c90673f
raptorf1 <raptor_f1@hotmail.com>
2020-12-23 13:12:19 +0200

    Applies prettier in all Map folder components
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- src/Components/Map/ClusterMarker.jsx
+++ src/Components/Map/ClusterMarker.jsx
@@ -1,34 +1,37 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { List } from 'immutable'
-import Marker from './Marker'
-import { Label } from 'semantic-ui-react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { List } from 'immutable';
+import Marker from './Marker';
+import { Label } from 'semantic-ui-react';
 
 class ClusterMarker extends React.PureComponent {
-
   state = {
     clusterFaceMarkers: this.props.points.slice(0, 1),
-  }
+  };
 
   render() {
-
     return (
-      <div style={{ 'display': 'flex', 'transform': 'translate(-50%, -50%)' }} >
-        {this.state.clusterFaceMarkers.map(marker =>
-          <Marker
-            key={marker.id}
-            lat={marker.lat}
-            lng={marker.lng}
-            id={marker.id}
-            total={marker.total}
-          />
-        )}
-        {this.props.points.length > 1 &&
-          <Label circular style={{ 'height': '2em', 'width': '2em', 'fontSize': 'x-small', 'backgroundColor': '#c90c61', 'color': '#ffffff', 'transform': 'translate(-350%, -200%)' }}>
+      <div style={{ display: 'flex', transform: 'translate(-50%, -50%)' }}>
+        {this.state.clusterFaceMarkers.map((marker) => (
+          <Marker key={marker.id} lat={marker.lat} lng={marker.lng} id={marker.id} total={marker.total} />
+        ))}
+        {this.props.points.length > 1 && (
+          <Label
+            circular
+            style={{
+              height: '2em',
+              width: '2em',
+              fontSize: 'x-small',
+              backgroundColor: '#c90c61',
+              color: '#ffffff',
+              transform: 'translate(-350%, -200%)',
+            }}
+          >
             +{this.props.points.length - 1}
-          </Label>}
+          </Label>
+        )}
       </div>
-    )
+    );
   }
 }
 
@@ -36,6 +39,6 @@ ClusterMarker.propTypes = {
   points: PropTypes.array,
   users: PropTypes.instanceOf(List),
   selected: PropTypes.bool,
-}
+};
 
-export default ClusterMarker
+export default ClusterMarker;
--- src/Components/Map/Marker.jsx
+++ src/Components/Map/Marker.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Label } from 'semantic-ui-react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Label } from 'semantic-ui-react';
 
 class Marker extends React.PureComponent {
-
   static defaultProps = {
     inGroup: false,
-  }
+  };
 
   render() {
-
     return (
-      <div style={{ 'transform': 'translate(-50%, -50%)' }}>
+      <div style={{ transform: 'translate(-50%, -50%)' }}>
         <Label
           pointing='below'
-          style={{ 'backgroundColor': '#c90c61', 'color': '#ffffff', 'transform': 'translate(-50%, -50%)' }}
+          style={{ backgroundColor: '#c90c61', color: '#ffffff', transform: 'translate(-50%, -50%)' }}
           id={this.props.id}
           onClick={this.props.handleDatapointClick}
         >
           {this.props.total}&nbsp;kr
         </Label>
       </div>
-    )
+    );
   }
 }
 
-Marker.propTypes = { inGroup: PropTypes.bool }
+Marker.propTypes = { inGroup: PropTypes.bool };
 
-export default Marker
+export default Marker;
