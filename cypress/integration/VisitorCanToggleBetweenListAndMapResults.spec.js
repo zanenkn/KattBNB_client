f6cc3340967bc53ce7d953277b8bd60f36e86cdb
raptorf1 <raptor_f1@hotmail.com>
2021-01-09 19:22:27 +0200

    Adds fixture and 1 sec wait for google map to load in a test
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- cypress/integration/VisitorCanToggleBetweenListAndMapResults.spec.js
+++ cypress/integration/VisitorCanToggleBetweenListAndMapResults.spec.js
@@ -14,7 +14,7 @@ describe('Visitor can toggle between list and map results', () => {
       url:
         'http://localhost:3007/api/v1/host_profiles?startDate=1570492800000&endDate=1570752000000&cats=2&locale=en-US',
       status: 200,
-      response: '',
+      response: 'fixture:search_results_list.json',
     });
     const now = new Date(2019, 9, 1).getTime();
     cy.clock(now);
@@ -45,6 +45,7 @@ describe('Visitor can toggle between list and map results', () => {
 
   it('and see map view when she hits the relevant button', () => {
     cy.get('#map-button').click();
+    cy.wait(1000);
     cy.get('.list-card').should('not.exist');
     cy.get(
       '[style="z-index: 3; position: absolute; height: 100%; width: 100%; padding: 0px; border-width: 0px; margin: 0px; left: 0px; top: 0px; touch-action: pan-x pan-y;"]'
