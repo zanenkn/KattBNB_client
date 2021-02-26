0e24c9c64cc2f6ffbafcde866aa79a55d9f50423
Laura Reale <laaru1988@gmail.com>
2021-02-26 19:36:14 +0100

    refactor UserCanCreateHostProfile test
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: raptorf1 <raptor_f1@hotmail.com>

--- cypress/integration/UserCanCreateHostProfile.spec.js
+++ cypress/integration/UserCanCreateHostProfile.spec.js
@@ -1,6 +1,29 @@
 const api = 'http://localhost:3007/api/v1';
 const host_profiles = `${api}/host_profiles`;
 
+function updateProfile(status, response){
+  cy.route({
+    method: 'POST',
+    url: `${host_profiles}`,
+    status: status,
+    response: response,
+  });
+}
+
+function pickCalendarDates(){
+  cy.get('.required > .DayPicker > .DayPicker-wrapper > .DayPicker-NavBar > .DayPicker-NavButton--next').click();
+  cy.get(
+    '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(2)'
+  ).click();
+  cy.get(
+    '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(3)'
+  ).click();
+  cy.get(
+    '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(8)'
+  ).click();
+  cy.get('#save-host-profile-button').click();
+}
+
 describe('User can create a host profile', () => {
   beforeEach(() => {
     cy.server();
@@ -17,16 +40,11 @@ describe('User can create a host profile', () => {
       response: [],
     });
     cy.login('fixture:successful_login.json', 'george@mail.com', 'password', 200);
-    cy.wait(2000);
     cy.get('#user-icon').click({ force: true });
   });
+
   it('successfully', () => {
-    cy.route({
-      method: 'POST',
-      url: `${host_profiles}`,
-      status: 200,
-      response: 'fixture:successful_host_profile_creation.json',
-    });
+    updateProfile(200, 'fixture:successful_host_profile_creation.json');
     cy.contains(
       'You are not registered as a cat host and do not appear in the search. If you would like to host cats, please create a host profile.'
     );
@@ -46,41 +64,14 @@ describe('User can create a host profile', () => {
 
     cy.get('#userInputAddress').type('Solståndsgatan 23');
     cy.get('#search').click();
-    cy.get('.required > .DayPicker > .DayPicker-wrapper > .DayPicker-NavBar > .DayPicker-NavButton--next').click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(2)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(3)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(4)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(5)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(6)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(7)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(8)'
-    ).click();
-    cy.get('#save-host-profile-button').click();
+    pickCalendarDates();
     cy.on('window:alert', (str) => {
       expect(str).to.equal('You have successfully created your host profile!');
     });
   });
 
   it('unsuccessfully', () => {
-    cy.route({
-      method: 'POST',
-      url: `${host_profiles}`,
-      status: 422,
-      response: 'fixture:unsuccessful_host_profile_creation.json',
-    });
+    updateProfile(422, 'fixture:unsuccessful_host_profile_creation.json');
     cy.contains(
       'You are not registered as a cat host and do not appear in the search. If you would like to host cats, please create a host profile.'
     );
@@ -99,29 +90,7 @@ describe('User can create a host profile', () => {
 
     cy.get('#userInputAddress').type('Solståndsgatan 23');
     cy.get('#search').click();
-    cy.get('.required > .DayPicker > .DayPicker-wrapper > .DayPicker-NavBar > .DayPicker-NavButton--next').click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(2)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(3)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(4)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(5)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(6)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(7)'
-    ).click();
-    cy.get(
-      '.DayPicker-Months > .DayPicker-Month > .DayPicker-Body > .DayPicker-Week:nth-child(3) > .DayPicker-Day:nth-child(8)'
-    ).click();
-    cy.get('#save-host-profile-button').click();
+    pickCalendarDates();
     cy.contains("Description can't be blank");
     cy.contains("Supplement price per cat per day can't be blank");
   });
