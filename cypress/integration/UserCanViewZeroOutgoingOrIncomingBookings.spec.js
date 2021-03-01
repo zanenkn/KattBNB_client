49a43d65cb329f5bf0c55c60638832ab2d18d2b4
raptorf1 <raptor_f1@hotmail.com>
2021-03-01 23:11:40 +0200

    Refactors UserCanViewZeroOutgoingOrIncomingBookings test
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- cypress/integration/UserCanViewZeroOutgoingOrIncomingBookings.spec.js
+++ cypress/integration/UserCanViewZeroOutgoingOrIncomingBookings.spec.js
@@ -1,14 +1,19 @@
-describe('User can view her outgoing or incoming bookings', () => {
-  beforeEach(function () {
-    cy.server();
-    cy.route({
-      method: 'GET',
-      url: 'http://localhost:3007/api/v1/bookings?stats=yes&user_id=66&host_nickname=GeorgeTheGreek&locale=en-US',
-      status: 200,
-      response: 'fixture:booking_stats_0.json',
-    });
+const api = 'http://localhost:3007/api/v1';
+
+function fetchUserBookings() {
+  cy.server();
+  cy.route({
+    method: 'GET',
+    url: `${api}/bookings?stats=yes&user_id=66&host_nickname=GeorgeTheGreek&locale=en-US`,
+    status: 200,
+    response: 'fixture:booking_stats_0.json',
+  });
+}
+
+describe('User can view 0 outgoing or incoming bookings', () => {
+  before(function () {
+    fetchUserBookings();
     cy.login('fixture:successful_login.json', 'george@mail.com', 'password', 200);
-    cy.wait(2000);
     cy.get('#bookings-icon').click({ force: true });
   });
 
@@ -26,6 +31,8 @@ describe('User can view her outgoing or incoming bookings', () => {
   });
 
   it("and get redirected to the FAQ page when clicking on 'Become a host' link", () => {
+    fetchUserBookings();
+    cy.go('back');
     cy.get('#view-incoming-bookings').click();
     cy.contains('FAQ');
   });
