28a41bc0f3d0982e436820f01b637159483bcd43
raptorf1 <raptor_f1@hotmail.com>
2021-03-02 19:54:03 +0200

    Refactors VisitorCanSignup
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- cypress/integration/VisitorCanSignup.spec.js
+++ cypress/integration/VisitorCanSignup.spec.js
@@ -1,35 +1,6 @@
 describe('Visitor can sign up', () => {
-  beforeEach(function () {
-    cy.server();
+  it('and gets an error message if Terms & Conditions are not accepted', () => {
     cy.visit('http://localhost:3000');
-  });
-
-  it('successfully', () => {
-    cy.get('.hamburger-box').click();
-    cy.get('#login').click();
-    cy.get('#create-account').click();
-    cy.get('#signup-form').within(() => {
-      let text = [
-        ['#email', 'zane@mail.com'],
-        ['#password', 'password'],
-        ['#passwordConfirmation', 'password'],
-        ['#nickname', 'KittenPrincess'],
-      ];
-
-      text.forEach((element) => {
-        cy.get(element[0]).type(element[1]);
-      });
-    });
-
-    cy.get('#location').click();
-    cy.get('.visible > .selected > .text').click();
-    cy.get('.fitted > label').click();
-    cy.get('#sign-up-button').click();
-    cy.visit('http://localhost:3000/signup-success');
-    cy.contains('Almost done!');
-  });
-
-  it('and gets error message if captcha is invalid', () => {
     cy.get('.hamburger-box').click();
     cy.get('#login').click();
     cy.get('#create-account').click();
@@ -40,35 +11,23 @@ describe('Visitor can sign up', () => {
         ['#passwordConfirmation', 'pass'],
         ['#nickname', 'KittenPrincess'],
       ];
-
       text.forEach((element) => {
         cy.get(element[0]).type(element[1]);
       });
     });
+    cy.get('#sign-up-button').click();
+    cy.contains('You must accept the Terms and Conditions to continue!');
+  });
 
+  it('and gets error message if captcha is invalid', () => {
     cy.get('.fitted > label').click();
     cy.get('#sign-up-button').click();
     cy.contains("You didn't input the captcha phrase correctly, please try again!");
   });
 
-  it('and gets an error message if she does not accept Terms & Conditions', () => {
-    cy.get('.hamburger-box').click();
-    cy.get('#login').click();
-    cy.get('#create-account').click();
-    cy.get('#signup-form').within(() => {
-      let text = [
-        ['#email', 'zane@mail'],
-        ['#password', 'pass'],
-        ['#passwordConfirmation', 'pass'],
-        ['#nickname', 'KittenPrincess'],
-      ];
-
-      text.forEach((element) => {
-        cy.get(element[0]).type(element[1]);
-      });
-    });
-
+  it('successfully', () => {
     cy.get('#sign-up-button').click();
-    cy.contains('You must accept the Terms and Conditions to continue!');
+    cy.visit('http://localhost:3000/signup-success');
+    cy.contains('Almost done!');
   });
 });
