3ced9dfefeb221e60742c83c5b6029de2fc1336a
raptorf1 <raptor_f1@hotmail.com>
2021-03-02 20:03:36 +0200

    Refactors VisitorCanViewMenu test
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- cypress/integration/VisitorCanViewMenu.spec.js
+++ cypress/integration/VisitorCanViewMenu.spec.js
@@ -1,10 +1,6 @@
 describe('Visitor can view menu', () => {
-  beforeEach(function () {
-    cy.server();
-    cy.visit('http://localhost:3000/');
-  });
-
   it('and can toggle visibility by clicking on the hamburger', () => {
+    cy.visit('http://localhost:3000/');
     cy.get('.hamburger-box').click();
     cy.get('#menu').should('be.visible');
     cy.get('.hamburger-box').click();
@@ -21,7 +17,6 @@ describe('Visitor can view menu', () => {
 
   it('and can see links to different homepage sections in SE', () => {
     let links = ['Logga in', 'Registrera', 'Om oss', 'Frågor och svar', 'Kontakta oss', 'Användarvillkor'];
-    cy.get('.hamburger-box').click();
     cy.get('#se').click();
     cy.get('.hamburger-box').click();
     links.forEach((link) => {
@@ -30,7 +25,6 @@ describe('Visitor can view menu', () => {
   });
 
   it('and can toggle menu visibility off by clicking outside the menu', () => {
-    cy.get('.hamburger-box').click();
     cy.get('#menu').should('be.visible');
     cy.get('[width="100px"] > [fill="#C90B61"]').click({ force: true });
     cy.get('#menu').should('not.be.visible');
