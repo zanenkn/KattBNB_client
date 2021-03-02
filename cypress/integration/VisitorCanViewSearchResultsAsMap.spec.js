6130806436b4a796283869b49f569a231fef0744
raptorf1 <raptor_f1@hotmail.com>
2021-03-02 22:44:01 +0200

    Refactors VisitorCanViewSearchResultsAsMap test
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- cypress/integration/VisitorCanViewSearchResultsAsMap.spec.js
+++ cypress/integration/VisitorCanViewSearchResultsAsMap.spec.js
@@ -1,21 +1,32 @@
+const api = 'http://localhost:3007/api/v1';
+
+function fetchHostProfile() {
+  cy.server();
+  cy.route({
+    method: 'GET',
+    url: `${api}/host_profiles?user_id=44&locale=en-US`,
+    status: 200,
+    response: 'fixture:host_profile_datapoint_click_map.json',
+  });
+}
+
+function fetchResults(url) {
+  cy.route({
+    method: 'GET',
+    url: url,
+    status: 200,
+    response: 'fixture:search_results_list.json',
+  });
+}
+
 describe('Visitor can view search results as a map', () => {
-  beforeEach(() => {
+  before(() => {
     cy.server();
     cy.visit('http://localhost:3000');
-    cy.route({
-      method: 'GET',
-      url:
-        'http://localhost:3007/api/v1/host_profiles?location=Stockholm&startDate=1570492800000&endDate=1570752000000&cats=2&locale=en-US',
-      status: 200,
-      response: 'fixture:search_results_list.json',
-    });
-    cy.route({
-      method: 'GET',
-      url:
-        'http://localhost:3007/api/v1/host_profiles?startDate=1570492800000&endDate=1570752000000&cats=2&locale=en-US',
-      status: 200,
-      response: 'fixture:search_results_list.json',
-    });
+    fetchResults(
+      `${api}/host_profiles?location=Stockholm&startDate=1570492800000&endDate=1570752000000&cats=2&locale=en-US`
+    );
+    fetchResults(`${api}/host_profiles?startDate=1570492800000&endDate=1570752000000&cats=2&locale=en-US`);
     const now = new Date(2019, 9, 1).getTime();
     cy.clock(now);
     cy.get('.twelve > [href="/search"]').click();
@@ -41,23 +52,14 @@ describe('Visitor can view search results as a map', () => {
 
   it('and see datapoints', () => {
     let labels = ['#44', '#55'];
-
     labels.forEach((label) => {
       cy.get(label).should('be.visible');
     });
   });
 
   it('and see a popup with host information by clicking on a specific datapoint', () => {
-    cy.server();
-    cy.route({
-      method: 'GET',
-      url: 'http://localhost:3007/api/v1/host_profiles?user_id=44&locale=en-US',
-      status: 200,
-      response: 'fixture:host_profile_datapoint_click_map.json',
-    });
-
+    fetchHostProfile();
     cy.get('#44').click({ force: true });
-
     let hostData = [
       'carla',
       'Stockholm',
@@ -65,39 +67,27 @@ describe('Visitor can view search results as a map', () => {
       'The stay for 2 cats with carla during the dates of 2019-10-08 until 2019-10-11 would in total cost',
       '679 kr',
     ];
-
     hostData.forEach((data) => {
       cy.contains(data);
     });
   });
 
   it("and see the full host profile after clicking 'more' in host popup", () => {
-    cy.server();
+    fetchHostProfile();
     cy.route({
       method: 'GET',
-      url: 'http://localhost:3007/api/v1/host_profiles?user_id=44&locale=en-US',
-      status: 200,
-      response: 'fixture:host_profile_datapoint_click_map.json',
-    });
-    cy.route({
-      method: 'GET',
-      url: 'http://localhost:3007/api/v1/reviews?host_profile_id=4&locale=en-US',
+      url: `${api}/reviews?host_profile_id=4&locale=en-US`,
       status: 200,
       response: [],
     });
-
     let hostData = [
       ['#nickname', '#description', '#per-day', ':nth-child(10) > #total'],
       ['carla', 'I have the nicest hair in the world! And I love cats btw :P', '169.75 kr/day', '679 kr'],
     ];
-
-    cy.get('#44').click({ force: true });
     cy.get('#more').click();
-
     hostData[0].forEach((data) => {
       cy.get(data).contains(hostData[1][hostData[0].indexOf(data)]);
     });
-
     cy.get('#avatar').should('be.visible');
   });
 });
