1146186beb782c89afa18d0217785c512de441ed
raptorf1 <raptor_f1@hotmail.com>
2021-03-03 23:34:53 +0200

    Exposes captcha in SignUp component during development. Refactors VisitorCanSignup test with existing fixtures.
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- cypress/integration/VisitorCanSignup.spec.js
+++ cypress/integration/VisitorCanSignup.spec.js
@@ -1,3 +1,15 @@
+const api = 'http://localhost:3007/api/v1';
+
+function signupPostRequest(status, response) {
+  cy.server();
+  cy.route({
+    method: 'POST',
+    url: `${api}/auth`,
+    status: status,
+    response: response,
+  });
+}
+
 describe('Visitor can sign up', () => {
   it('and gets an error message if Terms & Conditions are not accepted', () => {
     cy.visit('http://localhost:3000');
@@ -6,9 +18,9 @@ describe('Visitor can sign up', () => {
     cy.get('#create-account').click();
     cy.get('#signup-form').within(() => {
       let text = [
-        ['#email', 'zane@mail'],
+        ['#email', 'george@'],
         ['#password', 'pass'],
-        ['#passwordConfirmation', 'pass'],
+        ['#passwordConfirmation', 'passd'],
         ['#nickname', 'KittenPrincess'],
       ];
       text.forEach((element) => {
@@ -25,9 +37,36 @@ describe('Visitor can sign up', () => {
     cy.contains("You didn't input the captcha phrase correctly, please try again!");
   });
 
-  it('successfully', () => {
+  it('and gets various error messages from API', () => {
+    signupPostRequest(422, 'fixture:unsuccessful_signup.json');
+    cy.get('#cypress-captcha').then((span) => {
+      const cap = span.text();
+      cy.get('#userCaptcha').type(cap);
+    });
     cy.get('#sign-up-button').click();
-    cy.visit('http://localhost:3000/signup-success');
+    cy.contains("Password confirmation doesn't match Password");
+    cy.contains('Password is too short (minimum is 6 characters)');
+    cy.contains('Email is not an email');
+    cy.contains("Location can't be blank");
+  });
+
+  it('successfully', () => {
+    signupPostRequest(200, 'fixture:successful_signup.json');
+    cy.get('#signup-form').within(() => {
+      let text = [
+        ['#email', 'zane@mail.com'],
+        ['#password', 'Am@zing-pass'],
+        ['#passwordConfirmation', 'Am@zing-pass'],
+        ['#nickname', 'KittenPrincess'],
+      ];
+      text.forEach((element) => {
+        cy.get(element[0]).clear().type(element[1]);
+      });
+    });
+    cy.get('.dropdown:nth-child(3)').click();
+    cy.get('.item:nth-child(2)').click();
+    cy.get('#sign-up-button').click();
+    cy.location('pathname').should('eq', '/signup-success');
     cy.contains('Almost done!');
   });
 });
