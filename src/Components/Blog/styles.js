8bbac07457d640f859cb2f79436d81ec6c9abce1
zanenkn <zane.neikena@gmail.com>
2021-03-13 19:37:45 +0100

    One more try to fix styling.
    Co-authored-by: george <raptor_f1@hotmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- src/Components/Blog/styles.js
+++ src/Components/Blog/styles.js
@@ -1,70 +1,58 @@
 import styled from 'styled-components';
 
 export const PostWrapper = styled.div`
-  &&& {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 4rem;
+  display: flex !important;
+  flex-direction: column !important;
+  margin-bottom: 4rem !important;
 
-    @media (min-width: 640px) {
-      flex-direction: row;
-      margin-bottom: 3rem;
-      align-items: center;
-    }
+  @media (min-width: 640px) {
+    flex-direction: row !important;
+    margin-bottom: 3rem !important;
+    align-items: center !important;
   }
 `;
 
 export const PostImage = styled.img`
-  &&& {
-    width: 100%;
-    margin-bottom: 1rem;
+  width: 100% !important;
+  margin-bottom: 1rem !important;
 
-    @media (min-width: 640px) {
-      width: 200px;
-      height: 200px;
-      margin: 0 2rem 0 0;
-    }
+  @media (min-width: 640px) {
+    width: 200px !important;
+    height: 200px !important;
+    margin: 0 2rem 0 0 !important;
   }
 `;
 
 export const FeaturedImage = styled.img`
-  &&& {
-    width: 100%;
-    max-height: 300px;
-    object-fit: cover;
-    @media (min-width: 640px) {
-      max-height: 400px;
-    }
-    margin-bottom: 2rem;
+  width: 100% !important;
+  max-height: 300px !important;
+  object-fit: cover !important;
+  @media (min-width: 640px) {
+    max-height: 400px !important;
   }
+  margin-bottom: 2rem !important;
 `;
 
 export const Divider = styled.hr`
-  &&& {
-    width: 100%;
-    border: solid 0.25pt silver;
-    width: 100%;
-    margin: 3rem auto;
-  }
+  width: 100% !important;
+  border: solid 0.25pt silver !important;
+  width: 100% !important;
+  margin: 3rem auto !important;
 `;
 
 export const AuthorAvatar = styled.img`
-  &&& {
-    width: 30px;
-    height: 30px;
-    border-radius: 9999px;
-    margin-right: 0.5rem;
-  }
+  width: 30px !important;
+  height: 30px !important;
+  border-radius: 9999px !important;
+  margin-right: 0.5rem !important;
 `;
 
 export const Flex = styled.div`
-  &&& {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    margin: 0.5rem auto 4rem;
-    & > p {
-      margin: 0;
-    }
+  display: flex !important;
+  flex-direction: row !important;
+  align-items: center !important;
+  margin: 0.5rem auto 4rem !important;
+  & > p {
+    margin: 0 !important;
   }
 `;
--- src/styles/common.js
+++ src/styles/common.js
@@ -1,14 +1,12 @@
 import styled from 'styled-components';
 
 export const ContentWrapper = styled.div`
-  &&& {
-    @media (max-width: 768px) {
-    padding-left: 2rem;
-    padding-right: 2rem;
-    }
-    margin: 0 auto;
-    padding-top: 5vw;
-    padding-bottom: 10vw;
-    max-width: 768px;
+  @media (max-width: 768px) {
+    padding-left: 2rem !important;
+    padding-right: 2rem !important;
   }
+  margin: 0 auto !important;
+  padding-top: 5vw !important;
+  padding-bottom: 10vw !important;
+  max-width: 768px !important;
 `;
