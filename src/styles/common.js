a60031048e62296b75a3afe0fc5e52cac8db3324
zanenkn <zane.neikena@gmail.com>
2021-03-13 19:27:40 +0100

    Hopefully a styling fix.
    Co-authored-by: george <raptor_f1@hotmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- src/styles/common.js
+++ src/styles/common.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
 export const ContentWrapper = styled.div`
-  @media (max-width: 768px) {
+  &&& {
+    @media (max-width: 768px) {
     padding-left: 2rem;
     padding-right: 2rem;
+    }
+    margin: 0 auto;
+    padding-top: 5vw;
+    padding-bottom: 10vw;
+    max-width: 768px;
   }
-  margin: 0 auto;
-  padding-top: 5vw;
-  padding-bottom: 10vw;
-  max-width: 768px;
 `;
