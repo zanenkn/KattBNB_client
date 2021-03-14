3913993cd797d4cf125887b96576edeb4265dd73
raptorf1 <raptor_f1@hotmail.com>
2021-03-14 16:15:58 +0200

    Adjusts passwordCheck module. Adds new error into translation files. Adjusts SignUp component accordingly.
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- src/Modules/passwordCheck.js
+++ src/Modules/passwordCheck.js
@@ -1,6 +1,6 @@
-const passwordCheck = (password) => {
-  const checkRegExp = /^(?=.\d)(?=.[a-z])(?=.*[A-Z]).{6,20}$/;
-  if (password.match(checkRegExp)) {
+const passwordCheck = (inputtxt) => {
+  let passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+  if (inputtxt.match(passw)) {
     return true;
   } else {
     return false;
