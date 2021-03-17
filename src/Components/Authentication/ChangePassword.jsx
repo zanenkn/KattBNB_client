54e8707dd300ad3f6abf91061e1f2963a3ed0548
raptorf1 <raptor_f1@hotmail.com>
2021-03-17 19:26:14 +0200

    Refactors ChangePassword component with hooks
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- src/Components/Authentication/ChangePassword.jsx
+++ src/Components/Authentication/ChangePassword.jsx
@@ -1,180 +1,141 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Header, Segment, Form, Message, Button } from 'semantic-ui-react';
 import axios from 'axios';
 import { detectLanguage } from '../../Modules/detectLanguage';
 import { wipeCredentials } from '../../Modules/wipeCredentials';
 import { passwordCheck } from '../../Modules/passwordCheck';
 import queryString from 'query-string';
-import { withTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 import Spinner from '../ReusableComponents/Spinner';
 
-class ChangePassword extends Component {
-  state = {
-    errors: '',
-    errorDisplay: false,
-    successDisplay: false,
-    loading: false,
-    password: '',
-    passwordConfirmation: '',
-  };
+const ChangePassword = ({ location: { search } }) => {
+  const { t, ready } = useTranslation('ChangePassword');
 
-  onChangeHandler = (e) => {
-    this.setState({ [e.target.id]: e.target.value });
-  };
+  const [errors, setErrors] = useState('');
+  const [errorDisplay, setErrorDisplay] = useState(false);
+  const [successDisplay, setSuccessDisplay] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
 
-  changePassword = (e) => {
-    e.preventDefault();
-    const { t } = this.props;
+  const changePassword = () => {
     if (window.navigator.onLine === false) {
-      this.setState({
-        errorDisplay: true,
-        errors: ['reusable:errors:window-navigator'],
-      });
+      setErrorDisplay(true);
+      setErrors(['reusable:errors:window-navigator']);
     } else {
-      if (
-        this.state.password === this.state.passwordConfirmation &&
-        passwordCheck(this.state.password) &&
-        this.props.location.search.length > 150
-      ) {
-        this.setState({ loading: true });
+      if (password === passwordConfirmation && passwordCheck(password) && search.length > 150) {
+        setLoading(true);
         const lang = detectLanguage();
         const path = '/api/v1/auth/password';
         const payload = {
-          password: this.state.password,
-          password_confirmation: this.state.passwordConfirmation,
-          uid: queryString.parse(this.props.location.search).uid,
-          'access-token': queryString.parse(this.props.location.search).token,
-          client: queryString.parse(this.props.location.search).client,
+          password: password,
+          password_confirmation: passwordConfirmation,
+          uid: queryString.parse(search).uid,
+          'access-token': queryString.parse(search).token,
+          client: queryString.parse(search).client,
           locale: lang,
         };
         axios
           .put(path, payload)
           .then(() => {
-            this.setState({
-              successDisplay: true,
-              errorDisplay: false,
-            });
+            setSuccessDisplay(true);
+            setErrorDisplay(false);
             setTimeout(function () {
               window.location.replace('/login');
             }, 2000);
           })
-          .catch((error) => {
-            if (error.response === undefined) {
+          .catch(({ response }) => {
+            if (response === undefined) {
               wipeCredentials('/is-not-available?atm');
-            } else if (error.response.status === 500) {
-              this.setState({
-                loading: false,
-                errorDisplay: true,
-                errors: ['reusable:errors:500'],
-              });
-            } else if (error.response.status === 503) {
+            } else if (response.status === 500) {
+              setLoading(false);
+              setErrorDisplay(true);
+              setErrors(['reusable:errors:500']);
+            } else if (response.status === 503) {
               wipeCredentials('/is-not-available?atm');
-            } else if (error.response.status === 401) {
+            } else if (response.status === 401) {
               window.alert(t('reusable:errors:401-password'));
               wipeCredentials('/password-reset');
             } else {
-              this.setState({
-                loading: false,
-                errorDisplay: true,
-                errors: error.response.data.errors.full_messages,
-              });
+              setLoading(false);
+              setErrorDisplay(true);
+              setErrors(response.data.errors.full_messages);
             }
           });
-      } else if (
-        this.state.password === this.state.passwordConfirmation &&
-        passwordCheck(this.state.password) &&
-        this.props.location.search.length < 150
-      ) {
-        this.setState({
-          errors: ['ChangePassword:error-1'],
-          errorDisplay: true,
-        });
+      } else if (password === passwordConfirmation && passwordCheck(password) && search.length < 150) {
+        setErrors(['ChangePassword:error-1']);
+        setErrorDisplay(true);
       } else {
-        this.setState({
-          errors: ['ChangePassword:error-2'],
-          errorDisplay: true,
-        });
+        setErrors(['ChangePassword:error-2']);
+        setErrorDisplay(true);
       }
     }
   };
 
-  listenEnterKey = (event) => {
+  const listenEnterKey = (event) => {
     if (event.key === 'Enter') {
-      this.changePassword(event);
+      changePassword();
     }
   };
 
-  render() {
-    const { t } = this.props;
-    let errorDisplay, successDisplay;
-
-    if (this.props.tReady) {
-      if (this.state.errorDisplay) {
-        errorDisplay = (
-          <Message negative>
-            <Message.Header style={{ textAlign: 'center' }}>{t('ChangePassword:error-header')}</Message.Header>
-            <ul id='message-error-list'>
-              {this.state.errors.map((error) => (
-                <li key={error}>{t(error)}</li>
-              ))}
-            </ul>
-          </Message>
-        );
-      }
-
-      if (this.state.successDisplay) {
-        successDisplay = (
-          <Message success style={{ textAlign: 'center' }}>
-            {t('ChangePassword:success-msg')}
-          </Message>
-        );
-      }
-
-      return (
-        <div className='content-wrapper'>
-          <Header as='h1'>{t('ChangePassword:title')}</Header>
-          <Segment className='whitebox'>
-            <p style={{ textAlign: 'center' }}>{t('ChangePassword:instructions')}</p>
-            <Form>
-              <Form.Input
-                required
-                id='password'
-                label={t('reusable:plch.password')}
-                value={this.state.password}
-                onChange={this.onChangeHandler}
-                placeholder={t('reusable:plch.password')}
-                type='password'
-                onKeyPress={this.listenEnterKey}
-              />
-              <Form.Input
-                required
-                id='passwordConfirmation'
-                label={t('reusable:plch.password-confirmation')}
-                value={this.state.passwordConfirmation}
-                onChange={this.onChangeHandler}
-                placeholder={t('reusable:plch.password-confirmation')}
-                type='password'
-                onKeyPress={this.listenEnterKey}
-              />
-            </Form>
-            {errorDisplay}
-            {successDisplay}
-            <Button
-              className='submit-button'
-              id='change-pass-button'
-              disabled={this.state.loading}
-              loading={this.state.loading}
-              onClick={this.changePassword}
-            >
-              {t('ChangePassword:title')}
-            </Button>
-          </Segment>
-        </div>
-      );
-    } else {
-      return <Spinner />;
-    }
+  if (ready) {
+    return (
+      <div className='content-wrapper'>
+        <Header as='h1'>{t('ChangePassword:title')}</Header>
+        <Segment className='whitebox'>
+          <p style={{ textAlign: 'center' }}>{t('ChangePassword:instructions')}</p>
+          <Form>
+            <Form.Input
+              required
+              id='password'
+              label={t('reusable:plch.password')}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder={t('reusable:plch.password')}
+              type='password'
+              onKeyPress={listenEnterKey}
+            />
+            <Form.Input
+              required
+              id='passwordConfirmation'
+              label={t('reusable:plch.password-confirmation')}
+              value={passwordConfirmation}
+              onChange={(e) => setPasswordConfirmation(e.target.value)}
+              placeholder={t('reusable:plch.password-confirmation')}
+              type='password'
+              onKeyPress={listenEnterKey}
+            />
+          </Form>
+          {errorDisplay && (
+            <Message negative>
+              <Message.Header style={{ textAlign: 'center' }}>{t('ChangePassword:error-header')}</Message.Header>
+              <ul id='message-error-list'>
+                {errors.map((error) => (
+                  <li key={error}>{t(error)}</li>
+                ))}
+              </ul>
+            </Message>
+          )}
+          {successDisplay && (
+            <Message success style={{ textAlign: 'center' }}>
+              {t('ChangePassword:success-msg')}
+            </Message>
+          )}
+          <Button
+            className='submit-button'
+            id='change-pass-button'
+            disabled={loading}
+            loading={loading}
+            onClick={changePassword}
+          >
+            {t('ChangePassword:title')}
+          </Button>
+        </Segment>
+      </div>
+    );
+  } else {
+    return <Spinner />;
   }
-}
+};
 
-export default withTranslation('ChangePassword')(ChangePassword);
+export default ChangePassword;
