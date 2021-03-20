809531fee8e541107fd2d36643aa8305693f9f56
raptorf1 <raptor_f1@hotmail.com>
2021-03-20 17:33:36 +0200

    Refactors SuccessfulRequest component with hooks. Adjusts relevant test.
    Co-authored-by: zane <zane.neikena@gmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>

--- src/Components/Bookings/SuccessfulRequest.jsx
+++ src/Components/Bookings/SuccessfulRequest.jsx
@@ -1,64 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Header, Segment } from 'semantic-ui-react';
 import Spinner from '../ReusableComponents/Spinner';
 import moment from 'moment';
-import { Trans, withTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 
-class SuccessfulRequest extends Component {
-  state = {
-    checkIn: '',
-    checkOut: '',
-    numberOfCats: '',
-    nickname: '',
-  };
+const SuccessfulRequest = ({ history }) => {
+  const { t, ready } = useTranslation('SuccessfulRequest');
 
-  componentDidMount() {
-    if (this.props.history.location.state === undefined || this.props.history.action === 'POP') {
-      this.props.history.push({ pathname: '/' });
+  const [checkIn, setCheckIn] = useState('');
+  const [checkOut, setCheckOut] = useState('');
+  const [numberOfCats, setNumberOfCats] = useState('');
+  const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    if (history.location.state === undefined || history.action === 'POP') {
+      history.push({ pathname: '/' });
     } else {
-      this.setState({
-        checkIn: moment(this.props.location.state.checkInDate).format('l'),
-        checkOut: moment(this.props.location.state.checkOutDate).format('l'),
-        numberOfCats: this.props.location.state.numberOfCats,
-        nickname: this.props.location.state.nickname,
-      });
+      const { checkInDate, checkOutDate, numberOfCats, nickname } = history.location.state;
+      setCheckIn(moment(checkInDate).format('l'));
+      setCheckOut(moment(checkOutDate).format('l'));
+      setNumberOfCats(numberOfCats);
+      setNickname(nickname);
     }
-  }
+    // eslint-disable-next-line
+  }, []);
 
-  render() {
-    const { t } = this.props;
-    if (this.props.tReady) {
-      return (
-        <div className='content-wrapper'>
-          <Header as='h1'>{t('SuccessfulRequest:title')}</Header>
-          <Segment className='whitebox' style={{ textAlign: 'center' }}>
-            <p>
-              <Trans i18nKey='SuccessfulRequest:p1' count={parseInt(this.state.numberOfCats)}>
-                You have successfully requested a booking for
-                <strong style={{ color: '#c90c61' }}>{{ count: this.state.numberOfCats }} cat</strong> with
-                <strong style={{ color: '#c90c61' }}>{{ host: this.state.nickname }}</strong> during the dates of
-                <strong style={{ color: '#c90c61' }}>{{ checkin: this.state.checkIn }}</strong> until
-                <strong style={{ color: '#c90c61' }}>{{ checkout: this.state.checkOut }}</strong>.
-              </Trans>
-            </p>
-            <p>
-              <Trans i18nKey='SuccessfulRequest:p2'>
-                <strong style={{ color: '#c90c61' }}>{{ host: this.state.nickname }}</strong> now has 3 days to accept
-                or decline your request. We will let you know by email. Questions? Check out our
-                <Header as={Link} to='faq' className='fake-link'>
-                  FAQ
-                </Header>
-                .
-              </Trans>
-            </p>
-          </Segment>
-        </div>
-      );
-    } else {
-      return <Spinner />;
-    }
+  if (ready) {
+    return (
+      <div className='content-wrapper'>
+        <Header as='h1'>{t('SuccessfulRequest:title')}</Header>
+        <Segment className='whitebox' style={{ textAlign: 'center' }}>
+          <p>
+            <Trans i18nKey='SuccessfulRequest:p1' count={parseInt(numberOfCats)}>
+              You have successfully requested a booking for
+              <strong style={{ color: '#c90c61' }}>{{ count: numberOfCats }} cat</strong> with
+              <strong style={{ color: '#c90c61' }}>{{ host: nickname }}</strong> during the dates of
+              <strong style={{ color: '#c90c61' }}>{{ checkin: checkIn }}</strong> until
+              <strong style={{ color: '#c90c61' }}>{{ checkout: checkOut }}</strong>.
+            </Trans>
+          </p>
+          <p>
+            <Trans i18nKey='SuccessfulRequest:p2'>
+              <strong style={{ color: '#c90c61' }}>{{ host: nickname }}</strong> now has 3 days to accept or decline
+              your request. We will let you know by email. Questions? Check out our
+              <Header as={Link} to='faq' className='fake-link'>
+                FAQ
+              </Header>
+              .
+            </Trans>
+          </p>
+        </Segment>
+      </div>
+    );
+  } else {
+    return <Spinner />;
   }
-}
+};
 
-export default withTranslation('SuccessfulRequest')(SuccessfulRequest);
+export default SuccessfulRequest;
