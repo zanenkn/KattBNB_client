76a4e9a3de2262635a4bd1299db3dd5a4f7ff6cc
zanenkn <zane.neikena@gmail.com>
2021-07-01 10:48:44 +0200

    Start of work with search component.
    Co-authored-by: george <raptor_f1@hotmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: oliverochman <oliver.ochman@hotmail.com>

--- src/Components/Search/index.js
+++ src/Components/Search/index.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import { LOCATION_OPTIONS } from '../Modules/locationData';
+import LOCATION_OPTIONS from '../../Modules/locationData.json';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 import { connect } from 'react-redux';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
-import '../NpmPackageCSS/react-day-picker-range.css';
-import { detectLanguage } from '../Modules/detectLanguage';
+//import '../NpmPackageCSS/react-day-picker-range.css';
+import { detectLanguage } from '../../Modules/detectLanguage';
 import MomentLocaleUtils, { formatDate, parseDate } from 'react-day-picker/moment';
 import { withTranslation } from 'react-i18next';
-import Spinner from './ReusableComponents/Spinner';
+import Spinner from '../ReusableComponents/Spinner';
 import { Helmet } from 'react-helmet';
+import { Whitebox, Header, InlineLink, Dropdown, TextField, Button, Notice, Text } from '../../UI-Components';
 
 class Search extends Component {
   constructor(props) {
@@ -38,10 +39,6 @@ class Search extends Component {
 
   onChangeHandler = (e) => {
     this.setState({ [e.target.id]: e.target.value });
-  };
-
-  handleLocationChange = (e, { value }) => {
-    this.setState({ location: value });
   };
 
   showFromMonth() {
@@ -109,7 +106,123 @@ class Search extends Component {
 
   render() {
     const { t, tReady } = this.props;
-    return <div>a</div>
+    const lang = detectLanguage();
+    const { from, to } = this.state;
+    const modifiers = { start: from, end: to };
+    const today = new Date();
+
+    if (!tReady) return <Spinner />;
+
+    return (
+      <>
+        <Helmet>
+          <title>KattBNB - boka kattvakt online!</title>
+          <meta
+            name='description'
+            content='Det är inte enkelt att hitta en pålitlig kattvakt. Men lugn, vi löser det. På KattBNB bokar du kattvakt online - snabbt och enkelt!'
+          />
+          <link rel='canonical' href='https://kattbnb.se/search' />
+          <meta property='og:title' content='KattBNB - boka kattvakt online!' />
+          <meta property='og:url' content='https://kattbnb.se/search' />
+          <meta property='og:type' content='website' />
+          <meta
+            property='og:description'
+            content='Ställ inte in din semester. Vi har kattvakt till din katt. På KattBNB bokar du kattpassning online - snabbt och enkelt!'
+          />
+          <meta property='og:image' content='https://kattbnb.se/KattBNB_og.jpg' />
+        </Helmet>
+        <Header color='primary' centered>
+          {t('Search:title')}
+        </Header>
+        <Whitebox>
+          <div id='search-form' style={{ margin: 'auto', maxWidth: '177px' }}>
+            <DayPickerInput
+              value={from || t('Search:checkin')}
+              format='LL'
+              formatDate={formatDate}
+              parseDate={parseDate}
+              inputProps={{ readOnly: true }}
+              dayPickerProps={{
+                selectedDays: [from, { from, to }],
+                disabledDays: { after: to, before: today },
+                fromMonth: today,
+                toMonth: to,
+                modifiers,
+                numberOfMonths: 1,
+                firstDayOfWeek: 1,
+                localeUtils: MomentLocaleUtils,
+                locale: lang,
+                showWeekNumbers: true,
+              }}
+              onDayChange={this.handleFromChange}
+            />
+            <DayPickerInput
+              ref={(el) => (this.to = el)}
+              value={to || t('Search:checkout')}
+              format='LL'
+              formatDate={formatDate}
+              parseDate={parseDate}
+              inputProps={this.state.from === undefined ? { disabled: true } : { disabled: false, readOnly: true }}
+              dayPickerProps={{
+                selectedDays: [from, { from, to }],
+                disabledDays: this.state.from !== undefined ? { before: from } : { before: today },
+                modifiers,
+                firstDayOfWeek: 1,
+                showWeekNumbers: true,
+                month: from,
+                fromMonth: from,
+                localeUtils: MomentLocaleUtils,
+                locale: lang,
+                numberOfMonths: 1,
+              }}
+              onDayChange={this.handleToChange}
+            />
+            <div style={this.state.from === undefined && this.state.to === undefined ? { visibility: 'hidden' } : {}}>
+              <InlineLink style={{ textAlign: 'right' }} onClick={this.clearDates}>
+                {t('Search:reset')}
+              </InlineLink>
+            </div>
+            <Dropdown
+              data={LOCATION_OPTIONS}
+              id='location'
+              onChange={(val) => this.setState({ location: val })}
+              onKeyPress={this.listenEnterKeySearch}
+            />
+            <TextField
+              label={t('Search:how-many')}
+              type='number'
+              required
+              id='cats'
+              value={this.state.cats}
+              onChange={this.onChangeHandler}
+              onKeyPress={this.listenEnterKeySearch}
+            />
+          </div>
+
+          {this.state.errors !== '' && (
+            <Notice nature='danger'>
+              <Text bold centered>
+                {t('Search:error-header')}
+              </Text>
+              <ul>
+                {this.state.errors.map((error) => (
+                  <li key={error}>{t(error)}</li>
+                ))}
+              </ul>
+            </Notice>
+          )}
+
+          <Button id='search-button' onClick={this.search}>
+            {t('Search:cta')}
+          </Button>
+          <div style={{ marginTop: '2rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <InlineLink to='/area-list' discreet>
+              {t('Search:sitters-near-you')}
+            </InlineLink>
+          </div>
+        </Whitebox>
+      </>
+    );
     // if (tReady) {
     //   let errorDisplay;
     //   const lang = detectLanguage();
@@ -117,38 +230,9 @@ class Search extends Component {
     //   const modifiers = { start: from, end: to };
     //   const today = new Date();
 
-    //   if (this.state.errors !== '') {
-    //     errorDisplay = (
-    //       <Message negative>
-    //         <Message.Header>{t('Search:error-header')}</Message.Header>
-    //         <ul>
-    //           {this.state.errors.map((error) => (
-    //             <li key={error}>{t(error)}</li>
-    //           ))}
-    //         </ul>
-    //       </Message>
-    //     );
-    //   }
-
     //   return (
     //     <div className='content-wrapper'>
-    //       <Helmet>
-    //         <title>KattBNB - boka kattvakt online!</title>
-    //         <meta
-    //           name='description'
-    //           content='Det är inte enkelt att hitta en pålitlig kattvakt. Men lugn, vi löser det. På KattBNB bokar du kattvakt online - snabbt och enkelt!'
-    //         />
-    //         <link rel='canonical' href='https://kattbnb.se/search' />
-    //         <meta property='og:title' content='KattBNB - boka kattvakt online!' />
-    //         <meta property='og:url' content='https://kattbnb.se/search' />
-    //         <meta property='og:type' content='website' />
-    //         <meta
-    //           property='og:description'
-    //           content='Ställ inte in din semester. Vi har kattvakt till din katt. På KattBNB bokar du kattpassning online - snabbt och enkelt!'
-    //         />
-    //         <meta property='og:image' content='https://kattbnb.se/KattBNB_og.jpg' />
-    //       </Helmet>
-    //       <Header as='h1'>{t('Search:title')}</Header>
+
     //       <Segment className='whitebox'>
     //         <Form id='search-form' style={{ margin: 'auto', maxWidth: '177px' }}>
     //           <div className='required field' style={{ marginBottom: '0.5em' }}>
