c5704a3be67de1a91832b3531ada39b5f08bffa6
oliverochman <oliver.ochman@hotmail.com>
2021-07-26 19:48:44 +0200

    Migrated outgoingUpcoming component
    Co-authored-by: george <raptor_f1@hotmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: zanenkn <zane.neikena@gmail.com>

--- src/Components/Bookings/OutgoingBookings/outgoingUpcoming.js
+++ src/Components/Bookings/OutgoingBookings/outgoingUpcoming.js
@@ -3,7 +3,8 @@ import moment from 'moment';
 import Spinner from '../../ReusableComponents/Spinner';
 import { Trans, useTranslation } from 'react-i18next';
 import { withRouter } from 'react-router-dom';
-import { Container } from '../../../UI-Components';
+import Booking from './booking';
+import { Text } from '../../../UI-Components';
 
 const OutgoingUpcoming = ({ bookings, history }) => {
   const { t, ready } = useTranslation('OutgoingUpcoming');
@@ -20,62 +21,56 @@ const OutgoingUpcoming = ({ bookings, history }) => {
 
   return (
     <>
-      <p className='small-centered-paragraph'>
+      <Text centered bold space={2}>
         <Trans count={parseInt(bookings.length)} i18nKey='OutgoingUpcoming:main-header'>
           <strong>You have {{ count: bookings.length }} upcoming booking.</strong>
         </Trans>
-      </p>
-      <p style={{ textAlign: 'center' }}>{t('OutgoingUpcoming:desc')}</p>
+      </Text>
+      <Text space={6} centered>
+        {t('OutgoingUpcoming:desc')}
+      </Text>
       {bookings.map((upcoming) => {
         return (
-          <Container
-            style={{ backgroundColor: '#e8e8e8', marginTop: '2rem', padding: '2rem' }}
-            id={upcoming.id}
-            data-cy='outgoing-upcoming'
-            key={upcoming.id}
-          >
-            <p className='small-centered-paragraph'>
+          <Booking
+            key={'bookingnr' + upcoming.id}
+            testId='outgoing-upcoming'
+            text={
               <Trans count={parseInt(upcoming.number_of_cats)} i18nKey='OutgoingUpcoming:upc-desc'>
                 You have successfully booked a stay with <strong>{{ nickname: upcoming.host_nickname }}</strong> for
                 your <strong>{{ count: upcoming.number_of_cats }} cat</strong> for the dates of
                 <strong>{{ startDate: moment(upcoming.dates[0]).format('YYYY-MM-DD') }}</strong> until
                 <strong>{{ endDate: moment(upcoming.dates[upcoming.dates.length - 1]).format('YYYY-MM-DD') }}</strong>.
               </Trans>
-            </p>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-              <span>
-                <p
-                  className='fake-link-underlined'
-                  id={`booking-details-${upcoming.id}`}
-                  onClick={() => {
-                    history.push({
-                      pathname: '/booking-details',
-                      state: {
-                        hostId: upcoming.host_id,
-                        avatar: upcoming.host_avatar,
-                        description: upcoming.host_description,
-                        nickname: upcoming.host_nickname,
-                        startDate: moment(upcoming.dates[0]).format('YYYY-MM-DD'),
-                        endDate: moment(upcoming.dates[upcoming.dates.length - 1]).format('YYYY-MM-DD'),
-                        priceTotal: upcoming.price_total,
-                        address: upcoming.host_full_address,
-                        lat: upcoming.host_real_lat,
-                        long: upcoming.host_real_long,
-                        location: upcoming.host_location,
-                        numberOfCats: upcoming.number_of_cats,
-                        hostProfileId: upcoming.host_profile_id,
-                        score: upcoming.host_profile_score,
-                      },
-                    });
-                  }}
-                >
-                  {t('OutgoingUpcoming:details')}
-                </p>
-              </span>
-              <p
-                className='fake-link-underlined'
-                id={`booking-receipt-${upcoming.id}`}
-                onClick={() => {
+            }
+            booking={upcoming}
+            links={[
+              {
+                text: t('OutgoingUpcoming:details'),
+                action: () => {
+                  history.push({
+                    pathname: '/booking-details',
+                    state: {
+                      hostId: upcoming.host_id,
+                      avatar: upcoming.host_avatar,
+                      description: upcoming.host_description,
+                      nickname: upcoming.host_nickname,
+                      startDate: moment(upcoming.dates[0]).format('YYYY-MM-DD'),
+                      endDate: moment(upcoming.dates[upcoming.dates.length - 1]).format('YYYY-MM-DD'),
+                      priceTotal: upcoming.price_total,
+                      address: upcoming.host_full_address,
+                      lat: upcoming.host_real_lat,
+                      long: upcoming.host_real_long,
+                      location: upcoming.host_location,
+                      numberOfCats: upcoming.number_of_cats,
+                      hostProfileId: upcoming.host_profile_id,
+                      score: upcoming.host_profile_score,
+                    },
+                  });
+                },
+              },
+              {
+                text: t('reusable:cta:view-receipt'),
+                action: () => {
                   history.push({
                     pathname: '/booking-receipt',
                     state: {
@@ -88,12 +83,10 @@ const OutgoingUpcoming = ({ bookings, history }) => {
                       createdAt: moment(upcoming.created_at).format('YYYY-MM-DD'),
                     },
                   });
-                }}
-              >
-                {t('reusable:cta:view-receipt')}
-              </p>
-            </div>
-          </Container>
+                },
+              },
+            ]}
+          />
         );
       })}
     </>
