b03c094d04eb5ae72aee910454975e5301eaee67
oliverochman <oliver.ochman@hotmail.com>
2021-07-26 19:22:33 +0200

    Migrated outgoingRequests component
    Co-authored-by: george <raptor_f1@hotmail.com>
    Co-authored-by: LauraRe <laaru1988@gmail.com>
    Co-authored-by: zanenkn <zane.neikena@gmail.com>

--- src/Components/Bookings/OutgoingBookings/outgoingRequests.js
+++ src/Components/Bookings/OutgoingBookings/outgoingRequests.js
@@ -5,6 +5,7 @@ import { Trans, useTranslation } from 'react-i18next';
 import Popup from 'reactjs-popup';
 import OutRequestUserMessagePopup from '../OutRequestUserMessagePopup';
 import { Text, Container } from '../../../UI-Components';
+import Booking from './booking';
 
 const OutgoingRequests = ({ bookings }) => {
   const { t, ready } = useTranslation('OutgoingRequests');
@@ -21,34 +22,36 @@ const OutgoingRequests = ({ bookings }) => {
 
   return (
     <>
-      <Text centered bold>
+      <Text centered bold space={2}>
         <Trans count={parseInt(bookings.length)} i18nKey='OutgoingRequests:main-header'>
           You have made {{ count: bookings.length }} booking request.
         </Trans>
       </Text>
-      <Text centered>{t('OutgoingRequests:desc')}</Text>
+      <Text space={6} centered>
+        {t('OutgoingRequests:desc')}
+      </Text>
       {bookings.map((request) => {
         return (
-          <Container
-            style={{ backgroundColor: '#e8e8e8', marginTop: '2rem', padding: '2rem' }}
-            id={request.id}
-            data-cy='outgoing-requests'
-            key={request.id}
-          >
-            <p className='small-centered-paragraph'>
+          <Booking
+            key={'bookingnr' + request.id}
+            testId='outgoing-requests'
+            text={
               <Trans count={parseInt(request.number_of_cats)} i18nKey='OutgoingRequests:req-desc'>
-                You have requested to book a stay with <strong>{{ nickname: request.host_nickname }}</strong> for your{' '}
+                You have requested to book a stay with <strong>{{ nickname: request.host_nickname }}</strong> for your
                 <strong>{{ count: request.number_of_cats }} cat</strong> during the dates of
                 <strong>{{ startDate: moment(request.dates[0]).format('YYYY-MM-DD') }}</strong> until
                 <strong>{{ endDate: moment(request.dates[request.dates.length - 1]).format('YYYY-MM-DD') }}</strong>.
               </Trans>
-            </p>
-            <p className='small-centered-paragraph'>
+            }
+            extraText={
               <Trans i18nKey='OutgoingRequests:reply-before'>
                 You will receive a reply before
                 <strong>{{ date: moment(request.created_at).add(3, 'days').format('YYYY-MM-DD') }}</strong>.
               </Trans>
-            </p>
+            }
+            booking={request}
+            links={[]}
+          >
             <Popup
               modal
               trigger={<p className='fake-link-underlined'>{t('OutgoingRequests:view-message')}</p>}
@@ -64,7 +67,7 @@ const OutgoingRequests = ({ bookings }) => {
                 endDate={moment(request.dates[request.dates.length - 1]).format('YYYY-MM-DD')}
               />
             </Popup>
-          </Container>
+          </Booking>
         );
       })}
     </>
