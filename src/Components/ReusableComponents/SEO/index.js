484b81f2838df09113805f50a21b907a1baf0af9
zanenkn <zane.neikena@gmail.com>
2021-12-11 13:18:17 +0100

    Simple seo component

---
+++ src/Components/ReusableComponents/SEO/index.js
@@ -0,0 +1,26 @@
+import { Helmet } from 'react-helmet';
+import { SeoSrc } from './seoSrc';
+
+const SEO = ({page}) => {
+  debugger
+  return (
+    <Helmet>
+      <title>{SeoSrc[page]['title']}</title>
+      <meta
+        name='description'
+        content={SeoSrc[page]['description']}
+      />
+      <link rel='canonical' href={`https://kattbnb.se${SeoSrc[page]['href']}`} />
+      <meta property='og:title' content={SeoSrc[page]['title']} />
+      <meta property='og:url' content={`https://kattbnb.se${SeoSrc[page]['href']}`} />
+      <meta property='og:type' content={SeoSrc[page]['type']} />
+      <meta
+        property='og:description'
+        content={SeoSrc[page]['description']}
+      />
+      <meta property='og:image' content={`https://kattbnb.se/${SeoSrc[page]['image']}`} />
+    </Helmet>
+  );
+};
+
+export default SEO;
---
+++ src/Components/ReusableComponents/SEO/seoSrc.js
@@ -0,0 +1,9 @@
+export const SeoSrc = {
+  'search': {
+    title: 'KattBNB - boka kattvakt online!',
+    description: 'Det är inte enkelt att hitta en pålitlig kattvakt. Men lugn, vi löser det. På KattBNB bokar du kattvakt online - snabbt och enkelt!',
+    href: '/search',
+    type: 'website',
+    image: 'KattBNB_og.jpg'
+  }
+}
