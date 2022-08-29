dd13a5867ccaaac5a5a60159366871424aa913c4
zanenkn <zane.neikena@gmail.com>
2022-08-29 18:47:02 +0200

    scroll to question functionality in faq

--- src/Components/FAQ/index.js
+++ src/Components/FAQ/index.js
@@ -6,33 +6,22 @@ import Spinner from '../../common/Spinner';
 import queryString from 'query-string';
 import Prismic from '@prismicio/client';
 import { detectLanguage } from '../../Modules/detectLanguage';
-import { Container, ContentWrapper, Text, Header, InlineLink } from '../../UI-Components';
+import { ContentWrapper, Text, Header, InlineLink, Container } from '../../UI-Components';
 import SEO from '../../common/SEO';
 import Question from './question';
 
 const Faq = ({ location }) => {
-  const handleClick = (e, titleProps) => {
-    const { index } = titleProps;
-    const newIndex = activeIndex === index ? -1 : index;
-    setActiveIndex(newIndex);
-  };
-
   const useReturnRef = (key) => {
     const ref = useCallback(
       (node) => {
-        if (node !== null && queryString.parse(window.location.search).section === key) {
-          let nav = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--nav'));
-          if (!queryString.parse(window.location.search).active) {
-            if (node.children[1].childElementCount > 0) {
-              window.scrollTo({ top: node.getBoundingClientRect().top - nav, behavior: 'smooth' });
-            }
-          } else {
-            if (node.children[1].childElementCount > 0) {
-              let titles = node.children[1].getElementsByClassName('title');
-              let index = parseInt(queryString.parse(window.location.search).active.substring(1)) - 1;
-              window.scrollTo({ top: titles[index].offsetTop, behavior: 'smooth' });
-            }
-          }
+        if (
+          node !== null &&
+          node.children[0].childElementCount > 1 &&
+          queryString.parse(window.location.search).section &&
+          queryString.parse(window.location.search).section === key
+        ) {
+          const nav = parseInt(window.getComputedStyle(document.documentElement).getPropertyValue('--nav'));
+          window.scrollTo({ top: node.getBoundingClientRect().top - nav, behavior: 'smooth' });
         }
       },
       [questions]
@@ -102,198 +91,78 @@ const Faq = ({ location }) => {
           .
         </Trans>
       </Text>
-      <Container ref={general} space={6}>
-        <Header level={3}>{t('Faq:general')}</Header>
-        {questions.general.map((question) => {
-          return (
-            <Question
-              question={question}
-              activeIndex={activeIndex}
-              onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
-            />
-          );
-        })}
-      </Container>
-      <Container ref={sitter} space={6}>
-        <Header level={3}>{t('Faq:sitter')}</Header>
-        {questions.sitter.map((question) => {
-          return (
-            <Question
-              question={question}
-              activeIndex={activeIndex}
-              onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
-            />
-          );
-        })}
-      </Container>
-      <Container ref={owner} space={6}>
-        <Header level={3}>{t('Faq:owner')}</Header>
-        {questions.owner.map((question) => {
-          return (
-            <Question
-              question={question}
-              activeIndex={activeIndex}
-              onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
-            />
-          );
-        })}
-      </Container>
-      <Container ref={privacy} space={6}>
-        <Header level={3}>{t('Faq:privacy')}</Header>
-        {questions.privacy.map((question) => {
-          return (
-            <Question
-              question={question}
-              activeIndex={activeIndex}
-              onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
-            />
-          );
-        })}
-      </Container>
-      <Container ref={payments} space={6}>
-        <Header level={3}>{t('Faq:payments')}</Header>
-        {questions.payments.map((question) => {
-          return (
-            <Question
-              question={question}
-              activeIndex={activeIndex}
-              onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
-            />
-          );
-        })}
-      </Container>
+      <div ref={general}>
+        <Container space={6}>
+          <Header level={3}>{t('Faq:general')}</Header>
+          {questions.general.map((question) => {
+            return (
+              <Question
+                question={question}
+                activeIndex={activeIndex}
+                onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
+              />
+            );
+          })}
+        </Container>
+      </div>
+      <div ref={sitter} space={6}>
+        <Container space={6}>
+          <Header level={3}>{t('Faq:sitter')}</Header>
+          {questions.sitter.map((question) => {
+            return (
+              <Question
+                question={question}
+                activeIndex={activeIndex}
+                onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
+              />
+            );
+          })}
+        </Container>
+      </div>
+      <div ref={owner} space={6}>
+        <Container space={6}>
+          <Header level={3}>{t('Faq:owner')}</Header>
+          {questions.owner.map((question) => {
+            return (
+              <Question
+                question={question}
+                activeIndex={activeIndex}
+                onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
+              />
+            );
+          })}
+        </Container>
+      </div>
+      <div ref={privacy} space={6}>
+        <Container space={6}>
+          <Header level={3}>{t('Faq:privacy')}</Header>
+          {questions.privacy.map((question) => {
+            return (
+              <Question
+                question={question}
+                activeIndex={activeIndex}
+                onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
+              />
+            );
+          })}
+        </Container>
+      </div>
+      <div ref={payments} space={6}>
+        <Container space={6}>
+          <Header level={3}>{t('Faq:payments')}</Header>
+          {questions.payments.map((question) => {
+            return (
+              <Question
+                question={question}
+                activeIndex={activeIndex}
+                onClick={() => setActiveIndex(activeIndex !== question.data.index ? question.data.index : null)}
+              />
+            );
+          })}
+        </Container>
+      </div>
     </ContentWrapper>
   );
-
-  //       <div className='expanding-wrapper'>
-  //         <div ref={general} style={{ paddingTop: '2rem' }}>
-  //           <Header as='h3' style={{ textAlign: 'left' }}>
-  //             {t('Faq:general')}
-  //           </Header>
-  //           <Accordion>
-  //             {questions.general.map((question) => {
-  //               return (
-  //                 <>
-  //                   <Accordion.Title
-  //                     active={activeIndex === parseInt(question.data.index)}
-  //                     index={parseInt(question.data.index)}
-  //                     onClick={handleClick}
-  //                     style={{ color: 'grey', fontWeight: '600' }}
-  //                   >
-  //                     <Icon name='dropdown' style={{ color: '#c90c61' }} />
-  //                     {question.data.header[0].text}
-  //                   </Accordion.Title>
-  //                   <Accordion.Content active={activeIndex === parseInt(question.data.index)}>
-  //                     {RichText.render(question.data.body)}
-  //                   </Accordion.Content>
-  //                 </>
-  //               );
-  //             })}
-  //           </Accordion>
-  //         </div>
-  //         <div ref={sitter} style={{ paddingTop: '2rem' }}>
-  //           <Header as='h3' style={{ textAlign: 'left' }}>
-  //             {t('Faq:sitter')}
-  //           </Header>
-  //           <Accordion>
-  //             {questions.sitter.map((question) => {
-  //               return (
-  //                 <>
-  //                   <Accordion.Title
-  //                     active={activeIndex === parseInt(question.data.index)}
-  //                     index={parseInt(question.data.index)}
-  //                     onClick={handleClick}
-  //                     style={{ color: 'grey', fontWeight: '600' }}
-  //                   >
-  //                     <Icon name='dropdown' style={{ color: '#c90c61' }} />
-  //                     {question.data.header[0].text}
-  //                   </Accordion.Title>
-  //                   <Accordion.Content active={activeIndex === parseInt(question.data.index)}>
-  //                     {RichText.render(question.data.body)}
-  //                   </Accordion.Content>
-  //                 </>
-  //               );
-  //             })}
-  //           </Accordion>
-  //         </div>
-  //         <div ref={owner} style={{ paddingTop: '2rem' }}>
-  //           <Header as='h3' style={{ textAlign: 'left' }}>
-  //             {t('Faq:owner')}
-  //           </Header>
-  //           <Accordion>
-  //             {questions.owner.map((question) => {
-  //               return (
-  //                 <>
-  //                   <Accordion.Title
-  //                     active={activeIndex === parseInt(question.data.index)}
-  //                     index={parseInt(question.data.index)}
-  //                     onClick={handleClick}
-  //                     style={{ color: 'grey', fontWeight: '600' }}
-  //                   >
-  //                     <Icon name='dropdown' style={{ color: '#c90c61' }} />
-  //                     {question.data.header[0].text}
-  //                   </Accordion.Title>
-  //                   <Accordion.Content active={activeIndex === parseInt(question.data.index)}>
-  //                     {RichText.render(question.data.body)}
-  //                   </Accordion.Content>
-  //                 </>
-  //               );
-  //             })}
-  //           </Accordion>
-  //         </div>
-  //         <div ref={privacy} style={{ paddingTop: '2rem' }}>
-  //           <Header as='h3' style={{ textAlign: 'left' }}>
-  //             {t('Faq:privacy')}
-  //           </Header>
-  //           <Accordion>
-  //             {questions.privacy.map((question) => {
-  //               return (
-  //                 <>
-  //                   <Accordion.Title
-  //                     active={activeIndex === parseInt(question.data.index)}
-  //                     index={parseInt(question.data.index)}
-  //                     onClick={handleClick}
-  //                     style={{ color: 'grey', fontWeight: '600' }}
-  //                   >
-  //                     <Icon name='dropdown' style={{ color: '#c90c61' }} />
-  //                     {question.data.header[0].text}
-  //                   </Accordion.Title>
-  //                   <Accordion.Content active={activeIndex === parseInt(question.data.index)}>
-  //                     {RichText.render(question.data.body)}
-  //                   </Accordion.Content>
-  //                 </>
-  //               );
-  //             })}
-  //           </Accordion>
-  //         </div>
-  //         <div ref={payments} style={{ paddingTop: '2rem' }}>
-  //           <Header as='h3' style={{ textAlign: 'left' }}>
-  //             {t('Faq:payments')}
-  //           </Header>
-  //           <Accordion>
-  //             {questions.payments.map((question) => {
-  //               return (
-  //                 <>
-  //                   <Accordion.Title
-  //                     active={activeIndex === parseInt(question.data.index)}
-  //                     index={parseInt(question.data.index)}
-  //                     onClick={handleClick}
-  //                     style={{ color: 'grey', fontWeight: '600' }}
-  //                   >
-  //                     <Icon name='dropdown' style={{ color: '#c90c61' }} />
-  //                     {question.data.header[0].text}
-  //                   </Accordion.Title>
-  //                   <Accordion.Content active={activeIndex === parseInt(question.data.index)}>
-  //                     {RichText.render(question.data.body)}
-  //                   </Accordion.Content>
-  //                 </>
-  //               );
-  //             })}
-  //           </Accordion>
-  //         </div>
-  //       </div>
-  //     </>
-  //   );
 };
 
 export default Faq;
