74a3ca2bc6343543fd6e2687df95568b1f0f9eac
zanenkn <zane.neikena@gmail.com>
2022-11-05 14:33:20 +0100

    refactor error 503

--- src/common/Error503.js
+++ src/common/Error503.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react';
 import Spinner from './Spinner';
 import { wipeCredentials } from '../Modules/wipeCredentials';
 import { useTranslation } from 'react-i18next';
+import { ContentWrapper, Whitebox, Header, Text } from '../UI-Components';
 
 const Error503 = (props) => {
   const { t, ready } = useTranslation('Error503');
@@ -12,19 +13,19 @@ const Error503 = (props) => {
       wipeCredentials('/');
     }
   }, []);
-  return <div>a</div>
-  // if (ready) {
-  //   return (
-  //     <div className='content-wrapper'>
-  //       <Header as='h1'>{t('Error503:title')}</Header>
-  //       <Segment className='whitebox' style={{ textAlign: 'center' }}>
-  //         <p>{t('Error503:desc')}</p>
-  //       </Segment>
-  //     </div>
-  //   );
-  // } else {
-  //   return <Spinner />;
-  // }
+
+  if (!ready) return <Spinner />;
+  
+  return (
+    <ContentWrapper>
+      <Header level={1} centered>
+        {t('Error503:title')}
+      </Header>
+      <Whitebox>
+        <Text centered>{t('Error503:desc')}</Text>
+      </Whitebox>
+    </ContentWrapper>
+  );
 };
 
 export default Error503;
